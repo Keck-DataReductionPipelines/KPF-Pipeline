9ff4ea47a8b6f2d068acf098dcc0a500fa247aac
Erik Petigura <petigura@caltech.edu>
2018-04-24 14:43:15 -0700

    added spectral_extraction function

--- kpfpipe/core.py
+++ kpfpipe/core.py
@@ -36,6 +36,30 @@ class Trace(object):
         
 
 
+def spectral_extraction(sci, bias, flat, trace, badpixel, order, orderlet, config):
+    """
+    Extract spectrum from a single orderlet
+    
+    Args:
+        sci (array): science frame e.g. kpf0.redspec
+        bias (array): bias frame e.g. kpf0.redspec
+        flat (array): flat field frame e.g. kpf0.redspec
+        badpixel (array): bad pixel mask  
+        trace (array): a kpfpipe.Trace object
+        order (int): index of order
+        orderlet (int): index of orderlet
+        config (kpfpipe.config): configuration object
+
+    Returns:
+        array: a 1D extracted spectrum        
+    """
+    
+    pass
+    
+    
+    
+    
+    
 
 
 
