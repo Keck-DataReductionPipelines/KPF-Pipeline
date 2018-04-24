4a2a2fa0566a61ef5098d819b312553a77361ac3
Erik Petigura <petigura@caltech.edu>
2018-04-24 14:16:06 -0700

    Create core.py
    added in trace object

---
+++ kpfpipe/core.py
@@ -0,0 +1,42 @@
+"""
+Placeholder module
+"""
+
+class Trace(object):
+    __init__(self, ):
+        """
+        Fill in
+        """
+        
+    def center(self, row, order, orderlet):
+        """Ceter of order
+        
+        Args:
+            row (float): row of the CCD
+            order (int): index of the order
+            orderlet (int): index of orderlet
+            
+        Returns:
+            float: column of orderlet (sub-pixel)
+        """
+        
+    def width(self, row, order, orderlet):
+        """Return the width (subpixel) of the order
+        
+        Args:
+            row (float): row of the CCD
+            order (int): index of the order
+            orderlet (int): index of orderlet
+            
+        Returns:
+            float: widht of the orderlet (sub-pixel)
+            
+        """
+        
+        
+
+
+
+
+
+
