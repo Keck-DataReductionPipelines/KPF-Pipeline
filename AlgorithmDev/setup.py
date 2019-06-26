6d4afea8d9827fdc6da2b5879e9a5172c66578d2
cwang2016 <cwang@ipac.caltech.edu>
2019-06-26 11:35:10 -0700

    Add AlgorithmDev for spectral extraction.

---
+++ AlgorithmDev/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='AlgorithmDev',
+    version='0.0.1',
+    description='Python modules for KPF algorithm development',
+    author='S.Y. Wang',
+    author_email='cwang@ipac.caltedh.edu',
+    packages = ['AlgorithmDev'],
+    install_requires=['future']
+)
+
