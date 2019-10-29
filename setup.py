a56f92b09beebf247bd22ce6e0210e9a616f61e3
BJ Fulton <bjfultn@gmail.com>
2019-10-29 10:13:15 -0700

    adding real CLI

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import re
+
+def get_property(prop, project):
+    result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop),
+                       open(project + '/__init__.py').read())
+    return result.group(1)
+
+reqs = []
+for line in open('requirements.txt', 'r').readlines():
+    reqs.append(line)
+
+setup(
+    name="kpfpipe",
+    version=get_property('__version__', 'kpfpipe'),
+    author="BJ Fulton, Arpita Roy, Andrew Howard",
+    packages=find_packages(),
+    entry_points={'console_scripts': ['kpf=kpfpipe.cli:main']},
+    install_requires=reqs,
+    # data_files=[
+    #     (
+    #         'rvsearch_example_data',
+    #         [
+    #             'example_data/HD128311.csv',
+    #             'example_data/recoveries.csv'
+    #         ]
+    #     )
+    # ],
+)
---
+++ sphinx/index.rst
@@ -0,0 +1,20 @@
+.. KPFpipe documentation master file, created by
+   sphinx-quickstart on Tue Oct 29 09:44:21 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to KPFpipe's documentation!
+===================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
