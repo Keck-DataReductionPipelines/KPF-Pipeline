9a30d6e41033687b903eb850f62938461bccd2d9
BJ Fulton <bjfultn@gmail.com>
2019-10-29 12:06:24 -0700

    adding to docs

--- kpfpipe/primitives/core.py
+++ kpfpipe/primitives/core.py
@@ -8,6 +8,10 @@ class KPF_Primitive(Base_primitive):
     Base primitive for orther KPF primitives.
     All KPF primitive classes should ultimately inherit from this one.
 
+    Args:
+        action (keckdrpframework.models.action.Action): Keck DRPF Action object
+        context (keckdrpframework.models.processing_context.Processing_context): Keck DRPF Processing_context object
+
     """
 
     def __init__(self, action, context):
