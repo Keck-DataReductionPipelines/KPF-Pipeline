7af1151d46258a1c1cbc49341644c7b4e56699d7
Qifan Wang <qwangcr@gmail.com>
2019-12-12 11:53:29 -0800

    added HARPS data and updated readme.md

--- modules/tfa/primitives.py
+++ modules/tfa/primitives.py
@@ -29,40 +29,6 @@ class ProcessSpec:
         berv = spec.header['eso drs berv']
         dlamb = np.sqrt((1+berv/mc.C_SPEED)/(1-berv/mc.C_SPEED# README Template
 
-A template of README best practices to make your README simple to understand and easy to use.
-
-## Installation
-
-Quickly describe how to install your project and how to get it running
-
-1. Install Node dependencies
-
-        npm install
-
-2. Install Bower dependencies
-
-        bower install
-
-3. Install Composer dependencies
-
-        composer install
-
-## Usage
-
-Here you can write any kind of things that help others to use your project. Feel free to add as many aspects as you like. Here are just three examples:
-
-### Build Process
-If there is anyhting special about your build process or things that others have to take care of, write it down here
-
-### Deployment
-Since some projects follow a special kind of deployment, you should provide a proper description on how to deploy.
-
-### Cronjobs
-If you project is using cron jobs or any other automated scripts you should describe their functionality here.
-
-## Support & Ownership
-
-Feel free to ask [Roland Lösslein](info@weaintplastic.com) if you need some support when there are any questions left or if you need some support.))
         for order in range(spec.NOrder): 
             spec.shift_wave(dlamb, order)
 
