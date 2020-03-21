1515677a19845b1b4e8f323e1bd5c97e42ebc18d
Dave Rumph <drumph@Daves-MacBook-Pro-2.local>
2020-03-20 18:16:49 -0700

    Recipe parser/node-walker I think at this point it could run in the Keck Framework with only some tweeks.  It can handle resuming after calls, and preserves its state across multiple events. TODO: implement import, maybe provide a function wrapper that hides the argument handling that's needed to make this all work.

--- examples/FauxLevel0Primatives.py
+++ examples/FauxLevel0Primatives.py
@@ -1,21 +1,47 @@
 # FauxLevel0Primatives.py
 
+from keckdrpframework.models.action import Action
+from keckdrpframework.models.processing_context import ProcessingContext
+from kpf_pipeline_args import KpfPipelineArgs
+
 """
 Provides some faked primitives that do nothing but provide results
 that allow testing of the pipeline parsing and state tree walking.
 See example.recipe and simple_example.recipe for test usage.
 """
-def read_data(input):
-    return [0, 1, 2, 3, 4, 5, 6]
+def read_data(action: Action, context: ProcessingContext):
+    """
+    read_data
+    args: data_set
+    results: numpy.array
+    """
 
-def Normalize(input):
+    output = ([0, 1, 2, 3, 4, 5, 6],)
+    return KpfPipelineArgs(action.args.visitor, action.args.tree, (output,))
+
+def Normalize(action: Action, context: ProcessingContext):
+    """
+    Normalize
+    args:
+        input: numpy.array
+    results: (numpy.array, result: numeric)
+    """
     result = 0.5
-    return (input, result)
+    input = action.args.args[0]
+    return KpfPipelineArgs(action.args.visitor, action.args.tree, (input, result))
 
-def NoiseReduce(input, param=0.):
+def NoiseReduce(action: Action, context: ProcessingContext):
+    param = 0.
+    input = action.args.args[0]
+    if len(action.args.args) > 1:
+        param = action.args.args[1]
     result = (param != 0.)
-    return (input, result)
+    return KpfPipelineArgs(action.args.visitor, action.args.tree, (input, result))
 
-def Spectrum1D(input, param=None):
+def Spectrum1D(action: Action, context: ProcessingContext):
+    param = None
+    input = action.args.args[0]
+    if len(action.args.args) > 1:
+        param = action.args.args[1]
     result = (param is not None)
-    return (input, result)
+    return KpfPipelineArgs(action.args.visitor, action.args.tree, (input, result))
