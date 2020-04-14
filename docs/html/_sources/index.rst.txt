acfaf6ff8a516ff3a0fe43d584225bbe565b4429
Dave Rumph <drumph@Daves-MacBook-Pro-2.local>
2020-04-14 11:47:44 -0700

    Initial version of recipe documentation

--- docs/html/_sources/index.rst.txt
+++ docs/html/_sources/index.rst.txt
@@ -13,4 +13,5 @@ This is the documentation for the KPF Data Reduction Pipeline.
     intro/getting_started.rst
     repository/start.rst
     data_models/io.rst
+    pipeline/recipe.rst
 
--- docs/html/_static/searchtools.js
+++ docs/html/_static/searchtools.js
@@ -250,7 +250,8 @@ var Search = {
       if (results.length) {
         var item = results.pop();
         var listItem = $('<li style="display:none"></li>');
-        if (DOCUMENTATION_OPTIONS.FILE_SUFFIX === '') {
+        var requestUrl = "";
+        if (DOCUMENTATION_OPTIONS.BUILDER === 'dirhtml') {
           // dirhtml builder
           var dirname = item[0] + '/';
           if (dirname.match(/\/index\/$/)) {
@@ -429,7 +430,7 @@ var Search = {
         for (j = 0; j < _files.length; j++) {
           file = _files[j];
           if (!(file in scoreMap))
-            scoreMap[file] = {}
+            scoreMap[file] = {};
           scoreMap[file][word] = o.score;
         }
       });
@@ -437,7 +438,7 @@ var Search = {
       // create the mapping
       for (j = 0; j < files.length; j++) {
         file = files[j];
-        if (file in fileMap)
+        if (file in fileMap && fileMap[file].indexOf(word) === -1)
           fileMap[file].push(word);
         else
           fileMap[file] = [word];
