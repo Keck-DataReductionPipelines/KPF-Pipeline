680659d8d3808abe0c61be63a570a44fd4d0ef9f
BJ Fulton <bjfultn@gmail.com>
2020-05-27 16:20:55 -0700

    use docker-compose now

--- jenkins/run_jenkins_server.sh
+++ jenkins/run_jenkins_server.sh
@@ -1,1 +1,5 @@
+docker pull jenkins/jenkins
+
+docker build . --tag jenkins-docker
+
 docker run --privileged --rm -d --group-add 0 -v /var/run/docker.sock:/var/run/docker.sock -v jenkinsvol:/var/jenkins_home -p 5555:8080 -p 50000:50000 -P jenkins-docker
