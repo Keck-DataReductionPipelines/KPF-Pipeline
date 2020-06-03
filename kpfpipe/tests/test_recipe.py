8ba1e2b58b9b0f6a240168f81bbe0d7d040a96f2
BJ Fulton <bjfultn@gmail.com>
2020-06-03 14:07:46 -0700

    removing all recipe tests

--- kpfpipe/tests/test_recipe.py
+++ kpfpipe/tests/test_recipe.py
@@ -64,87 +64,87 @@ kpf2 = kpf2_from_fits(fname, data_type="NEID")
 result = to_fits(kpf2, "temp_level2.fits")
 """
 
-class TestKpfPipeline(KPFPipeline):
-    """
-    Test pipeline class extending KpfPipeline
-    """
+# class TestKpfPipeline(KPFPipeline):
+#     """
+#     Test pipeline class extending KpfPipeline
+#     """
 
-    def __init__(self, context: ProcessingContext):
-        """ constructor """
-        KPFPipeline.__init__(self, context)
-        self.event_table['test_start_recipe'] = ("test_start_recipe", "starting recipe", None)
+#     def __init__(self, context: ProcessingContext):
+#         """ constructor """
+#         KPFPipeline.__init__(self, context)
+#         self.event_table['test_start_recipe'] = ("test_start_recipe", "starting recipe", None)
 
-    def test_start_recipe(self, action, context):
-        """
-        Starts evaluating the recipe file (Python syntax) specified in context.config.run.recipe.
-        All actions are executed consecutively in the high priority queue
+#     def test_start_recipe(self, action, context):
+#         """
+#         Starts evaluating the recipe file (Python syntax) specified in context.config.run.recipe.
+#         All actions are executed consecutively in the high priority queue
 
-        Args:
-            action (keckdrpframework.models.action.Action): Keck DRPF Action object
-            context (keckdrpframework.models.ProcessingContext.ProcessingContext): Keck DRPF ProcessingContext object
-        """
-        try: 
-            recipe_str = action.args.recipe
-            self._recipe_ast = ast.parse(recipe_str)
-            self._recipe_visitor = KpfPipelineNodeVisitor(pipeline=self, context=context)
-            self._recipe_visitor.visit(self._recipe_ast)
-        except:
-            print(sys.exc_info())
+#         Args:
+#             action (keckdrpframework.models.action.Action): Keck DRPF Action object
+#             context (keckdrpframework.models.ProcessingContext.ProcessingContext): Keck DRPF ProcessingContext object
+#         """
+#         try: 
+#             recipe_str = action.args.recipe
+#             self._recipe_ast = ast.parse(recipe_str)
+#             self._recipe_visitor = KpfPipelineNodeVisitor(pipeline=self, context=context)
+#             self._recipe_visitor.visit(self._recipe_ast)
+#         except:
+#             print(sys.exc_info())
 
-    def test_primitive_validate_args(self, action: Action, context: ProcessingContext):
-        """
-        for each pair of arguments, validate that they are equal
-        """
-        args = action.args
-        if len(args) % 2 != 0:
-            assert False, f"test_primitive_validate_args called with an odd number of arguments, {len(args)}"
-        arg_iter = iter(args)
-        while True:
-            try:
-                arg1 = next(arg_iter)
-                arg2 = next(arg_iter)
-            except StopIteration:
-                break
-            except Exception as e:
-                assert False, f"Unexpected exception in test_primitive_validate_args: {e}"
-            assert arg1 == arg2, f"values didn't match as expected, {arg1} vs {arg2}"
+#     def test_primitive_validate_args(self, action: Action, context: ProcessingContext):
+#         """
+#         for each pair of arguments, validate that they are equal
+#         """
+#         args = action.args
+#         if len(args) % 2 != 0:
+#             assert False, f"test_primitive_validate_args called with an odd number of arguments, {len(args)}"
+#         arg_iter = iter(args)
+#         while True:
+#             try:
+#                 arg1 = next(arg_iter)
+#                 arg2 = next(arg_iter)
+#             except StopIteration:
+#                 break
+#             except Exception as e:
+#                 assert False, f"Unexpected exception in test_primitive_validate_args: {e}"
+#             assert arg1 == arg2, f"values didn't match as expected, {arg1} vs {arg2}"
 
-# This is the default framework configuration file path
-framework_config = 'configs/framework.cfg'
-framework_logcfg= 'configs/framework_logger.cfg'
+# # This is the default framework configuration file path
+# framework_config = 'configs/framework.cfg'
+# framework_logcfg= 'configs/framework_logger.cfg'
 
-pipe_config = "examples/default_simple.cfg"
+# pipe_config = "examples/default_simple.cfg"
 
-def run_recipe(recipe: str, pipe_config: str=pipe_config):
-    """
-    This is the code that runs the recipe given as the only argument.
-    It mimics the kpf framework/pipeline startup code in cli.py, but
-    uses strings instead of file names for the recipes.
-    """
-    pipe = TestKpfPipeline
+# def run_recipe(recipe: str, pipe_config: str=pipe_config):
+#     """
+#     This is the code that runs the recipe given as the only argument.
+#     It mimics the kpf framework/pipeline startup code in cli.py, but
+#     uses strings instead of file names for the recipes.
+#     """
+#     pipe = TestKpfPipeline
 
-    # Setup a pipeline logger
-    # This is to differentiate between the loggers of framework and pipeline
-    # and individual modules.
-    # The configs related to the logger is under the section [LOGGER]
+#     # Setup a pipeline logger
+#     # This is to differentiate between the loggers of framework and pipeline
+#     # and individual modules.
+#     # The configs related to the logger is under the section [LOGGER]
 
-    # Try to initialize the framework 
-    try:
-        framework = Framework(pipe, framework_config)
-        # Overwrite the framework logger with this instance of logger
-        # using framework default logger creates some obscure problem
-        framework.logger = start_logger('DRPFrame', framework_logcfg)
-        framework.pipeline.start(pipe_config)
-    except Exception as e:
-        print("Failed to initialize framework, exiting ...", e)
-        # traceback.print_exc()
-        # sys.exit(1)
+#     # Try to initialize the framework 
+#     try:
+#         framework = Framework(pipe, framework_config)
+#         # Overwrite the framework logger with this instance of logger
+#         # using framework default logger creates some obscure problem
+#         framework.logger = start_logger('DRPFrame', framework_logcfg)
+#         framework.pipeline.start(pipe_config)
+#     except Exception as e:
+#         print("Failed to initialize framework, exiting ...", e)
+#         # traceback.print_exc()
+#         # sys.exit(1)
 
-    # python code
-    arg = Arguments(name="test_start_recipe_args", recipe=recipe)
-    framework.append_event('test_start_recipe', arg)
-    framework.append_event('exit', Arguments())
-    framework.start()
+#     # python code
+#     arg = Arguments(name="test_start_recipe_args", recipe=recipe)
+#     framework.append_event('test_start_recipe', arg)
+#     framework.append_event('exit', Arguments())
+#     framework.start()
 
 # def test_recipe_basics():
 #     try:
