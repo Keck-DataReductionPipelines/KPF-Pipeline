3bcdc633f69339329977e75919d48e856a288289
cwang2016 <cwang@ipac.caltech.edu>
2020-06-09 18:18:31 -0700

    feature/order_trace: 1. fix the checking logic in order trace upper/lower edge estimation 		     2. use logger in the module to log the running info and time. 		     3. enable to print the debug info into a file or standard output                      4. update the input of the modules to incorporate with the defined pipeline and the data model.

--- modules/optimal_extraction/src/optimal_extraction.py
+++ modules/optimal_extraction/src/optimal_extraction.py
@@ -29,12 +29,15 @@ class OptimalExtraction(KPF0_Primitive):
         KPF0_Primitive.__init__(self, action, context)
         # start a logger
         self.logger = start_logger(self.__class__.__name__, None)
+        if not self.logger:
+            self.logger = self.context.logger
 
         # input argument
-        # assume action.args.arg.flat_data includes data, order_trace_result and header['ORDERTRACE']['POLY_ORD']
-        # and action.args.args.arg.spectrum_data includes data, header['PRIMARY']
-        self.flat_data = action.args.arg.flat_data
-        self.spectral_data = action.args.arg.spectral_data
+        # action.args[0] is for level 0 fits
+        # action.args[1] is for level 0 flat with order trace result extension
+
+        self.input_spectrum = action.args[0]
+        self.input_flat = action.args[1]
         # input configuration
         self.config = configparser.ConfigParser()
         try:
@@ -44,20 +47,24 @@ class OptimalExtraction(KPF0_Primitive):
             self.config.read(DEFAULT_CFG_PATH)
 
         # Order trace algorithm setup
-        self.alg = OrderTraceAlg(self.flat_data.data, self.spectral_data, config)
+        self.alg = OrderTraceAlg(self.input_flat, self.input_spectrum, config)
 
     def _pre_condition(self) -> bool:
         """
         Check for some necessary pre conditions
         """
         # input argument must be KPF0
-        success = isinstance(self.flat_data, KPF0) and isinstance(self.spectral_data, KPF0)
+        success = isinstance(self.input_flat, KPF0) and isinstance(self.input_spectrum, KPF0)
 
         return success
 
     def _perform(self):
-        opt_ext_result = self.alg.extract_spectrum(rectification_method=OptimalExtractAlg.VERTICAL, print_progress='no')
-        self.action.args.optimal_extraction_result = opt_ext_result['optimal_extraction_result']
+        # rectification_method: OptimalExtractAlg.NoRECT(fastest) OptimalExtractAlg.VERTICAL, OptimalExtractAlg.NORMAL
+        # extraction_method: 'optimal' (default), 'sum'
+        opt_ext_result = self.alg.extract_spectrum(rectification_method=OptimalExtractAlg.VERTICAL)
+        # result in dataframe
+        optimal_extraction_result = opt_ext_result['optimal_extraction_result']
+        # convert to level 1 data?
 
         return self.action.args
 
