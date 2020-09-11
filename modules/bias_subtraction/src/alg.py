2d9afbc4f3c932385417cf5f734dd5c60f5184ff
Abbysh <abbys@ipac.caltech.edu>
2020-09-11 10:34:08 -0400

    second attempt at fixing file-not-found error in bias_subtraction_test, assigned ownCloud test data to variable 'result_data'

--- modules/bias_subtraction/src/alg.py
+++ modules/bias_subtraction/src/alg.py
@@ -11,9 +11,9 @@ from astropy.io import fits
 
 #Subtracting 2D array, function to subtract master bias frame from raw data image
 
-class BiasSubtractionAlg:
+class ImageProcessingAlg:
     """steps
-    Bias Subtraction:
+    For Bias Subtraction:
         1. Reads in bias frame .fits data
         2. Reads in raw .fits data
         3. Checks whether both data arrays have same dimensions, prints "equal" or "not equal"
@@ -21,17 +21,59 @@ class BiasSubtractionAlg:
         5. Returns array of raw minus bias
         
         In pipeline terms: inputs two L0 files, outputs one L0 file
+          
+    Args:
+        
+    Attributes:
+    
+    Raises:
     """
-    #def __init__(self,data,config=None, logger=None)
+    #def __init__(self,data,config=None, logger=None):
+        
          
     def bias_subtraction(rawimage, masterbias):
         biasdata = fits.getdata(masterbias, ext=0)
         rawdata = fits.getdata(rawimage, ext=0)
-    #add check to see if both matrices have the same dimensions, Cindy's recommendation
+    #check to see if both matrices have the same dimensions, Cindy's recommendation
         if biasdata.shape==rawdata.shape:
-            print (".Fits Dimensions Equal, Check Passed")
+            print ("Bias .fits Dimensions Equal, Check Passed")
         if biasdata.shape!=rawdata.shape:
-            print (".Fits Dimensions NOT Equal! Check Failed")
+            print ("Bias .fits Dimensions NOT Equal! Check Failed")
         raw_minus_bias=rawdata-biasdata
         return raw_minus_bias
-
+    """ "add save of raw minus bias file as .fits """
+    
+    #def flat_division():
+        #flatdata=fits.getdata(masterflat,ext=0)
+        #rawminbiasdata=fits.getdata(rawminusbiasframe,ext=0)
+        #if rawminbiasdata.shape==flatdata.shape:
+            #print ("Flat .fits dimensions equal, check passed")
+        #if rawminbiasdata.shape!=flatdata.shape:
+            #print ("Flat .fits dimensions NOT equal! check failed")
+        #raw_min_bias_div_flat=rawminbiasdata/flatdata
+        #return raw_min_bias_div_flat
+    """ add save as .fits """
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
