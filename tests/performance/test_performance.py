d0902f468903ea8d5e1c02c4834528f826e09e22
BJ Fulton <bjfultn@gmail.com>
2020-09-16 15:01:50 -0700

    added real performance test

--- tests/performance/test_performance.py
+++ tests/performance/test_performance.py
@@ -2,122 +2,38 @@ import pytest
 import warnings
 import os
 import shutil
+import time
 from dotenv import load_dotenv
 
 from kpfpipe.models.level0 import *
+from tests.regression.test_level0 import test_from_NEID
 
 # Load .env file for test path 
 load_dotenv()
 
-# RECEIPT
+# Decorator function to assert that the execution time of a wrapped function 
+# takes less than time_limit in seconds
+def execution_time_limit(time_limit=300):
+    def timer(func):
+        def check(*args, **kwargs):
+            t1 = time.time()
+            func(*args, **kwargs)
+            t2 = time.time()
+            tt = t2 - t1
+
+            print("Function {:s} completed in {:.0f} seconds.".format(func.__name__, tt))
+            assert tt < time_limit, "Execution time for function {:s} took longer than the allocated limit ({:.0f} s > {:.0f} s)" \
+                                    .format(func.__name__, tt, time_limit)
+
+        return check
+    return timer
+
+# Test time to create level 0 files from NEID data. Should be pretty quick, 5 s limit
 # =============================================================================
-def test_receipt():
-    '''
-    Add an entry in the receipt
-    '''
-    data = KPF0()
-    data.receipt_add_entry('test', 'test_path', 'test', 'PASS')
-    assert(len(data.receipt) == 1)
-
-# =============================================================================
-# AUXILIARY
-
-def test_aux():
-    '''
-    Create and then delete an auxiliary extension 
-    '''
-    data = KPF0()
-    data.create_extension('hello')
-    # At this point only one extenion should exist
-    assert(len(data.extension) == 1)
-    assert('hello' in data.extension.keys())
-    assert('hello' in data.header.keys())
-
-    data.del_extension('hello')
-    # No extension should exist
-    assert(len(data.extension) == 0)
-    assert('hello' not in data.header.keys())
-
-    # 
-
-def test_aux_exceptions():
-    '''
-    Test that proper exceptions are raised when invalid 
-    input are given
-    '''
-    data = KPF0()
-    data.create_extension('test1')
-
-    with pytest.raises(NameError):
-        # creating extension with duplicate name
-        data.create_extension('test1')
-    
-    with pytest.raises(KeyError):
-        # deleting a non-existent extension
-        data.del_extension('test2')
-    
-    with pytest.raises(KeyError):
-        # deleting a core HDU
-        data.del_extension('PRIMARY')
-
-# =============================================================================
-# IO
-# Level 0 path: 
-fpath = os.environ['KPFPIPE_TEST_DATA'] + '/NEIDdata/TAUCETI_20191217/L0'
-flist = [f for f in os.listdir(fpath)][0:1]
-    
-def test_from_NEID():
-    '''
-    Read all available level 0 data and check for data
-    '''
-    for f in flist:
-        data = KPF0.from_fits(os.path.join(fpath, f), 'NEID')
-
-        assert(isinstance(data.data, np.ndarray))
-        assert(data.variance.shape == data.data.shape)
-    
-def test_NEID2KPF():
-    '''
-    Check that data 
-    '''
-    # Make a temporary folder
-    try:
-        os.mkdir('temp_level0')
-    except FileExistsError:
-        pass
-
-    for f in flist:
-        # read NEID data and convert to KPF data
-        data = KPF0.from_fits(os.path.join(fpath, f), 'NEID')
-        to_path = 'temp_level0/' + f
-        data.to_fits(to_path)
-        # read the converted data
-        data2 = KPF0.from_fits(to_path, 'KPF')
-        # compare the data value of the two
-        assert(np.all(data2.data == data.data))
-        assert(np.all(data2.variance == data.variance))
-    # Clean up 
-    shutil.rmtree('temp_level0')
-
-    # import tempfile
-
-def test_exceptions():
-    
-    f = flist[0]
-    f_naught = f.split('.')[0] # same file without .fits file extension
-    data = KPF0.from_fits(os.path.join(fpath, f), 'NEID')
-    
-    with pytest.raises(IOError):
-        data2 = KPF0()
-        # Invalid file extension
-        data2.from_fits(os.path.join(fpath, f_naught), 'NEID')
-
-    with pytest.raises(IOError):
-        # overwriting without setting overwrite to True
-        data.read(os.path.join(fpath, f), 'NEID')
-        
-
+@execution_time_limit(time_limit=5)
+def test_level0_creation():
+    test_from_NEID()
 
 if __name__ == '__main__':
-    pass
+    test_level0_creation()
     
