205562018dedfe53b8b2ef6bf7ea5547a01f25cc
Abbysh <abbys@ipac.caltech.edu>
2020-09-29 10:57:28 -0400

    Small changes to alg.py docstring

--- modules/bias_subtraction/src/alg.py
+++ modules/bias_subtraction/src/alg.py
@@ -5,31 +5,50 @@ Created on Thu Sep  3 13:45:51 2020
 
 @author: paminabby
 """
-
 #packages
 from astropy.io import fits
 
 #Subtracting 2D array, function to subtract master bias frame from raw data image
 
 class BiasSubtraction:
-    """Args:
-    Attributes:
-    Raises: """
+    """
+    The BiasSubtraction class performs master bias frame subtraction from a raw science frame. 
+    Working on file input and export.
+    
+    """
 
     def __init__(self,data,config=None, logger=None):
-        """constructor"""
-         
-    def bias_subtraction(self,rawimage, masterbias):
-"""steps
-    For Bias Subtraction:
-        1. Reads in bias frame .fits data
-        2. Reads in raw .fits data
-        3. Checks whether both data arrays have same dimensions, prints "equal" or "not equal"
-        4. Subtracts bias array values from raw array values
-        5. Returns array of raw minus bias
-        
-        In pipeline terms: inputs two L0 files, outputs one L0 file"""
+        """[summary]
 
+        Args:
+            data ([type]): [description]
+            config ([type], optional): [description]. Defaults to None.
+            logger ([type], optional): [description]. Defaults to None.
+
+        Returns:
+            [type]: [description]
+        """
+    #use KPF0_from_fits?
+
+    def bias_subtraction(rawimage, masterbias):
+        """
+        Steps:
+            Reads in bias frame .fits data
+            Reads in raw science .fits data
+            Checks whether both data arrays have same dimensions, prints "equal" or "not equal"
+            Stacks, creates average of bias frames element-wise - final average results is master bias
+            Subtracts master bias array values from raw array values
+            Returns array of bias-corrected science frame
+        
+            In pipeline terms: inputs two L0 files, outputs one L0 file
+    
+        Args:
+            rawimage (str): The string to the raw science frame .fits file
+            masterbias (str): The string to master bias .fits file - the result of combining/averaging several bias frames
+
+        Returns:
+            raw_bcorrect (array):
+        """
         biasdata = fits.getdata(masterbias, ext=0)
         rawdata = fits.getdata(rawimage, ext=0)
     #check to see if both matrices have the same dimensions, Cindy's recommendation
@@ -37,7 +56,6 @@ class BiasSubtraction:
             print ("Bias .fits Dimensions Equal, Check Passed")
         if biasdata.shape!=rawdata.shape:
             print ("Bias .fits Dimensions NOT Equal! Check Failed")
-        raw_minus_bias=rawdata-biasdata
-        return raw_minus_bias
-
-        """ "add save of raw minus bias file as .fits """
+        raw_bcorrect=rawdata-biasdata
+    #to_fits ?
+        return raw_bcorrect
