e51a4962d81c77e1a744d0d3585c1139bee1355c
Abbysh <abbys@ipac.caltech.edu>
2020-10-02 20:15:17 -0400

    Added (test) bias recipe, updates to bias_subtraction.py, alg.py, and frame combining primitive

--- modules/Utils/frame_combine.py
+++ modules/Utils/frame_combine.py
@@ -16,16 +16,26 @@ Frame combine Steps:
 class FrameCombinePrimitive(KPF0_Primitive):
     def __init__(self, action, context):
         KPF0_Primitive.__init__(self, action, context)
-        self.L0_objects=self.action.arg[0]
+        self.L0_names=self.action.args[0]
+        self.data_type=self.action.args[1]
 
     def _perform(self):
         #loop here through L0 objects
         arrays_list=[]
-        for obj in self.L0_objects:
+        self.logger.info(f'L0_names: {self.L0_names}')
+        for name in self.L0_names:
+            obj=KPF0.from_fits(name,self.data_type)
             arrays_list.append(obj.data)
+            self.logger.info(f'file: {name}, obj.data_type is {type(obj.data)}')
         data=np.dstack(arrays_list)
         #create level0 object, assign master_frame
         master_frame = KPF0()
         #assuming all data will be 2D arrays
         master_frame.data=np.mean(data,2)
+
+        master_frame.receipt_add_entry('frame_combine', self.__module__, f'input_files={self.L0_names}', 'PASS')
+        if self.logger:
+            self.logger.info("frame_combine: Receipt written")
+
+        print (f'master_frame_type:{type(master_frame)}')
         return Arguments(master_frame)
--- modules/bias_subtraction/src/bias_subtraction.py
+++ modules/bias_subtraction/src/bias_subtraction.py
@@ -14,22 +14,24 @@ from keckdrpframework.models.processing_context import ProcessingContext
 
 # Local dependencies
 from modules.bias_subtraction.src.alg import BiasSubtraction
-from modules.utils.frame_combine import frame_combine
+#from modules.utils.frame_combine import frame_combine
 
 # Global read-only variables
 DEFAULT_CFG_PATH = 'modules/bias_subtraction/configs/default.cfg'
 
 class BiasSubtraction(KPF0_Primitive):
+
     def __init__(self, action:Action, context:ProcessingContext) -> None:
 
         #Initialize parent class
         KPF0_Primitive.__init__(self,action,context)
         self.logger=start_logger(self.__class__.__name__, config_path)
 
-        #Input argument
-        #self.input=action.args[0]
-        self.rawdata=self.input.data
-        
+        #Input arguments
+        self.rawdata=self.action.args[0]
+        self.masterbias=self.action.args[1]
+        self.data_type=self.action.args[2]
+
         #Input configuration
         self.config=configparser.ConfigParser()
         try:
@@ -48,23 +50,19 @@ class BiasSubtraction(KPF0_Primitive):
         self.alg=BiasSubtraction(self.rawimage,self.masterbias,config=self.config,logger=self.logger)
 
         #Preconditions
-        """
-        Potential preconditions:
-            Make sure can find a master bias? If not, could be a problem with frame combination fxn
-            Checking that master bias frame and raw frame have same dimensions as each other?
-        """
+
         #Postconditions
         
         #Perform - primitive's action
     def _perform(self) -> None:
 
         # 1) stack bias files using util fxn, creates master bias
-        if self.logger:
-            self.logger.info("Bias Subtraction: creating master bias...")
-        masterbias_data=frame_combine(biases_data)
+        #if self.logger:
+            #self.logger.info("Bias Subtraction: creating master bias...")
+        #masterbias_data=frame_combine(biases_data)
 
         # 2) subtract master bias from raw
         if self.logger:
             self.logger.info("Bias Subtraction: subtracting master bias from raw image...")
-        bias_corrected_raw=self.alg.bias_subtraction(rawdata,masterbias_data)
+        bias_corrected_raw=self.alg.bias_subtraction(self.rawdata,self.masterbias)
 
