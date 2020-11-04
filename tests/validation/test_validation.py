72f4b293adda7281abe23e3cdfd0875e3bcee440
BJ Fulton <bjfultn@gmail.com>
2020-11-04 13:14:17 -0800

    rename validation tests

--- tests/validation/test_validation.py
+++ tests/validation/test_validation.py
@@ -12,14 +12,15 @@ load_dotenv()
 # corresponding reference data to compare.
 # importing test files
 test_bias = os.getenv('KPFPIPE_TEST_DATA')+ '/NEIDdata/BIAS/neidTemp_Bias20190325.fits'
-test_raw= os.getenv('KPFPIPE_TEST_DATA') + '/NEIDdata/TAUCETI_20191217/L0/neidTemp_2D20191217T023129.fits' 
+test_raw = os.getenv('KPFPIPE_TEST_DATA') + '/NEIDdata/TAUCETI_20191217/L0/neidTemp_2D20191217T023129.fits' 
 
 # defining bias subtraction function
 # gets data from fits files, subtracts bias array values from raw array values
-def test_bias_subtraction(rawimage, masterbias):
+def test_bias_subtraction(test_raw=os.getenv('KPFPIPE_TEST_DATA') + '/NEIDdata/TAUCETI_20191217/L0/neidTemp_2D20191217T023129.fits', 
+                          test_bias=os.getenv('KPFPIPE_TEST_DATA')+ '/NEIDdata/BIAS/neidTemp_Bias20190325.fits'):
     
-    biasdata = fits.getdata(masterbias, ext=0)
-    rawdata = fits.getdata(rawimage, ext=0)
+    biasdata = fits.getdata(test_bias, ext=0)
+    rawdata = fits.getdata(test_raw, ext=0)
     #add check to see if both matrices have the same dimensions, Cindy's recommendation
     if biasdata.shape==rawdata.shape:
         print (".Fits Dimensions Equal, Check Passed")
