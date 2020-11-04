51eaff54fd15edeb9877a446e3b213348d0503dd
BJ Fulton <bjfultn@gmail.com>
2020-11-04 11:53:57 -0800

    Adding dummy test

---
+++ tests/validation/validation_tests.py
@@ -0,0 +1,23 @@
+"""Scientific validation/verification tests go here."""
+
+
+# Tests below are dummy tests to test the test framework. We will need to construct a test and 
+# corresponding reference data to compare.
+# importing test files
+test_bias = os.getenv('KPFPIPE_TEST_DATA')+ '/NEIDdata/BIAS/neidTemp_Bias20190325.fits'
+test_raw= os.getenv('KPFPIPE_TEST_DATA') + '/NEIDdata/TAUCETI_20191217/L0/neidTemp_2D20191217T023129.fits' 
+
+# defining bias subtraction function
+# gets data from fits files, subtracts bias array values from raw array values
+def test_bias_subtraction(rawimage, masterbias):
+    
+    biasdata = fits.getdata(masterbias, ext=0)
+    rawdata = fits.getdata(rawimage, ext=0)
+    #add check to see if both matrices have the same dimensions, Cindy's recommendation
+    if biasdata.shape==rawdata.shape:
+        print (".Fits Dimensions Equal, Check Passed")
+    if biasdata.shape!=rawdata.shape:
+        print (".Fits Dimensions NOT Equal! Check Failed")
+    raw_minus_bias = rawdata - biasdata
+
+    return raw_minus_bias
