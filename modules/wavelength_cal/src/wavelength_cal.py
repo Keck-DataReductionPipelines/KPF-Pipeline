685df61d91ff632a5126b4a5f7d5b4c855cf012d
Abbysh <abbys@ipac.caltech.edu>
2020-11-09 12:22:21 -0500

    Preliminary LFC code through gaussian fitting step, with documentation

--- modules/wavelength_cal/src/wavelength_cal.py
+++ modules/wavelength_cal/src/wavelength_cal.py
@@ -13,7 +13,71 @@ from keckdrpframework.models.arguments import Arguments
 from keckdrpframework.models.processing_context import ProcessingContext
 
 # Local dependencies
-from modules.wavelength_cal.src.alg import WavelengthCal
+from modules.wavelength_cal.src.alg import peak_detect
+from modules.wavelength_cal.src.alg import approx_fit
 
-# Global read-only variables
 DEFAULT_CFG_PATH = 'modules/wavelength_cal/configs/default.cfg'
+
+class WaveCalibrate(KPF0_Primitive):
+    """
+    This module defines class 'WaveCalibrate,' which inherits from KPF0_Primitive and provides methods
+    to perform the event 'LFC wavelength calibration' in the recipe.
+
+    Args:
+        KPF0_Primitive: Parent class
+        action (keckdrpframework.models.action.Action): Contains positional arguments and keyword arguments passed by the `LFCWaveCalibration` event issued in recipe.
+        context (keckdrpframework.models.processing_context.ProcessingContext): Contains path of config file defined for `wavelength_cal` module in master config file associated with recipe.
+
+    Attributes:
+        config_path (str):
+        config (configparser.ConfigParser): Config context.
+        logger (logging.Logger): Instance of logging.Logger
+        alg (modules.wavelength_cal.src.alg.LFCWaveCalibration): Instance of `LFCWaveCalibration,` which has operation codes for LFC Wavelength Calibration.
+    """
+    def __init__(self, 
+                action:Action, 
+                context:ProcessingContext) -> None:
+        """
+        WaveCalibrate constructor.
+
+        Args:
+            action (Action): [description]
+            context (ProcessingContext): [description]
+        """
+        #Initialize parent class
+        KPF0_Primitive.__init__(self,action,context)
+
+        #Input arguments
+
+        #Input configuration
+        self.config=configparser.ConfigParser()
+        try:
+            self.config_path=context.config_path['wavelength_cal']
+        except:
+            self.config_path = DEFAULT_CFG_PATH
+        self.config.read(self.config_path)
+
+        #Start logger
+        self.logger=None
+        #self.logger=start_logger(self.__class__.__name__,config_path)
+        if not self.logger:
+            self.logger=self.context.logger
+        self.logger.info('Loading config from: {}'.format(self.config_path))
+
+
+        #Algorithm setup
+
+        self.alg=LFCWaveCalibration(self.LFCData,self.row,self.config,self.logger)
+
+        #Preconditions
+       
+        #Postconditions
+        
+    #Perform - primitive's action
+    def _perform(self) -> None:
+        """Primitive action - 
+        Performs wavelength calibration by calling method '' from LFCWaveCalibration.
+
+        Returns:
+        """
+
