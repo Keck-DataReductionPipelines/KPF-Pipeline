4b0c8e009e6bd2a1bb1096b681f0bee233553579
Abbysh <abbys@ipac.caltech.edu>
2020-12-09 10:01:20 -0800

    alg.py and wavelength_cal.py so far- currently working to understand arpita's wave cal basics code

--- modules/wavelength_cal/src/alg.py
+++ modules/wavelength_cal/src/alg.py
@@ -3,7 +3,11 @@ from astropy.io import fits
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks as peak
 from scipy.optimize import curve_fit as cv
-from lmfit.models import GaussianModel #fit type can be changed
+#from lmfit.models import GaussianModel #fit type can be changed
+#uses _find_peaks, gaussfit3, gaussval2 from PyReduce
+
+from kpfpipe.models.level0 import KPF0
+from keckdrpframework.models.arguments import Arguments
 
 class LFCWaveCalibration:
     """
@@ -14,63 +18,137 @@ class LFCWaveCalibration:
    Args:
         config (configparser.ConfigParser, optional): Config context. Defaults to None.
         logger (logging.Logger, optional): Instance of logging.Logger. Defaults to None.
-        LFcomb (array): FITS laser frequency comb data
+        LFCData (array): FITS laser frequency comb data
 
     Attributes:
         logger (logging.Logger): Instance of logging.Logger.
 
     Raises:
+        
 
     """
-    def __init__(self, LFCData, row, config=None, logger=None): #maybe add row as well
+    def __init__(self, LFCData, f0, f_rep,config=None, logger=None): #maybe add row as well
         """
-        Inits LFCWaveCalibration class with LFC data, row, config, logger.
+        Inits LFCWaveCalibration class with LFC data, config, logger.
 
         Args:
             LFCData (np.ndarray): The FITS LFCData
-            row (integer): Row of FITS data to examine.
             config (configparser.ConfigParser, optional): Config context. Defaults to None.
             logger (logging.Lobber, optional): Instance of logging.Logger. Defaults to None.
         """
         self.LFCData=LFCData
+        self.f0=f0
+        self.f_rep=f_rep
         self.config=config
         self.logger=logger
-        self.row=row
 
     #file loaded in
+    #load in specifically calflux?
 
     def peak_detect(self):
-        """Finds peaks given specific row of data and various thresholds peaks must surpass. 
-
-        Returns:
-            peakxcoords: X-coordinates of found peaks
-            peakxhts: Y-coordinates (heights) of found peaks
-        """
-        peakxcoords,properties=peak(calflux[self.row],distance=10,threshold=50,height=0) #row of data, tester uses calflux[40]
-        peakhts=properties["peak_heights"]
-        return peakxcoords,peakhts
-
-    def approx_fit(self,peakxs,peakys): #currently gaussian
-        """Approximates gaussians to peaks.
+        #algorithm from PyReduce
+        #PyReduce notes:
+            # Find peaks in the data spectrum
+            # Run find_peak twice
+            # once to find the average distance between peaks
+            # once for real (disregarding close peaks)
+        """Detects peaks in LFC Data. First runs peak finding to find average distance between peaks, then a second to 
+        find peak x and y-coordinates, disregarding close-together peaks. 
+        Then, fits peaks to Gaussian to get accurate peak positions. Takes i-th peak x-value as center of gaussian, subtracts and adds 
+        average width around it to get x data for gaussian fit. y data is simply the corresponding LFC data. Performs gaussian fitting 
+        on x and y data arrays to get coefficients.
 
         Args:
-            peakxs (array): X-coordinates of peaks
-            peakys (array): Y-coordinates of peaks (peak heights)
-        """
-        func=GaussianModel()
-        #choosing fit type
-        wavewid=np.mean(np.diff(peakxs))
-        #getting average difference between peaks, will change to iterate through soon
-        wavehalf=int(wavewid/2)
-        x=np.arange(0,int(wavewid),1)
-        #x components
-        for i in range(wavehalf,len(calflux[self.row]),wavehalf):
-            pars=func.guess(calflux[self.row][i-wavehalf:i+wavehalf],x=x)
-            out=func.fit(calflux[self.row][i-wavehalf:i+wavehalf],pars,x=x)
-            #iterating through every curve to get overlapping fit
+            LFCData (np.ndarray): The FITS LFCData
 
-    #     def poly_fit(self):
-    #     """ Fits polynomial to spectrum.
+        Returns:
+            n (np.int): Number of peaks found
+            new_peaks (np.ndarray): X-coordinate of each found peak
+            peakhts (np.ndarray): Y-coordinate of each found peak (peak height)
+        """
+        width = lfc_peak_width
+        height = np.ma.median(self.LFCdata)
+        peaks, _ = peak(self.LFCdata, height=height, width=width)
+        distance = np.median(np.diff(peaks)) // 4
+        peaks, props = peak(self.LFCdata, height=height, distance=distance, width=width)
+        peakhts=props["peak_heights"]
+
+        # Fit peaks with gaussian to get accurate position
+        new_peaks = peaks.astype(float)
+        width = np.mean(np.diff(peaks)) // 2
+        for j, p in enumerate(peaks):
+            idx = p + np.arange(-width, width + 1, 1)
+            idx = np.clip(idx, 0, len(data) - 1).astype(int)
+
+            coef= gauss_fit(np.arange(len(idx)), self.LFCdata[idx])
+            new_peaks[j] = coef[1] + p - width
+
+        n = np.arange(len(peaks))
+
+        return n, new_peaks, peakhts
+
+    def gauss_fit(x, y):
+        """ 
+        Simple gaussian fit: gauss = A * exp(-(x-mu)**2/(2*sig**2)) + offset
+
+        Args:
+            x (np.ndarray): array of shape (n,) of x data
+            y (np.ndarray): array of shape (n,) of y data
+
+        Returns:
+            popt (list) : list of shape (4,) including parameters A, mu, sigma**2, offset
+        """
+        x = np.ma.compressed(x)
+        y = np.ma.compressed(y)
+        gauss = gaussvalue
+        i = np.argmax(y[len(y) // 4 : len(y) * 3 // 4]) + len(y) // 4
+        p0 = [y[i], x[i], 1, np.min(y)]
+
+        with np.warnings.catch_warnings():
+            np.warnings.simplefilter("ignore")
+            popt, _ = curve_fit(gauss, x, y, p0=p0)
+
+        return popt
+
+    def gauss_value(x, a, mu, sig, const):
+        """Gaussian function.
+
+        Args:
+            x (np.ndarray): Peak x-coordinates
+            a (np.float): Constant, i-th value of peak y-coordinates (height of peak curve)
+            mu (np.float): Constant, i-th value of peak x-coordinates (peak position)
+            sig (np.int): Standard deviation
+            const (np.float): Offset
+
+        Returns:
+            a * np.exp(-((x - mu) ** 2) / (2 * sig)) + const []: Gauss function values
+        """
+        return a * np.exp(-((x - mu) ** 2) / (2 * sig)) + const
+
+    # def calc_LFC_modes(self):
+    #     #need f0 and f_rep fed in in recipe?
+    #     #c = speed of light
+    #     nlines=
+    #     fn=self.f0+(nlines*f_rep)
+    #     ln=c*1e13/fn
+
+
+    # def poly_fit(self,peakxs,peakys):
+    #     """Fits order to polynomial.
+
     #     Args:
+    #         peakxs (np.ndarray): X-coordinates of peaks
+    #         peakys (np.ndarray): Y-coordinates of peaks (peak heights)
+
+    #     Returns:
     #     """
+        
+    # def get(self):
+    #     """Returns LFC wavelength solution.
+
+    #     Returns:
+    #         wavesoln: LFC wavelength solution.
+    #     """
+    #     return wavesoln
+
 
