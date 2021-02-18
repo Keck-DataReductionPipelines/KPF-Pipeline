9742be5f9dfd1b3490ee2ba4cc0758e4cbd765a4
BJ Fulton <bjfultn@gmail.com>
2021-02-18 09:31:07 -0800

    set context.pipe_config=pipeline.config

--- kpfpipe/pipelines/kpfpipeline.py
+++ kpfpipe/pipelines/kpfpipeline.py
@@ -112,7 +112,7 @@ class KPFPipeline(BasePipeline):
 
         # Add useful attributes onto the self.context object
         self.context.arg = arg
-        # self.context.config = self.config
+        self.context.pipe_config = self.config
 
         self.logger.info('Finished initializing Pipeline')
 
