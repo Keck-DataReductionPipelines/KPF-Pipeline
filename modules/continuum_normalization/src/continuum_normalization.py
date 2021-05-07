f3619ac5c8039024114e4c09fafff7d0dd14d994
Abbysh <abbys@ipac.caltech.edu>
2021-05-07 14:29:23 -0400

    Added alphashape algorithm and continued to add to documentation. Algorithm still in progress

--- modules/continuum_normalization/src/continuum_normalization.py
+++ modules/continuum_normalization/src/continuum_normalization.py
@@ -1,13 +1,15 @@
 # Standard dependencies
 import configparser
 import numpy as np
+import pandas as pd
 
 # Pipeline dependencies
 from kpfpipe.logger import start_logger
-from kpfpipe.primitives.level0 import KPF0_Primitive
-from kpfpipe.models.level0 import KPF0
+from kpfpipe.primitives.level1 import KPF1_Primitive
+from kpfpipe.models.level1 import KPF1
 
 # External dependencies
+from modules.Utils.config_parser import ConfigHandler
 from keckdrpframework.models.action import Action
 from keckdrpframework.models.arguments import Arguments
 from keckdrpframework.models.processing_context import ProcessingContext
@@ -18,17 +20,17 @@ from modules.continuum_normalization.src.alg import ContinuumNorm
 # Global read-only variables
 DEFAULT_CFG_PATH = 'modules/continuum_normalization/configs/default.cfg'
 
-class ContNorm(KPF0_Primitive):
+class ContNorm(KPF1_Primitive):
     """This module defines class `ContNorm` which inherits from KPF0_Primitive and provides methods
     to perform the event `Continuum Normalization` in the recipe.
 
     Args:
-        KPF0_Primitive: Parent class.
+        KPF1_Primitive: Parent class.
         action (keckdrpframework.models.action.Action): Contains positional arguments and keyword arguments passed by the `ContinuumNormalization` event issued in recipe.
         context (keckdrpframework.models.processing_context.ProcessingContext): Contains path of config file defined for `continuum_normalization` module in master config file associated with recipe.
 
     Attributes:
-        l0_obj (kpfpipe.models.level1.KPF1): Instance of `KPF0`, assigned by `actions.args[0]`
+        l1_obj (kpfpipe.models.level1.KPF1): Instance of `KPF1`, assigned by `actions.args[0]`
         config (configparser.ConfigParser): Config context.
         logger (logging.Logger): Instance of logging.Logger
         alg (modules.wavelength_cal.src.alg.LFCWaveCalibration): Instance of `LFCWaveCalibration,` which has operation codes for LFC Wavelength Calibration.
@@ -44,16 +46,16 @@ class ContNorm(KPF0_Primitive):
         Args:
             action (Action): Contains positional arguments and keyword arguments passed by the `ContinuumNormalization` event issued in recipe:
               
-                `action.args[0] (kpfpipe.models.level0.KPF0)`: Instance of `KPF0` containing level 0 spectrum
+                `action.args[0] (kpfpipe.models.level1.KPF1)`: Instance of `KPF1` containing level 1 spectrum
 
             context (ProcessingContext): Contains path of config file defined for `continuum_normalization` module in master config file associated with recipe.
 
         """
         #Initialize parent class
-        KPF0_Primitive.__init__(self,action,context)
+        KPF1_Primitive.__init__(self,action,context)
 
         #input recipe arguments
-        self.l0_obj=self.action.args[0]
+        self.l1_obj=self.action.args[0]
 
         #Input configuration
         self.config=configparser.ConfigParser()
@@ -62,6 +64,13 @@ class ContNorm(KPF0_Primitive):
         except:
             self.config_path = DEFAULT_CFG_PATH
         self.config.read(self.config_path)
+
+        ### check formatting
+        configpull=ConfigHandler(config,'PARAM')
+        self.run_cont_norm = configpull.get_config_value('run_cont_norm', True)
+        self.cont_norm_poly = configpull.get_config_value('cont_norm_poly', True)
+        self.cont_norm_alpha = configpull.get_config_value('cont_norm_alpha', True)
+        ###
 
         #Start logger
         self.logger=None
@@ -84,14 +93,38 @@ class ContNorm(KPF0_Primitive):
         Performs continuum normalization by calling on ContinuumNorm in alg.
 
         Returns:
+            poly_norm_spec: Polynomial method normalized spectrum
+            poly_yfit: Y-values of fitted polynomial from polynomial method 
+            afs_norm_spec: Alphashape method normalized spectrum
+            afs_yfit: Y-values of fitted curve from alphashape method
 
         """
-        if self.logger:
-            self.logger.info("Continuum Normalization: Extracting spectrum data")
-        raw_spectrum=self.l0_obj.data['SCI'][0,:,:] #correct extension?
 
         if self.logger:
-            self.logger.info("Continuum Normalization: Polynomial fitting")
-        polyfit_cn= self.alg.flatspec(raw_spectrum,)
+            self.logger.info("Continuum Normalization: Extracting wavelength and flux data")
 
+        flux=self.l1_obj.data['SCI'][0,:,:] #correct extension for sciflux?
+        wav=self.l1_obj.data['SCI'][1,:,:] #correct extension for sciwave?
+        dataframe = pd.DataFrame({'wav': np.array(wav,'d'), 'flux': np.array(flux,'d')}, columns=['wav','flux'])
 
+        if self.run_cont_norm == False:
+            if self.logger:
+                self.logger.info("Continuumm Normalization: Module skipped")
+            result = flux
+
+        if self.cont_norm_poly == True & self.cont_norm_alpha == True:
+            if self.logger:
+                self.logger.info("Continuum Normalization: Running both polynomial fitting and alphashape fitting")
+            poly_norm_spec, poly_yfit, afs_norm_spec, afs_yfit = self.alg.run_all_cont_norm(flux,dataframe)
+
+        if self.cont_norm_poly == False & self.cont_norm_alpha == True:
+            if self.logger:
+                self.logger.info("Continuum Normalization: Running alphashape fitting")
+            afs_norm_spec, afs_yfit = self.alg.run_all_cont_norm(flux,dataframe)
+
+        if self.cont_norm_poly == True & self.cont_norm_alpha == False:
+            if self.logger:
+                self.logger.info("Continuum Normalization: Running polynomial fitting")
+            poly_norm_spec, poly_yfit = self.alg.run_all_cont_norm(flux,dataframe)
+
+#could do something so just one run statement, then "if shape of result = x, result = afs"?
