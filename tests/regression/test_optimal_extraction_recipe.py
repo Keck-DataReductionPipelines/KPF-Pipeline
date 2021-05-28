b453e35efca9424e2950581baf61433817a7bc01
cwang2016 <cwang@ipac.caltech.edu>
2021-05-28 14:06:12 -0700

    feature/order_rectification_primitive add test on OrderRectification primitive into the regression test for the recipe.

--- tests/regression/test_optimal_extraction_recipe.py
+++ tests/regression/test_optimal_extraction_recipe.py
@@ -39,6 +39,41 @@ for code in obs_list:
                                                 start_order=start_result_order, wavecal_fits=input_lev1_file)
 """
 
+rectification_neid_recipe = """# test recipe for rectification on NEID data
+from modules.optimal_extraction.src.order_rectification import OrderRectification
+
+test_data_dir = KPFPIPE_TEST_DATA
+flat_data_dir = test_data_dir + config.ARGUMENT.flat_data_dir
+data_type = config.ARGUMENT.data_type
+output_dir = config.ARGUMENT.output_dir
+input_lev0_prefix = config.ARGUMENT.input_lev0_file_prefix
+obs_list = config.ARGUMENT.obs_list
+max_result_order = 2
+start_result_order = 20
+rect_method = config.ARGUMENT.rectification_method
+order_name = config.ARGUMENT.order_name
+
+lev0_flat_file = flat_data_dir + 'stacked_2fiber_flat_L0.fits'
+
+lev0_flat_data = kpf0_from_fits(lev0_flat_file, data_type='KPF')
+_, short_lev0_flat = split(lev0_flat_file)
+lev0_flat_stem, lev0_flat_ext = splitext(short_lev0_flat)
+
+lev0_flat_rect = OrderRectification(None, lev0_flat_data, order_name=order_name,
+                                    rectification_method=rect_method, max_result_order=max_result_order,
+                                    start_order=start_result_order)
+result = to_fits(lev0_flat_rect, output_dir+'flat_rect.fits')
+for code in obs_list:
+    input_lev0_file = test_data_dir + input_lev0_prefix + code + '.fits'
+    _, short_lev0_file = split(input_lev0_file)
+    lev0_stem, lev0_ext = splitext(short_lev0_file)
+    lev0_data = kpf0_from_fits(input_lev0_file, data_type=data_type)
+    output_lev0_file = output_dir + 'spectrum_rect.fits'
+    lev0_data_rect = OrderRectification(lev0_data, lev0_flat_rect, None, order_name=order_name,
+                            rectification_method=rect_method, max_result_order=max_result_order,
+                            start_order=start_result_order)
+    result=to_fits(lev0_data_rect, output_lev0_file)        
+"""
 optimal_extraction_neid_config = "examples/default_recipe_test_neid.cfg"
 
 
@@ -46,3 +81,5 @@ def test_recipe_optimal_extraction_neid():
     recipe_test(optimal_extraction_neid_recipe, optimal_extraction_neid_config)
 
 
+def test_recipe_rectification_neid():
+    recipe_test(rectification_neid_recipe, optimal_extraction_neid_config)
