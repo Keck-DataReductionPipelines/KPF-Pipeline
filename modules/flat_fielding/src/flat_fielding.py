b3840f2d17904dbf627717daf1c56c86d41152eb
Abbysh <abbys@ipac.caltech.edu>
2021-06-23 13:59:06 -0700

    changes to flat fielding module. It and bias subtraction have very similar structures, but differ most in their recipes. Bias subtraction recipe does overscan subtraction/FFI-formation on all L0 files, while flat fielding will simply use that output

--- modules/flat_fielding/src/flat_fielding.py
+++ modules/flat_fielding/src/flat_fielding.py
@@ -13,7 +13,7 @@ from keckdrpframework.models.arguments import Arguments
 from keckdrpframework.models.processing_context import ProcessingContext
 
 # Local dependencies
-from modules.flat_fielding.src.alg import FlatFielding
+from modules.flat_fielding.src.alg import FlatFieldingAlg
 #from modules.utils.frame_combine import frame_combine
 
 # Global read-only variables
@@ -58,11 +58,11 @@ class FlatFielding(KPF0_Primitive):
         #Initialize parent class
         KPF0_Primitive.__init__(self,action,context)
         
-        #Input argument
-        #self.input=action.args[0]
-        self.rawdata=self.action.args[0]
+              #Input arguments
+        self.raw_file=self.action.args[0]
         self.masterflat=self.action.args[1]
-        self.data_type=self.action.args[2]
+        self.ffi_exts=self.action.args[2]
+        self.data_type=self.action.args[3]
         
         #Input configuration
         self.config=configparser.ConfigParser()
@@ -81,7 +81,7 @@ class FlatFielding(KPF0_Primitive):
         #Flat Fielding algorithm setup
 
         #Option 1
-        self.alg=FlatFielding(self.rawdata,config=self.config,logger=self.logger)
+        self.alg=FlatFieldingAlg(self.raw_file,self.ffi_exts,config=self.config,logger=self.logger)
 
         #Preconditions
         
