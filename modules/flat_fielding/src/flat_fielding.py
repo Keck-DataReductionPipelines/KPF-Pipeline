d7540355dceb1e49f4f67664460cc8ec6d357492
Abbysh <abbys@ipac.caltech.edu>
2021-10-05 07:55:32 -0700

    further end-to-end testing

--- modules/flat_fielding/src/flat_fielding.py
+++ modules/flat_fielding/src/flat_fielding.py
@@ -97,9 +97,7 @@ class FlatFielding(KPF0_Primitive):
         Returns:
             Arguments object(np.ndarray): Level 0, flat-corrected, raw observation data
         """
-        print('pumpkin', self.masterflat)
         #self.masterflat = fits.open(self.masterflat)
-
         #Option 1:
         if self.logger:
             self.logger.info("Flat-fielding: dividing raw image by master flat")
