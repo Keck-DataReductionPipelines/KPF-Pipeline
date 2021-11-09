93a13f8240df7afc73aa93e822a20dadcd1e079a
Howard Isaacson <hisaacson@berkeley.edu>
2021-11-09 14:33:03 -0800

    Initial structure copied from bias module

---
+++ modules/line_activity/README.md
@@ -0,0 +1,4 @@
+Description: 
+
+   Uses L1 data and extracts spectral segments for use in activity analysis.
+   
---
+++ modules/line_activity/src/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  3 14:05:46 2020
+
+@author: paminabby
+"""
+
