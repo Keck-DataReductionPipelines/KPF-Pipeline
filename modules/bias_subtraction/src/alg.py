97a4ddd1ecd1d90d367093fdf5c2d322403e6648
Abbysh <abbys@ipac.caltech.edu>
2021-11-18 09:20:37 -0500

    Requested changes + removal of extraneous prints

--- modules/bias_subtraction/src/alg.py
+++ modules/bias_subtraction/src/alg.py
@@ -1,9 +1,6 @@
 #packages
 import numpy as np
 import matplotlib.pyplot as plt
-from astropy.io import fits
-###
-
 from modules.Utils.config_parser import ConfigHandler
 from kpfpipe.models.level0 import KPF0
 from keckdrpframework.models.arguments import Arguments
@@ -13,26 +10,27 @@ class BiasSubtractionAlg:
     Bias subtraction calculation.
 
     This module defines 'BiasSubtraction' and methods to perform bias subtraction by subtracting a master bias frame from the raw data frame.  
-
-    Args:
-        rawimage (np.ndarray): The FITS raw data with image extensions
-        config (configparser.ConfigParser): Config context.
-        logger (logging.Logger): Instance of logging.Logger.
     
     Attributes:
         rawimage (np.ndarray): From parameter 'rawimage'.
+        ffi_exts (list): From parameter 'ffi_exts'.
+        quicklook (bool): From parameter 'quicklook'.
+        data_type (str): From parameter 'data_type'.
+        config (configparser.ConfigParser, optional): From parameter 'config'.
+        logger (logging.Logger, optional): From parameter 'logger'.
     
     Raises:
-        Exception: If raw image and bias frame don't have the same dimensions
+        Exception: If raw image and bias frame don't have the same dimensions.
     """
-
 
     def __init__(self,rawimage,ffi_exts,quicklook,data_type,config=None,logger=None):
         """Inits BiasSubtraction class with raw data, config, logger.
 
         Args:
             rawimage (np.ndarray): The FITS raw data.
-            ffi_exts (np.ndarray): The extensions in L0 FITS files where FFIs (full frame images) are stored.
+            ffi_exts (list): The extensions in L0 FITS files where FFIs (full frame images) are stored.
+            quicklook (bool): If true, quicklook pipeline version of bias subtraction is run, outputting information and plots.
+            data_type (str): Instrument name, currently choice between KPF and NEID.
             config (configparser.ConfigParser, optional): Config context. Defaults to None.
             logger (logging.Logger, optional): Instance of logging.Logger. Defaults to None.
         """
@@ -42,7 +40,6 @@ class BiasSubtractionAlg:
         self.data_type=data_type
         self.config=config
         self.logger=logger
-        #self.imagesize=
         
     def bias_subtraction(self,masterbias):
         """Subtracts bias data from raw data.
@@ -54,45 +51,27 @@ class BiasSubtractionAlg:
         Raises:
             Exception: If raw image and bias frame don't have the same dimensions.
         """
-        ###for testing purposes###
-        #masterbias = fits.open(masterbias)
-        #masterbias = masterbias[1].data
-        # masterbias = np.zeros_like(frame)
-        ###
         if self.quicklook == False: 
             if self.data_type == 'KPF':
                 for ffi in self.ffi_exts:
                     print(self.rawimage.info)
                     print(masterbias.info())
-                    if self.rawimage[ffi].data.shape==masterbias[ffi].data.shape:
-                        print ("Bias .fits Dimensions Equal, Check Passed")
-                    else:
-                        raise Exception ("Bias .fits Dimensions NOT Equal! Check failed")
-
+                    assert self.rawimage[ffi].data.shape==masterbias[ffi].data.shape, "Bias .fits Dimensions NOT Equal! Check failed"
                     self.rawimage[ffi].data=self.rawimage[ffi].data-masterbias[ffi].data
-                #ext no+1 for mflat because there is a primary ext coded into the masterflat currently
                 
             if self.data_type == 'NEID':
                 print(self.rawimage.info())
                 print('shapes:',self.rawimage['DATA'].shape,masterbias['DATA'].shape)
-                if self.rawimage['DATA'].shape==masterbias['DATA'].shape:
-                    print ("Bias .fits Dimensions Equal, Check Passed")
-                else:
-                    raise Exception ("Bias .fits Dimensions NOT Equal! Check failed")
+                assert self.rawimage['DATA'].shape==masterbias['DATA'].shape, "Bias .fits Dimensions NOT Equal! Check failed"
                 self.rawimage['DATA']=self.rawimage['DATA']-masterbias['DATA']
-
 
         if self.quicklook == True:
             for no,ffi in enumerate(self.ffi_exts):
                 print('shapes:',self.rawimage[ffi].data.shape,masterbias[ffi].data.shape)
-                #until data model for master files is added:
-                if self.rawimage[ffi].shape==masterbias[ffi].data.shape:
-                    print ("Bias .fits Dimensions Equal, Check Passed")
-                else:
-                    raise Exception ("Bias .fits Dimensions NOT Equal! Check failed")
+                assert self.rawimage[ffi].data.shape==masterbias[ffi].data.shape, "Bias .fits Dimensions NOT Equal! Check failed"
                 self.rawimage[ffi].data=self.rawimage[ffi].data-masterbias[ffi].data
 
-                counts = masterbias[no+1].data #red and green potentially masters, no+1 means ignoring primary?
+                counts = masterbias[ffi].data 
                 flatten_counts = np.ravel(counts)
                 low, high = np.percentile(flatten_counts,[0.1,99.9])
                 counts[(counts>high) | (counts<low)] = np.nan #bad pixels
@@ -115,5 +94,6 @@ class BiasSubtractionAlg:
         """
         return self.rawimage
 
-        #raise flag when counts are significantly diff from master bias
-        #identify bad pixels
+        
+#quicklook TODO: raise flag when counts are significantly diff from master bias, identify bad pixels
+        
