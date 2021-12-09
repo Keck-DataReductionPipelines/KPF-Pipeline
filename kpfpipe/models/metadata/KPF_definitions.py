4548043b88556d70b7f87cefc94db858a8e9561c
BJ Fulton <bjfultn@gmail.com>
2021-12-09 09:30:28 -0800

    image ca hk extensions

--- kpfpipe/models/metadata/KPF_definitions.py
+++ kpfpipe/models/metadata/KPF_definitions.py
@@ -89,7 +89,8 @@ LEVEL1_EXTENSIONS = {'PRIMARY': fits.PrimaryHDU,
                      'RED_TELLURIC': fits.BinTableHDU,
                      'RED_SKY': fits.BinTableHDU,
 
-                     'CA_HK': fits.BinTableHDU,
+                     'CA_HK_FLUX': fits.ImageHDU,
+                     'CA_HK_WAVE': fits.ImageHDU,
                     }
 # KPF level 2 extensions should be defined here
 # as a dictionary with the name of the extensions as keys
