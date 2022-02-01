598097a0c6a7f3427f9c278ba32ddb5a08212406
Sarah Blunt <sblunt@caltech.edu>
2022-02-01 15:46:54 -0800

    both LFC recipes working

--- examples/sarah_wavecal_tests/plot_LFC_results.py
+++ examples/sarah_wavecal_tests/plot_LFC_results.py
@@ -2,10 +2,19 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 import numpy as np
 
-filename = 'neidL1_20220127T135910'
+prev_wls_sol = 'no' # 'yes'/'no'; if 'yes', plot results for LFC run w/o previous knowledge of wls/pixels
+
+assert prev_wls_sol in ['yes', 'no'], 'prev_wls_sol toggle must be either "yes" or "no"'
+
+if prev_wls_sol == 'yes':
+    filename = 'neidL1_20220130T135641'
+else:
+    filename = 'neidL1_20220127T135910'
+
+output_path = '/data/KPF-Pipeline-TestData/DRP_V2_Testing/NEID-cals/output_wavecal/LFC_{}prev'.format(prev_wls_sol)
 
 # read in our solution
-our_file = fits.open('/code/KPF-Pipeline/outputs/{}_L1_wave.fits'.format(filename))
+our_file = fits.open('{}/{}_L1_wave.fits'.format(output_path, filename))
 our_wls = our_file['CALWAVE'].data
 
 # read in NEID team solution
@@ -13,7 +22,8 @@ neid_file = fits.open('/data/KPF-Pipeline-TestData/DRP_V2_Testing/NEID-cals/{}.f
 neid_wls = neid_file['CALWAVE'].data
 
 # make a difference plot
-fig, ax = plt.subplots(2, 1, figsize=(15,5))
+fig, ax = plt.subplots(2, 1, figsize=(15,7))
+plt.subplots_adjust(hspace=0)
 for i in np.arange(len(our_wls)):
     if np.median(our_wls[i]) > 0:
         ax[0].plot(our_wls[i] - neid_wls[i], color='grey', alpha=0.5)
@@ -21,9 +31,7 @@ for i in np.arange(len(our_wls)):
         ax[1].plot((our_wls[i][:-1] - neid_wls[i][:-1]) / pixel_size, color='grey', alpha=0.5)
 
 ax[1].set_xlabel('pixel')
-ax[0].set_xlabel('pixel')
-ax[0].set_ylabel('Our WLS - NEID Team WLS [ang]')
-ax[1].set_ylabel('Our WLS - NEID Team WLS [pix]')
+ax[0].set_ylabel('Our WLS - NEID WLS [$\\rm \AA$]')
+ax[1].set_ylabel('Our WLS - NEID WLS [pix]')
 ax[1].set_ylim(-.15, .15)
-plt.tight_layout()
-plt.savefig('/code/KPF-Pipeline/outputs/LFC_vs_NEID.png', dpi=250)
+plt.savefig('{}/LFC_vs_NEID.png'.format(output_path), dpi=250)
