b61327b4cbe8794b686cbbadac69e875e42766cf
cwang2016 <cwang@ipac.caltech.edu>
2022-06-22 14:30:12 -0700

    update DRP_Event materials and change simple.recipe back to modules.Trivial.KPFM_Trivial

--- events/DRP_Event/KPFM_Trivial.py
+++ events/DRP_Event/KPFM_Trivial.py
@@ -1,13 +1,7 @@
-"""
-    The file contains primitive KPFModeExample
-
-    Attributes:
-       KPFModeExample: primitive for simmple recipe test
-
-
-"""
-
-from kpfpipe.primitives.core import KPF_Primitive
+# Pipeline dependencies
+from kpfpipe.logger import start_logger
+from kpfpipe.primitives.level1 import KPF1_Primitive
+from kpfpipe.models.level1 import KPF1
 
 # External dependencies
 from keckdrpframework.models.action import Action
@@ -15,27 +9,24 @@ from keckdrpframework.models.arguments import Arguments
 from keckdrpframework.models.processing_context import ProcessingContext
 
 
-class KPFModExample(KPF_Primitive):
-    def __init__(self,
+class KPFModExample(KPF1_Primitive):
+
+    def __init__(self, 
                  action: Action,
                  context: ProcessingContext) -> None:
-
+        '''
+        Example KPF module
+        '''
         # Initialize parent class
-        KPF_Primitive.__init__(self, action, context)
+        KPF1_Primitive.__init__(self, action, context)
+        # start a logger
+        self.logger = start_logger(self.__class__.__name__, None)
 
-        self.logger = self.context.logger
-
-    def _pre_condition(self) -> bool:
-        """
-        check if the extensions exist in the data model object
-        """
-        return True
-
-    def _post_condition(self) -> bool:
-        return True
-
-    def _perform(self):
-        result = 'KPFModeExample done'
+    def _perform(self) -> None:
+        '''
+        This primitive's action
+        '''
         print('[{}] Performed!'.format(self.__class__.__name__))
+        result = "KPFModExample done"
         return Arguments(result)
 
