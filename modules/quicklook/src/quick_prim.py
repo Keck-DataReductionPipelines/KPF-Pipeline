02d35e9a56e117643870add4514d554d6cb8ad16
DaiFei <daifei@MacBook-Pro-2.local>
2022-07-08 12:54:33 -0700

    primitive0

--- modules/quicklook/src/quick_prim.py
+++ modules/quicklook/src/quick_prim.py
@@ -18,7 +18,7 @@ from modules.quicklook.src.alg import QuicklookAlg
 # Global read-only variables
 DEFAULT_CFG_PATH = 'modules/quicklook/configs/default.cfg'
 
-class Quicklook(KPF0_Primitive): 
+class Quicklook(KPF0_Primitive):
 
     def __init__(self,
                     action:Action,
@@ -27,9 +27,9 @@ class Quicklook(KPF0_Primitive):
         KPF0_Primitive.__init__(self,action,context)
 
         #Input arguments
-        #self.input_file=self.action.args[0]
-        self.file_name=self.action.args[0]
-        self.output_dir=self.action.args[1]
+        self.input_file=self.action.args[0]
+        self.file_name=self.action.args[1]
+        self.output_dir=self.action.args[2]
         # input configuration
         self.config = configparser.ConfigParser()
         try:
@@ -49,4 +49,4 @@ class Quicklook(KPF0_Primitive):
         self.alg=QuicklookAlg(config=self.config,logger=self.logger)
 
     def _perform(self) -> None:
-        self.alg.qlp_procedures(self.file_name,self.output_dir)
+        self.alg.qlp_procedures(self.input_file,self.file_name,self.output_dir)
