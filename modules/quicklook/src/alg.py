077a286db7dc5d1c0133726ba65310eea3150203
DaiFei <daifei@MacBook-Pro-2.local>
2022-07-08 14:33:55 -0700

    primitive0

--- modules/quicklook/src/alg.py
+++ modules/quicklook/src/alg.py
@@ -229,14 +229,14 @@ class QuicklookAlg:
             wav_red = np.array(hdulist1['RED_CAL_WAVE'].data,'d')
 
 
-
-
+        print(hdulist1.info())
+        print(np.shape(flux_green),np.shape(flux_red))
         flux_green = np.array(hdulist['GREEN_SCI_FLUX1'].data,'d')
         flux_red = np.array(hdulist['RED_SCI_FLUX1'].data,'d')#hdulist[40].data
 
         if np.shape(flux_green)==(0,):flux_green = wav_green*0.#place holder when there is no data
         if np.shape(flux_red)==(0,): flux_red = wav_red*0.#place holder when there is no data
-        print(np.shape(flux_green),np.shape(flux_red))
+
         wav = np.concatenate((wav_green,wav_red),axis = 0)
         flux = np.concatenate((flux_green,flux_red),axis = 0)
 
