e66b114c63824facff8eb285c3bddfd1b9f64631
DaiFei <daifei@MacBook-Pro-2.local>
2022-08-11 21:20:47 -0700

    order_trace

--- modules/quicklook/src/alg.py
+++ modules/quicklook/src/alg.py
@@ -166,7 +166,7 @@ class QuicklookAlg:
                 #print(order_trace.iloc[i]['X1'],int(order_trace.iloc[i]['X2']-order_trace.iloc[i]['X1']))
                 x_grid = np.linspace(order_trace.iloc[i]['X1'],order_trace.iloc[i]['X2'],int(order_trace.iloc[i]['X2']-order_trace.iloc[i]['X1']))
                 y_grid = order_trace.iloc[i]['Coeff0']+x_grid*order_trace.iloc[i]['Coeff1']+x_grid**2*order_trace.iloc[i]['Coeff2']+x_grid**3*order_trace.iloc[i]['Coeff3']
-                #plt.plot(x_grid,y_grid,color ='red',linewidth = 0.1)
+                plt.plot(x_grid,y_grid,color ='red',linewidth = 0.1)
                 plt.plot(x_grid,y_grid-order_trace.iloc[i]['BottomEdge'],color ='red',linewidth = 0.1,alpha = 0.5)
                 plt.plot(x_grid,y_grid+order_trace.iloc[i]['TopEdge'],color ='magenta',linewidth = 0.1,alpha = 0.5)
                 #plt.fill_between(x_grid,y_grid-order_trace.iloc[i]['BottomEdge'],y_grid+order_trace.iloc[i]['TopEdge'],color ='pink',alpha = 0.2)
