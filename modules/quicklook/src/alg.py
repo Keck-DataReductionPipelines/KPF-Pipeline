12b298da1fe33472b00f717927672f809dae0fa3
DaiFei <daifei@MacBook-Pro-2.local>
2022-08-12 15:20:21 -0700

    end_of_night

--- modules/quicklook/src/alg.py
+++ modules/quicklook/src/alg.py
@@ -74,8 +74,20 @@ class QuicklookAlg:
             '''
 
             #plot how the order trace changes
-            order_trace_list = glob.glob('/data/order_trace/*/*.csv', recursive = True)
-            print(order_trace_list)
+            order_trace_list = glob.glob('/data/order_trace/*/*_GREEN_CCD.csv', recursive = True)
+            for order_trace_file in order_trace_list:
+                order_trace = pd.read_csv(order_trace_file)
+                print(order_trace_file,order_trace)
+                for i in range(np.shape(order_trace)[0]):#[50]:#range(np.shape(order_trace)[0])
+
+                    x_grid = np.linspace(order_trace.iloc[i]['X1'],order_trace.iloc[i]['X2'],int(order_trace.iloc[i]['X2']-order_trace.iloc[i]['X1']))
+                    y_grid = order_trace.iloc[i]['Coeff0']+x_grid*order_trace.iloc[i]['Coeff1']+x_grid**2*order_trace.iloc[i]['Coeff2']+x_grid**3*order_trace.iloc[i]['Coeff3']
+                    plt.plot(x_grid,y_grid,color ='red',linewidth = 0.2)
+                    plt.plot(x_grid,y_grid-order_trace.iloc[i]['BottomEdge'],color ='white',linewidth = 0.2,alpha = 1)
+                    plt.plot(x_grid,y_grid+order_trace.iloc[i]['TopEdge'],color ='black',linewidth = 0.2,alpha = 1)
+            plt.xlim(3200,4000)
+            plt.ylim(3200,4000)
+            plt.savefig(output_dir+'fig/order_trace_evolution.pdf')
             return
         print('working on',date,exposure_name)
 
