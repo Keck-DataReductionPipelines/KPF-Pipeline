85b2db2556ac394b01407dc78aef2e940b73aa38
DaiFei <daifei@MacBook-Pro-2.local>
2022-08-11 21:48:05 -0700

    order_trace

--- modules/quicklook/src/alg.py
+++ modules/quicklook/src/alg.py
@@ -237,7 +237,7 @@ class QuicklookAlg:
             plt.title(ccd_color[i_color]+' '+version+' Column Cut Through Column '+str(which_column))#(Middle of CCD)
             plt.ylim(1,1.2*np.nanmax(counts[:,which_column]))
             plt.legend()
-            '''
+
             #show the order order_trace
             if version == 'Flat_All':
                  for i in range(np.shape(order_trace)[0]):#[50]:#range(np.shape(order_trace)[0])
@@ -245,10 +245,10 @@ class QuicklookAlg:
                      x_grid = np.linspace(0,order_trace.iloc[i]['X2'],int(order_trace.iloc[i]['X2']-0))
                      y_grid = order_trace.iloc[i]['Coeff0']+x_grid*order_trace.iloc[i]['Coeff1']+x_grid**2*order_trace.iloc[i]['Coeff2']+x_grid**3*order_trace.iloc[i]['Coeff3']
                      print(i,y_grid,y_grid[which_column])
-                     plt.plot([y_grid[which_column],y_grid[which_column]],[1,1.*np.nanmax(counts[:,which_column]],color ='red',linewidth = 0.2)
+                     #plt.plot([y_grid[which_column],y_grid[which_column]],[1,1.*np.nanmax(counts[:,which_column]],color ='red',linewidth = 0.2)
                      #plt.plot(x_grid[which_column],y_grid[which_column]-order_trace.iloc[i]['BottomEdge'],color ='white',linewidth = 0.2,alpha = 1)
                      #plt.plot(x_grid[which_column],y_grid[which_column]+order_trace.iloc[i]['TopEdge'],color ='black',linewidth = 0.2,alpha = 1)
-            '''
+
             plt.savefig(output_dir+'fig/'+exposure_name+'_Column_cut_'+ccd_color[i_color]+'.pdf')
             plt.savefig(output_dir+'fig/'+exposure_name+'_Column_cut_'+ccd_color[i_color]+'.png', dpi=200)
 
