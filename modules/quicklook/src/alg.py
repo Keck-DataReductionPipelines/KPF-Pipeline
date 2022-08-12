fc0c8010308d2bc3b6ae36f41b76728cfdb8d8b8
DaiFei <daifei@MacBook-Pro-2.local>
2022-08-12 15:13:00 -0700

    end_of_night

--- modules/quicklook/src/alg.py
+++ modules/quicklook/src/alg.py
@@ -74,7 +74,7 @@ class QuicklookAlg:
             '''
 
             #plot how the order trace changes
-            order_trace_list = glob.glob('/data/order_trace/*', recursive = True)
+            order_trace_list = glob.glob('/data/order_trace/*/*.csv', recursive = True)
             print(order_trace_list)
             return
         print('working on',date,exposure_name)
