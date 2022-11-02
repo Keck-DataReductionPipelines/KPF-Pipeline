70337cae9ec5aba1ed93bf2bafe2f6834201f92a
Russ Laher <laher@ipac.caltech.edu>
2022-11-02 14:17:26 -0700

    Restructured for the automated testing.

--- tests/regression/test_FitsHeaders.py
+++ tests/regression/test_FitsHeaders.py
@@ -1,28 +1,43 @@
 from modules.Utils.kpf_fits import FitsHeaders
 
-fits_files_path = '/data'
-files_in_dir = fits_files_path+'/KP*.fits'
-print('files_in_dir = ', files_in_dir)
+def test_match_headers_float_le():
 
-# Find all level 0 processed fits files with matching EXPTIME less than or equal zero.
+    """
+    Find all fits files with matching EXPTIME less than or equal zero.
+    """
 
-fh = FitsHeaders(files_in_dir, 'EXPTIME', '0.0')
-input_files = fh.match_headers_float_le()
+    fh = FitsHeaders(files_in_dir, 'EXPTIME', '0.0')
+    input_files = fh.match_headers_float_le()
 
-print('Output from match_headers_float_le...')
+    print('Output from match_headers_float_le...')
 
-i = 1
-for input_file in (input_files):
-    print(i,input_file)
-    i = i + 1
+    i = 1
+    for input_file in (input_files):
+        print(i,input_file)
+        i += 1
 
-fh2 = FitsHeaders(files_in_dir, 'IMTYPE', 'NONE')
-input_files2 = fh2.match_headers_string_lower()
+def test_match_headers_string_lower():
 
-print('Output from match_headers_string_lower...')
+    """
+    Find all fits files with lowercase IMTYPE value matching "none".
+    """
 
-i = 1
-for input_file in (input_files2):
-    print(i,input_file)
-    i = i + 1
+    fh2 = FitsHeaders(files_in_dir, 'IMTYPE', 'NONE')
+    input_files2 = fh2.match_headers_string_lower()
 
+    print('Output from match_headers_string_lower...')
+
+    i = 1
+    for input_file in (input_files2):
+        print(i,input_file)
+        i += 1
+
+
+if __name__ == '__main__':
+
+    fits_files_path = '/data'
+    files_in_dir = fits_files_path+'/KP*.fits'
+    print('files_in_dir = ', files_in_dir)
+
+    test_match_headers_float_le()
+    test_match_headers_string_lower()
