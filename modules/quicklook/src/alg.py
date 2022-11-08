4403692bbe51b8561a1f0a4b5fb554a2de0fc786
DaiFei <daifei@MacBook-Pro-113.local>
2022-11-07 20:51:28 -0800

    EM

--- modules/quicklook/src/alg.py
+++ modules/quicklook/src/alg.py
@@ -403,10 +403,10 @@ class QuicklookAlg:
             plt.style.use('seaborn-whitegrid')
             plt.figure(figsize=(12, 6), tight_layout=True)
             od_arr = [0.1, 0.4, 0.5, 0.6, 0.7, 0.8] # OD0.1, OD1.0, OD1.3, OD2.0, OD3.0, OD4.0
-            print('tdur_sec',np.float(time_em[-1]-time_em[0]))
-            grid_width = math.ceil(np.float(time_em[-1]-time_em[0])*1.1/10)*10
-            for i_grid in range(12):
-                plt.axvspan(  0+i_grid*grid_width,  (i_grid+1)*grid_width, alpha=od_arr[i_grid%6], color='gray')
+            #print('tdur_sec',np.float(time_em[-1]-time_em[0]))
+            #grid_width = math.ceil(np.float(time_em[-1]-time_em[0])*1.1/10)*10
+            #for i_grid in range(12):
+            #    plt.axvspan(  0+i_grid*grid_width,  (i_grid+1)*grid_width, alpha=od_arr[i_grid%6], color='gray')
 
             plt.plot(time_em, int_SCI_flux_750p    / ((847+4.8/2)-750)           / tdur_sec, marker='o', color='r', label = 'SCI 750-849 nm')
             plt.plot(time_em, int_SCI_flux_650_750 / (750-650)                   / tdur_sec, marker='o', color='orange', label = 'SCI 650-750 nm')
