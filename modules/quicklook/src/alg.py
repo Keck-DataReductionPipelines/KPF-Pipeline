f962e6021a0a813ffcc65724d662adad7e185a7d
DaiFei <daifei@MacBook-Pro-113.local>
2022-11-10 19:38:16 -0800

    rv grid

--- modules/quicklook/src/alg.py
+++ modules/quicklook/src/alg.py
@@ -584,8 +584,13 @@ class QuicklookAlg:
             for i_color in range(len(ccf_color)):
                 ccf = np.array(hdulist[ccf_color[i_color]].data,'d')
                 print('ccf',np.shape(ccf))
-                step = float(self.config['RV']['step'])
-                vel_grid = np.array(range(-int(np.shape(ccf)[2]/2),int(np.shape(ccf)[2]/2),1),'d')*step
+                #step = float(self.config['RV']['step'])
+                step = float(hdulist[ccf_color[i_color]].header['STEPV'])
+                startv = float(hdulist[ccf_color[i_color]].header['STARTV'])
+
+                print('gamma',hdulist['GREEN_CCF'].header)
+                #vel_grid = np.array(range(-int(np.shape(ccf)[2]/2),int(np.shape(ccf)[2]/2),1),'d')*step
+                vel_grid = startv+np.array(range(np.shape(ccf)[2])),'d')*step
 
                 '''
                 # plot the individual orders?
