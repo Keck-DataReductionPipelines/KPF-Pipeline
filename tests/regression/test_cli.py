af8b627c4e6e8c57011585464c5af11aa43215bb
BJ Fulton <bjfultn@gmail.com>
2022-12-09 13:09:03 -0800

    fixed cli test

--- tests/regression/test_cli.py
+++ tests/regression/test_cli.py
@@ -10,7 +10,7 @@ def test_help():
     assert rc == 2, "running command '{}' failed".format(cmd)
 
 def test_launch():
-    cmd = 'kpf -r examples/simple.recipe -c examples/default_simple.cfg'
+    cmd = 'kpf --ncpus=1 -r examples/simple.recipe -c examples/default_simple.cfg'
     child = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
     streamdata = child.communicate()[0]
     rc = child.returncode
