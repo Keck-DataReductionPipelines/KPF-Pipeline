49499e9b6cae6ded701a934f54671f0b025fa466
DaiFei <daifei@MacBook-Pro-113.local>
2023-05-16 16:43:16 -0700

    daily masters

--- modules/quicklook/src/alg.py
+++ modules/quicklook/src/alg.py
@@ -208,8 +208,8 @@ class QuicklookAlg:
                 master_counts = np.array(hdulist1[ccd_color[i_color]].data,'d')
                 master_flatten_counts = np.ravel(master_counts)
                 if version == 'Dark':#scale up dark exposures
-                    master_counts*=hdr['EXPTIME']
-                    master_flatten_counts*=hdr['EXPTIME']
+                    master_counts*=1. #hdr['EXPTIME']
+                    master_flatten_counts*=1. #hdr['EXPTIME']
             print(version,hdr,hdr['EXPTIME'],type(hdr['EXPTIME']),hdulist1[0].header['EXPTIME'],Cal_Source,master_file,os.path.exists(master_file))
             #print(master_counts)
             input("Press Enter to continue...")
