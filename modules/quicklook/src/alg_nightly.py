4be2f996a3232b3d131ae1e67518edff366cf5dc
DaiFei <daifei@MacBook-Pro-113.local>
2023-06-09 16:33:25 -0700

    nightly_summary

--- modules/quicklook/src/alg_nightly.py
+++ modules/quicklook/src/alg_nightly.py
@@ -122,13 +122,13 @@ class Nightly_summaryAlg:
                 order_trace_file = self.config['L1']['order_trace']+ccd_color[i_color]+'.csv'
                 order_trace = pd.read_csv(order_trace_file)
                 #print(order_trace_file,order_trace)
-                for i in range(np.shape(order_trace)[0]):#[50]:#range(np.shape(order_trace)[0])
+                for j in range(np.shape(order_trace)[0]):#[50]:#range(np.shape(order_trace)[0])
                     #print(order_trace.iloc[i]['X1'],int(order_trace.iloc[i]['X2']-order_trace.iloc[i]['X1']))
-                    x_grid = np.linspace(order_trace.iloc[i]['X1'],order_trace.iloc[i]['X2'],int(order_trace.iloc[i]['X2']-order_trace.iloc[i]['X1']))
-                    y_grid = order_trace.iloc[i]['Coeff0']+x_grid*order_trace.iloc[i]['Coeff1']+x_grid**2*order_trace.iloc[i]['Coeff2']+x_grid**3*order_trace.iloc[i]['Coeff3']
+                    x_grid = np.linspace(order_trace.iloc[j]['X1'],order_trace.iloc[j]['X2'],int(order_trace.iloc[j]['X2']-order_trace.iloc[j]['X1']))
+                    y_grid = order_trace.iloc[j]['Coeff0']+x_grid*order_trace.iloc[j]['Coeff1']+x_grid**2*order_trace.iloc[j]['Coeff2']+x_grid**3*order_trace.iloc[j]['Coeff3']
                     plt.plot(x_grid,y_grid,color ='magenta',linewidth = 0.2)
-                    plt.plot(x_grid,y_grid-order_trace.iloc[i]['BottomEdge'],':',color ='white',linewidth = 0.2,alpha = 1)
-                    plt.plot(x_grid,y_grid+order_trace.iloc[i]['TopEdge'],'--',color ='black',linewidth = 0.2,alpha = 1)
+                    plt.plot(x_grid,y_grid-order_trace.iloc[j]['BottomEdge'],':',color ='white',linewidth = 0.2,alpha = 1)
+                    plt.plot(x_grid,y_grid+order_trace.iloc[j]['TopEdge'],'--',color ='black',linewidth = 0.2,alpha = 1)
                     #plt.fill_between(x_grid,y_grid-order_trace.iloc[i]['BottomEdge'],y_grid+order_trace.iloc[i]['TopEdge'],color ='pink',alpha = 0.2)
                     #print(x_grid,y_grid)
                 plt.xlim(3200,4000)
