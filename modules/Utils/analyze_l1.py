d6ef4eb7177356ce041bc5540dd5ead382e67a61
DaiFei <daifei@MacBook-Pro-113.local>
2023-06-19 14:03:48 -0700

    l1 snr

--- modules/Utils/analyze_l1.py
+++ modules/Utils/analyze_l1.py
@@ -12,17 +12,17 @@ class AnalyzeL1:
         L0 - an L0 object
 
     Attributes:
-        GREEN_SNR (from compute_l1_snr): Two-dimensional array of 
-            SNR values for the Green CCD.  The first array index specifies 
-            the spectral order (0-34 = green, 0-31 = red).  
-            The second array index specifies the orderlet: 
+        GREEN_SNR (from compute_l1_snr): Two-dimensional array of
+            SNR values for the Green CCD.  The first array index specifies
+            the spectral order (0-34 = green, 0-31 = red).
+            The second array index specifies the orderlet:
             0=CAL, 1=SCI1, 2=SCI2, 3=SCI3, 4=SKY, 5=SCI1+SCI2+SCI3
-        RED_SNR (from compute_l1_snr): Similar to GREEN_SNR, 
+        RED_SNR (from compute_l1_snr): Similar to GREEN_SNR,
             but for the Red CCD.
-        GREEN_SNR_WAV (from compute_l1_snr): One-dimensional array 
-            of the wavelength of the middle of the spectral orders on 
+        GREEN_SNR_WAV (from compute_l1_snr): One-dimensional array
+            of the wavelength of the middle of the spectral orders on
             the green CCD.
-        RED_SNR_WAV (from compute_l1_snr): Similar to GREEN_SNR, 
+        RED_SNR_WAV (from compute_l1_snr): Similar to GREEN_SNR,
             but for the Red CCD.
     """
 
@@ -35,53 +35,53 @@ class AnalyzeL1:
             self.logger = None
             print('---->AnalyzeL0 class constructor')
 
-    def measure_L1_snr(self, snr_percentile=95):
+    def measure_L1_snr(self,L1, snr_percentile=95):
 
         """
-        Compute the signal-to-noise ratio (SNR) for each spectral order and 
-        orderlet in an L1 spectrum from KPF.  
+        Compute the signal-to-noise ratio (SNR) for each spectral order and
+        orderlet in an L1 spectrum from KPF.
         SNR is defined as signal / sqrt(abs(variance)) and can be negative.
 
         Args:
-            snr_percentile: snr_percentile in the SNR distribution for each combination of 
-                order and orderlet 
+            snr_percentile: snr_percentile in the SNR distribution for each combination of
+                order and orderlet
 
         Attributes:
             GREEN_SNR - Two-dimensional array of SNR values for the Green CCD.
-                The first array index specifies the spectral order 
-                (0-34 = green, 0-31 = red).  The second array index 
-                specifies the orderlet: 
+                The first array index specifies the spectral order
+                (0-34 = green, 0-31 = red).  The second array index
+                specifies the orderlet:
                 0=CAL, 1=SCI1, 2=SCI2, 3=SCI3, 4=SKY, 5=SCI1+SCI2+SCI3.
-                For example, GREEN_SNR[1,2] is the SNR for order=1 and the 
+                For example, GREEN_SNR[1,2] is the SNR for order=1 and the
                 SCI2 orderlet.
             RED_SNR - Similar to GREEN_SNR, but for the Red CCD.
-            GREEN_SNR_WAV - One-dimensional array of the wavelength of the 
+            GREEN_SNR_WAV - One-dimensional array of the wavelength of the
                 middle of the spectral orders on the green CCD.
             RED_SNR_WAV - Similar to GREEN_SNR, but for the Red CCD.
 
         Returns:
             None
         """
-        
+
         self.snr_percentile = snr_percentile
 
         # Determine the number of orders
         norders_green = (L1['GREEN_SKY_WAVE'].data).shape[0]
         norders_red   = (L1['RED_SKY_WAVE'].data).shape[0]
         orderlets = {'CAL','SCI1','SCI2','SCI3','SKY'}
-        norderlets = len(orderlets) 
+        norderlets = len(orderlets)
 
         # Define SNR arrays (needed for operations below where VAR = 0)
         GREEN_SCI_SNR1 = 0 * L1['GREEN_SCI_VAR1'].data
         GREEN_SCI_SNR2 = 0 * L1['GREEN_SCI_VAR2'].data
         GREEN_SCI_SNR3 = 0 * L1['GREEN_SCI_VAR3'].data
-        GREEN_CAL_SNR  = 0 * L1['GREEN_CAL_VAR'].data 
-        GREEN_SKY_SNR  = 0 * L1['GREEN_SKY_VAR'].data 
-        GREEN_SCI_SNR  = 0 * L1['GREEN_SCI_VAR1'].data 
-        RED_SCI_SNR1   = 0 * L1['RED_SCI_VAR1'].data  
-        RED_SCI_SNR2   = 0 * L1['RED_SCI_VAR2'].data  
-        RED_SCI_SNR3   = 0 * L1['RED_SCI_VAR3'].data  
-        RED_CAL_SNR    = 0 * L1['RED_CAL_VAR'].data   
+        GREEN_CAL_SNR  = 0 * L1['GREEN_CAL_VAR'].data
+        GREEN_SKY_SNR  = 0 * L1['GREEN_SKY_VAR'].data
+        GREEN_SCI_SNR  = 0 * L1['GREEN_SCI_VAR1'].data
+        RED_SCI_SNR1   = 0 * L1['RED_SCI_VAR1'].data
+        RED_SCI_SNR2   = 0 * L1['RED_SCI_VAR2'].data
+        RED_SCI_SNR3   = 0 * L1['RED_SCI_VAR3'].data
+        RED_CAL_SNR    = 0 * L1['RED_CAL_VAR'].data
         RED_SKY_SNR    = 0 * L1['RED_SKY_VAR'].data
         RED_SCI_SNR    = 0 * L1['RED_SCI_VAR1'].data
 
@@ -92,41 +92,41 @@ class AnalyzeL1:
         RED_SNR_WAV   = np.zeros(norders_red)
 
         # Compute SNR arrays for each of the orders, orderlets, and CCDs.
-        GREEN_SCI_SNR1 = np.divide(L1['GREEN_SCI_FLUX1'].data, 
-                                   np.sqrt(abs(L1['GREEN_SCI_VAR1'].data)), 
+        GREEN_SCI_SNR1 = np.divide(L1['GREEN_SCI_FLUX1'].data,
+                                   np.sqrt(abs(L1['GREEN_SCI_VAR1'].data)),
                                    where=(L1['GREEN_SCI_VAR1'].data!=0))
-        GREEN_SCI_SNR2 = np.divide(L1['GREEN_SCI_FLUX2'].data, 
-                                   np.sqrt(abs(L1['GREEN_SCI_VAR2'].data)), 
+        GREEN_SCI_SNR2 = np.divide(L1['GREEN_SCI_FLUX2'].data,
+                                   np.sqrt(abs(L1['GREEN_SCI_VAR2'].data)),
                                    where=(L1['GREEN_SCI_VAR2'].data!=0))
-        GREEN_SCI_SNR3 = np.divide(L1['GREEN_SCI_FLUX3'].data, 
-                                   np.sqrt(abs(L1['GREEN_SCI_VAR3'].data)), 
+        GREEN_SCI_SNR3 = np.divide(L1['GREEN_SCI_FLUX3'].data,
+                                   np.sqrt(abs(L1['GREEN_SCI_VAR3'].data)),
                                    where=(L1['GREEN_SCI_VAR3'].data!=0))
-        GREEN_SCI_SNR  = np.divide(L1['GREEN_SCI_FLUX1'].data+L1['GREEN_SCI_FLUX3'].data+L1['GREEN_SCI_FLUX3'].data, 
-                                   np.sqrt(abs(L1['GREEN_SCI_VAR1'].data)+abs(L1['GREEN_SCI_VAR2'].data)+abs(L1['GREEN_SCI_VAR3'].data)), 
+        GREEN_SCI_SNR  = np.divide(L1['GREEN_SCI_FLUX1'].data+L1['GREEN_SCI_FLUX3'].data+L1['GREEN_SCI_FLUX3'].data,
+                                   np.sqrt(abs(L1['GREEN_SCI_VAR1'].data)+abs(L1['GREEN_SCI_VAR2'].data)+abs(L1['GREEN_SCI_VAR3'].data)),
                                    where=(L1['GREEN_SCI_VAR1'].data+L1['GREEN_SCI_VAR2'].data+L1['GREEN_SCI_VAR3'].data!=0))
-        GREEN_CAL_SNR  = np.divide(L1['GREEN_CAL_FLUX'].data, 
-                                   np.sqrt(abs(L1['GREEN_CAL_VAR'].data)), 
+        GREEN_CAL_SNR  = np.divide(L1['GREEN_CAL_FLUX'].data,
+                                   np.sqrt(abs(L1['GREEN_CAL_VAR'].data)),
                                    where=(L1['GREEN_CAL_VAR'].data!=0))
-        GREEN_SKY_SNR  = np.divide(L1['GREEN_SKY_FLUX'].data, 
-                                   np.sqrt(abs(L1['GREEN_SKY_VAR'].data)), 
+        GREEN_SKY_SNR  = np.divide(L1['GREEN_SKY_FLUX'].data,
+                                   np.sqrt(abs(L1['GREEN_SKY_VAR'].data)),
                                    where=(L1['GREEN_SKY_VAR'].data!=0))
-        RED_SCI_SNR1   = np.divide(L1['RED_SCI_FLUX1'].data, 
-                                   np.sqrt(abs(L1['RED_SCI_VAR1'].data)), 
+        RED_SCI_SNR1   = np.divide(L1['RED_SCI_FLUX1'].data,
+                                   np.sqrt(abs(L1['RED_SCI_VAR1'].data)),
                                    where=(L1['RED_SCI_VAR1'].data!=0))
-        RED_SCI_SNR2   = np.divide(L1['RED_SCI_FLUX2'].data, 
-                                   np.sqrt(abs(L1['RED_SCI_VAR2'].data)), 
+        RED_SCI_SNR2   = np.divide(L1['RED_SCI_FLUX2'].data,
+                                   np.sqrt(abs(L1['RED_SCI_VAR2'].data)),
                                    where=(L1['RED_SCI_VAR2'].data!=0))
-        RED_SCI_SNR3   = np.divide(L1['RED_SCI_FLUX3'].data, 
-                                   np.sqrt(abs(L1['RED_SCI_VAR3'].data)), 
+        RED_SCI_SNR3   = np.divide(L1['RED_SCI_FLUX3'].data,
+                                   np.sqrt(abs(L1['RED_SCI_VAR3'].data)),
                                    where=(L1['RED_SCI_VAR3'].data!=0))
-        RED_SCI_SNR    = np.divide(L1['RED_SCI_FLUX1'].data+L1['RED_SCI_FLUX3'].data+L1['RED_SCI_FLUX3'].data, 
-                                   np.sqrt(abs(L1['RED_SCI_VAR1'].data)+abs(L1['RED_SCI_VAR2'].data)+abs(L1['RED_SCI_VAR3'].data)), 
+        RED_SCI_SNR    = np.divide(L1['RED_SCI_FLUX1'].data+L1['RED_SCI_FLUX3'].data+L1['RED_SCI_FLUX3'].data,
+                                   np.sqrt(abs(L1['RED_SCI_VAR1'].data)+abs(L1['RED_SCI_VAR2'].data)+abs(L1['RED_SCI_VAR3'].data)),
                                    where=(L1['RED_SCI_VAR1'].data+L1['RED_SCI_VAR2'].data+L1['RED_SCI_VAR3'].data!=0))
-        RED_CAL_SNR    = np.divide(L1['RED_CAL_FLUX'].data, 
-                                   np.sqrt(abs(L1['RED_CAL_VAR'].data)), 
+        RED_CAL_SNR    = np.divide(L1['RED_CAL_FLUX'].data,
+                                   np.sqrt(abs(L1['RED_CAL_VAR'].data)),
                                    where=(L1['RED_CAL_VAR'].data!=0))
-        RED_SKY_SNR    = np.divide(L1['RED_SKY_FLUX'].data, 
-                                   np.sqrt(abs(L1['RED_SKY_VAR'].data)), 
+        RED_SKY_SNR    = np.divide(L1['RED_SKY_FLUX'].data,
+                                   np.sqrt(abs(L1['RED_SKY_VAR'].data)),
                                    where=(L1['RED_SKY_VAR'].data!=0))
 
         # Compute SNR per order and per orderlet
@@ -151,26 +151,26 @@ class AnalyzeL1:
         self.GREEN_SNR = GREEN_SNR
         self.RED_SNR   = RED_SNR
         self.GREEN_SNR_WAV = GREEN_SNR_WAV
-        self.RED_SNR_WAV   = RED_SNR_WAV            
-            
-    def plot_L1_snr(self, fig_path=None, show_plot=False):
+        self.RED_SNR_WAV   = RED_SNR_WAV
+
+    def plot_L1_snr(self,L1, fig_path=None, show_plot=False):
 
         """
-        
-        Generate a plot of SNR per order as compuated using the compute_l1_snr 
+
+        Generate a plot of SNR per order as compuated using the compute_l1_snr
         function.
 
         Args:
-            fig_path (string) - set to the path for a SNR vs. wavelength file 
+            fig_path (string) - set to the path for a SNR vs. wavelength file
                 to be generated.
             show_plot (boolean) - show the plot in the current environment.
 
         Returns:
-            PNG plot in fig_path or shows the plot it the current environment 
+            PNG plot in fig_path or shows the plot it the current environment
             (e.g., in a Jupyter Notebook).
 
         """
-        
+
         # Make 3-panel plot. First, create the figure and subplots
         fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True, figsize=(10,14))
 
