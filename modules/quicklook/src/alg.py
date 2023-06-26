9bb82c0d80ffcaefa337dfda6abb7e4aa1b91f1b
DaiFei <daifei@MacBook-Pro-113.local>
2023-06-26 13:07:57 -0700

    L0 plots

--- modules/quicklook/src/alg.py
+++ modules/quicklook/src/alg.py
@@ -129,7 +129,7 @@ class QuicklookAlg:
 
         #operate on L0 data before image assembly
         L0_file = self.config['IO']['input_prefix_l0_pre']+date+'/'+exposure_name+'_L0.fits'
-        L0 = fits.open(L0_data)
+        L0 = fits.open(L0_file)
 
         green_image = np.flipud(np.concatenate((L0['GREEN_AMP1'].data, L0['GREEN_AMP2'].data), axis=1))/2**16
         plt.figure(tight_layout=True)
