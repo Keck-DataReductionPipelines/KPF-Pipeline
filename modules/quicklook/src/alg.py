bf97c5784144f033f91cf4a63e6cee0b41354930
DaiFei <daifei@MacBook-Pro-113.local>
2023-06-29 16:32:54 -0700

    L0 updates

--- modules/quicklook/src/alg.py
+++ modules/quicklook/src/alg.py
@@ -129,13 +129,16 @@ class QuicklookAlg:
 
 
         #operate on L0 data before image assembly
-        L0_obj = AnalyzeL0(kpf0_file)
+        L0_file = self.config['IO']['input_prefix_l0_pre']+date+'/'+exposure_name+'.fits'
+        L0_kpf = kpf0_from_fits(L0_file,data_type='KPF')
+        L0_obj = AnalyzeL0(L0_kpf)
+        print(L0_obj,L0_obj.info())
         if os.path.exists(output_dir+'/'+exposure_name+'/L0/') == False: os.makedirs(output_dir+'/'+exposure_name+'/L0/')
         L0_obj.plot_L0_stitched_image(chip='green', fig_path=output_dir+'/'+exposure_name+'/L0/'+exposure_name+'_GREEN_L0_zoomable.png', show_plot=False)
         L0_obj.plot_L0_stitched_image(chip='red', fig_path=output_dir+'/'+exposure_name+'/L0/'+exposure_name+'_RED_L0_zoomable.png', show_plot=False)
 
         '''
-        L0_file = self.config['IO']['input_prefix_l0_pre']+date+'/'+exposure_name+'.fits'
+
         L0 = fits.open(L0_file)
 
         green_image = np.flipud(np.concatenate((L0['GREEN_AMP1'].data, L0['GREEN_AMP2'].data), axis=1))/2**16
