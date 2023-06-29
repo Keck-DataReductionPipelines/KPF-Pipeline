2e946b1e7f9ff42631576f7d4c1d01258dbe8113
DaiFei <daifei@MacBook-Pro-113.local>
2023-06-29 16:37:56 -0700

    L0 updates

--- modules/quicklook/src/alg.py
+++ modules/quicklook/src/alg.py
@@ -13,7 +13,6 @@ import math
 from astropy import modeling
 from astropy.time import Time
 from datetime import datetime
-from modules.Utils.analyze_l1 import AnalyzeL0
 
 
 class QuicklookAlg:
@@ -129,16 +128,6 @@ class QuicklookAlg:
 
 
         #operate on L0 data before image assembly
-        '''
-        L0_file = self.config['IO']['input_prefix_l0_pre']+date+'/'+exposure_name+'.fits'
-        L0_kpf = kpf0_from_fits(L0_file,data_type='KPF')
-        L0_obj = AnalyzeL0(L0_kpf)
-        print(L0_obj,L0_obj.info())
-        if os.path.exists(output_dir+'/'+exposure_name+'/L0/') == False: os.makedirs(output_dir+'/'+exposure_name+'/L0/')
-        L0_obj.plot_L0_stitched_image(chip='green', fig_path=output_dir+'/'+exposure_name+'/L0/'+exposure_name+'_GREEN_L0_zoomable.png', show_plot=False)
-        L0_obj.plot_L0_stitched_image(chip='red', fig_path=output_dir+'/'+exposure_name+'/L0/'+exposure_name+'_RED_L0_zoomable.png', show_plot=False)
-        '''
-
         L0_file = self.config['IO']['input_prefix_l0_pre']+date+'/'+exposure_name+'.fits'
         L0 = fits.open(L0_file)
 
@@ -173,7 +162,6 @@ class QuicklookAlg:
         plt.savefig(output_dir+'/'+exposure_name+'/L0/'+exposure_name+'_RED_L0_zoomable.png',dpi=500,facecolor='white')
         plt.close()
         L0.close()
-
 
 
         #read ccd directly
