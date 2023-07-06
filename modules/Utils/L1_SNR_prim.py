1032c27aba43cd7abf7c0ad55f02f55e588733a6
DaiFei <daifei@MacBook-Pro-113.local>
2023-07-05 22:25:45 -0700

    L1 SNR plots

--- modules/Utils/L1_SNR_prim.py
+++ modules/Utils/L1_SNR_prim.py
@@ -54,7 +54,7 @@ class L1_SNR(KPF1_Primitive):
     #Perform
     def _perform(self) -> None:
         #L1_file = fits.open(self.data_dir+self.l1_obj.filename)
-
+        print('test',self.l1_obj['GREEN_SCI_FLUX1'],self.l1_obj['GREEN_SCI_FLUX1'].data)
         L1_SNR = AnalyzeL1(self.l1_obj.extensions) #L1_file
         L1_SNR.measure_L1_snr(snr_percentile=95)# self.l1_obj,L1_file
         #print(L1_SNR.GREEN_SNR) #(orders number, orderlet number)
