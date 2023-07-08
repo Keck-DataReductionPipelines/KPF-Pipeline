0721c78d288e5d510be8c14a9a5ac33edbbb70be
Andrew Howard <awhoward@gmail.com>
2023-07-07 21:44:38 -0700

    moved variables to self.

--- modules/Utils/analyze_em.py
+++ modules/Utils/analyze_em.py
@@ -19,12 +19,34 @@ class AnalyzeEM:
 
     def __init__(self, L0, logger=None):
         self.L0 = L0 
+
+        self.EM_gain = 1.48424 #np.float(self.config['EM']['gain'])
+        self.starname = self.L0['PRIMARY'].header['TARGNAME']
+        self.ObsID = self.L0['PRIMARY'].header['OFNAME']
+
+        # Read data tables
+        self.dat_SKY = Table.read(self.L0, format='fits',hdu='EXPMETER_SKY')
+        self.dat_SCI = Table.read(self.L0, format='fits',hdu='EXPMETER_SCI')
+        self.df_SKY_EM = self.dat_SKY.to_pandas()
+        self.df_SCI_EM = self.dat_SCI.to_pandas()
+        i = 0
+        for col in self.df_SCI_EM.columns:
+            if col.lower().startswith('date'):
+                i += 1
+            else:
+                break
+        self.wav_SCI_str = self.df_SCI_EM.columns[i:]
+        self.wav_SCI     = self.df_SCI_EM.columns[i:].astype(float)
+        self.wav_SKY_str = self.df_SKY_EM.columns[i:]
+        self.wav_SKY     = self.df_SKY_EM.columns[i:].astype(float)
+
         if logger:
             self.logger = logger
             self.logger.debug('AnalyzeEM class constructor')
         else:
             self.logger = None
             print('---->AnalyzeEM class constructor')
+
 
     def plot_EM_time_series(self, fig_path=None, show_plot=False):
 
@@ -41,52 +63,52 @@ class AnalyzeEM:
 
         """
 
-        EM_gain = 1.48424 #np.float(self.config['EM']['gain'])
-        starname = L0['PRIMARY'].header['TARGNAME']
-        ObsID = L0['PRIMARY'].header['OFNAME']
+        #EM_gain = 1.48424 #np.float(self.config['EM']['gain'])
+        #starname = L0['PRIMARY'].header['TARGNAME']
+        #ObsID = L0['PRIMARY'].header['OFNAME']
 
-        # Read data tables
-        dat_SKY = Table.read(L0, format='fits',hdu='EXPMETER_SKY')
-        dat_SCI = Table.read(L0, format='fits',hdu='EXPMETER_SCI')
-        df_SKY_EM = dat_SKY.to_pandas()
-        df_SCI_EM = dat_SCI.to_pandas()
-        i = 0
-        for col in df_SCI_EM.columns:
-            if col.lower().startswith('date'):
-                i += 1
-            else:
-                break
-        wav_SCI_str = df_SCI_EM.columns[i:]
-        wav_SCI     = df_SCI_EM.columns[i:].astype(float)
-        wav_SKY_str = df_SKY_EM.columns[i:]
-        wav_SKY     = df_SKY_EM.columns[i:].astype(float)
+        ## Read data tables
+        #dat_SKY = Table.read(self.L0, format='fits',hdu='EXPMETER_SKY')
+        #dat_SCI = Table.read(self.L0, format='fits',hdu='EXPMETER_SCI')
+        #df_SKY_EM = dat_SKY.to_pandas()
+        #df_SCI_EM = dat_SCI.to_pandas()
+        #i = 0
+        #for col in df_SCI_EM.columns:
+        #    if col.lower().startswith('date'):
+        #        i += 1
+        #    else:
+        #        break
+        #wav_SCI_str = self.df_SCI_EM.columns[i:]
+        #wav_SCI     = self.df_SCI_EM.columns[i:].astype(float)
+        #wav_SKY_str = self.df_SKY_EM.columns[i:]
+        #wav_SKY     = self.df_SKY_EM.columns[i:].astype(float)
 
         # Define time arrays
-        date_end = np.array(df_SCI_EM["Date-End"], dtype=np.datetime64)
-        if 'Date-Beg-Corr' in df_SCI_EM.columns:
-            date_beg = np.array(df_SCI_EM["Date-Beg-Corr"], dtype=np.datetime64)
-            date_end = np.array(df_SCI_EM["Date-End-Corr"], dtype=np.datetime64)
+        date_end = np.array(self.df_SCI_EM["Date-End"], dtype=np.datetime64)
+        if 'Date-Beg-Corr' in self.df_SCI_EM.columns:
+            date_beg = np.array(self.df_SCI_EM["Date-Beg-Corr"], dtype=np.datetime64)
+            date_end = np.array(self.df_SCI_EM["Date-End-Corr"], dtype=np.datetime64)
         else:
-            date_beg = np.array(df_SCI_EM["Date-Beg"], dtype=np.datetime64)
-            date_end = np.array(df_SCI_EM["Date-End"], dtype=np.datetime64)
+            date_beg = np.array(self.df_SCI_EM["Date-Beg"], dtype=np.datetime64)
+            date_end = np.array(self.df_SCI_EM["Date-End"], dtype=np.datetime64)
         tdur_sec = (date_end-date_beg).astype(float)/1000. # exposure duration in sec
         time_em     = (date_beg-date_beg[0]).astype(float)/1000. # seconds since beginning
         
         # Define flux arrays
-        ind_550m    = np.where((wav_SCI <  550))
-        ind_550_650 = np.where((wav_SCI >= 550) & (wav_SCI < 650))
-        ind_650_750 = np.where((wav_SCI >= 650) & (wav_SCI < 750))
-        ind_750p    = np.where((wav_SCI >= 750))
-        int_SCI_flux         = df_SCI_EM.sum(axis=1)                         # flux (ADU) vs. time (per sample)
-        int_SCI_flux_550m    = df_SCI_EM[wav_SCI_str[np.where((wav_SCI <  550))]].sum(axis=1)
-        int_SCI_flux_550_650 = df_SCI_EM[wav_SCI_str[np.where((wav_SCI >= 550) & (wav_SCI < 650))]].sum(axis=1)
-        int_SCI_flux_650_750 = df_SCI_EM[wav_SCI_str[np.where((wav_SCI >= 650) & (wav_SCI < 750))]].sum(axis=1)
-        int_SCI_flux_750p    = df_SCI_EM[wav_SCI_str[np.where((wav_SCI >= 750))]].sum(axis=1)
-        int_SKY_flux         = df_SKY_EM.sum(axis=1)                         # flux (ADU) vs. time (per sample)
-        int_SKY_flux_550m    = df_SKY_EM[wav_SKY_str[np.where((wav_SKY <  550))]].sum(axis=1)
-        int_SKY_flux_550_650 = df_SKY_EM[wav_SKY_str[np.where((wav_SKY >= 550) & (wav_SKY < 650))]].sum(axis=1)
-        int_SKY_flux_650_750 = df_SKY_EM[wav_SKY_str[np.where((wav_SKY >= 650) & (wav_SKY < 750))]].sum(axis=1)
-        int_SKY_flux_750p    = df_SKY_EM[wav_SKY_str[np.where((wav_SKY >= 750))]].sum(axis=1)
+        ind_550m    = np.where((self.wav_SCI <  550))
+        ind_550_650 = np.where((self.wav_SCI >= 550) & (self.wav_SCI < 650))
+        ind_650_750 = np.where((self.wav_SCI >= 650) & (self.wav_SCI < 750))
+        ind_750p    = np.where((self.wav_SCI >= 750))
+        int_SCI_flux         = self.df_SCI_EM.sum(axis=1)                         # flux (ADU) vs. time (per sample)
+        int_SCI_flux_550m    = self.df_SCI_EM[self.wav_SCI_str[np.where((self.wav_SCI <  550))]].sum(axis=1)
+        int_SCI_flux_550_650 = self.df_SCI_EM[self.wav_SCI_str[np.where((self.wav_SCI >= 550) & (self.wav_SCI < 650))]].sum(axis=1)
+        int_SCI_flux_650_750 = self.df_SCI_EM[self.wav_SCI_str[np.where((self.wav_SCI >= 650) & (self.wav_SCI < 750))]].sum(axis=1)
+        int_SCI_flux_750p    = self.df_SCI_EM[self.wav_SCI_str[np.where((self.wav_SCI >= 750))]].sum(axis=1)
+        int_SKY_flux         = self.df_SKY_EM.sum(axis=1)                         # flux (ADU) vs. time (per sample)
+        int_SKY_flux_550m    = self.df_SKY_EM[self.wav_SKY_str[np.where((self.wav_SKY <  550))]].sum(axis=1)
+        int_SKY_flux_550_650 = self.df_SKY_EM[self.wav_SKY_str[np.where((self.wav_SKY >= 550) & (self.wav_SKY < 650))]].sum(axis=1)
+        int_SKY_flux_650_750 = self.df_SKY_EM[self.wav_SKY_str[np.where((self.wav_SKY >= 650) & (self.wav_SKY < 750))]].sum(axis=1)
+        int_SKY_flux_750p    = self.df_SKY_EM[self.wav_SKY_str[np.where((self.wav_SKY >= 750))]].sum(axis=1)
 
         # Plot time series
         plt.figure(figsize=(10, 4), tight_layout=True)
@@ -101,7 +123,7 @@ class AnalyzeEM:
         plt.plot(time_em, int_SKY_flux_550_650 / (650-550) / tdur_sec,':', marker='o', color='g')
         plt.plot(time_em, int_SKY_flux_550m    / (550-445) / tdur_sec,':', marker='o', color='b')
         plt.plot(time_em, int_SKY_flux         / (870-445) / tdur_sec,':', marker='o', color='k', label = 'SKY 445-870 nm')
-        plt.title('Exposure Meter Time Series: ' + str(ObsID) + ' - ' + starname, fontsize=14)
+        plt.title('Exposure Meter Time Series: ' + str(self.ObsID) + ' - ' + self.starname, fontsize=14)
         plt.xlabel("Time (sec)",fontsize=14)
         plt.ylabel("Exposure Meter Flux (e-/nm/s)",fontsize=14)
         plt.yscale('log')
@@ -132,46 +154,46 @@ class AnalyzeEM:
 
         """
 
-        EM_gain = 1.48424 #np.float(self.config['EM']['gain'])
-        starname = L0['PRIMARY'].header['TARGNAME']
-        ObsID = L0['PRIMARY'].header['OFNAME']
+        #EM_gain = 1.48424 #np.float(self.config['EM']['gain'])
+        #starname = L0['PRIMARY'].header['TARGNAME']
+        #ObsID = L0['PRIMARY'].header['OFNAME']
 
-        # Read data tables
-        dat_SKY = Table.read(L0, format='fits',hdu='EXPMETER_SKY')
-        dat_SCI = Table.read(L0, format='fits',hdu='EXPMETER_SCI')
-        df_SKY_EM = dat_SKY.to_pandas()
-        df_SCI_EM = dat_SCI.to_pandas()
-        i = 0
-        for col in df_SCI_EM.columns:
-            if col.lower().startswith('date'):
-                i += 1
-            else:
-                break
-        wav_SCI_str = df_SCI_EM.columns[i:]
-        wav_SCI     = df_SCI_EM.columns[i:].astype(float)
-        wav_SKY_str = df_SKY_EM.columns[i:]
-        wav_SKY     = df_SKY_EM.columns[i:].astype(float)
+        ## Read data tables
+        #dat_SKY = Table.read(L0, format='fits',hdu='EXPMETER_SKY')
+        #dat_SCI = Table.read(L0, format='fits',hdu='EXPMETER_SCI')
+        #df_SKY_EM = dat_SKY.to_pandas()
+        #df_SCI_EM = dat_SCI.to_pandas()
+        #i = 0
+        #for col in df_SCI_EM.columns:
+        #    if col.lower().startswith('date'):
+        #        i += 1
+        #    else:
+        #        break
+        #wav_SCI_str = self.df_SCI_EM.columns[i:]
+        #wav_SCI     = self.df_SCI_EM.columns[i:].astype(float)
+        #wav_SKY_str = self.df_SKY_EM.columns[i:]
+        #wav_SKY     = self.df_SKY_EM.columns[i:].astype(float)
 
         # Define dispersion
-        disp_SCI = wav_SCI*0+np.gradient(wav_SCI,1)*-1
-        disp_SKY = wav_SKY*0+np.gradient(wav_SKY,1)*-1
-        disp_SCI_smooth = np.polyval(np.polyfit(wav_SCI,disp_SCI, deg=6),wav_SCI)
-        disp_SKY_smooth = np.polyval(np.polyfit(wav_SKY,disp_SKY, deg=6),wav_SKY)
-        df_SCI_EM_norm        = df_SCI_EM[wav_SCI_str] * EM_gain /disp_SCI_smooth
-        df_SKY_EM_norm        = df_SKY_EM[wav_SCI_str] * EM_gain /disp_SKY_smooth
+        disp_SCI = self.wav_SCI*0+np.gradient(self.wav_SCI,1)*-1
+        disp_SKY = self.wav_SKY*0+np.gradient(self.wav_SKY,1)*-1
+        disp_SCI_smooth = np.polyval(np.polyfit(self.wav_SCI,disp_SCI, deg=6),self.wav_SCI)
+        disp_SKY_smooth = np.polyval(np.polyfit(self.wav_SKY,disp_SKY, deg=6),self.wav_SKY)
+        df_SCI_EM_norm  = self.df_SCI_EM[self.wav_SCI_str] * self.EM_gain /disp_SCI_smooth
+        df_SKY_EM_norm  = self.df_SKY_EM[self.wav_SCI_str] * self.EM_gain /disp_SKY_smooth
         
         # Define time arrays
-        date_end = np.array(df_SCI_EM["Date-End"], dtype=np.datetime64)
-        if 'Date-Beg-Corr' in df_SCI_EM.columns:
-            date_beg = np.array(df_SCI_EM["Date-Beg-Corr"], dtype=np.datetime64)
-            date_end = np.array(df_SCI_EM["Date-End-Corr"], dtype=np.datetime64)
+        date_end = np.array(self.df_SCI_EM["Date-End"], dtype=np.datetime64)
+        if 'Date-Beg-Corr' in self.df_SCI_EM.columns:
+            date_beg = np.array(self.df_SCI_EM["Date-Beg-Corr"], dtype=np.datetime64)
+            date_end = np.array(self.df_SCI_EM["Date-End-Corr"], dtype=np.datetime64)
         else:
-            date_beg = np.array(df_SCI_EM["Date-Beg"], dtype=np.datetime64)
-            date_end = np.array(df_SCI_EM["Date-End"], dtype=np.datetime64)
+            date_beg = np.array(self.df_SCI_EM["Date-Beg"], dtype=np.datetime64)
+            date_end = np.array(self.df_SCI_EM["Date-End"], dtype=np.datetime64)
         tdur_sec = (date_end-date_beg).astype(float)/1000. # exposure duration in sec
         time_em  = (date_beg-date_beg[0]).astype(float)/1000. # seconds since beginning
-        int_SCI_spec         = df_SCI_EM_norm[:5].sum(axis=0) / np.sum(tdur_sec[:5]) # flux vs. wavelength per sec (use first five samples)
-        int_SKY_spec         = df_SKY_EM_norm[:5].sum(axis=0) / np.sum(tdur_sec[:5]) # flux vs. wavelength per sec (use first five samples)
+        int_SCI_spec = df_SCI_EM_norm[:5].sum(axis=0) / np.sum(tdur_sec[:5]) # flux vs. wavelength per sec (use first five samples)
+        int_SKY_spec = df_SKY_EM_norm[:5].sum(axis=0) / np.sum(tdur_sec[:5]) # flux vs. wavelength per sec (use first five samples)
 
         # Plot spectra
         plt.figure(figsize=(10, 4), tight_layout=True)
@@ -183,12 +205,12 @@ class AnalyzeEM:
         plt.xticks(fontsize=14)
         plt.yticks(fontsize=14)
         plt.xlim(445,870)
-        lns1 = ax1.plot(wav_SCI, int_SCI_spec, marker='.', color='k', label ='SCI',zorder = 1)
+        lns1 = ax1.plot(self.wav_SCI, int_SCI_spec, marker='.', color='k', label ='SCI',zorder = 1)
         ax2 = ax1.twinx()
-        lns2 = ax2.plot(wav_SKY, int_SKY_spec, marker='.', color='brown', label = 'SKY',zorder = 0, alpha = 0.5)
+        lns2 = ax2.plot(self.wav_SKY, int_SKY_spec, marker='.', color='brown', label = 'SKY',zorder = 0, alpha = 0.5)
         ax1.set_ylim(0,np.percentile(int_SCI_spec,99.9)*1.1)
         ax2.set_ylim(0,np.percentile(int_SKY_spec,99.9)*1.1)
-        plt.title('Exposure Meter Spectrum: ' + str(ObsID) + ' - ' + starname, fontsize=14)
+        plt.title('Exposure Meter Spectrum: ' + str(self.ObsID) + ' - ' + self.starname, fontsize=14)
         plt.yticks(fontsize=14, color='brown')
         ax1.set_xlabel("Wavelength (nm)",fontsize=14)
         ax1.set_ylabel("SCI Exposure Meter Flux (e-/nm/s)",fontsize=14)
