648a41accb21a3e12c08d57d9cc1ecced9472656
BJ Fulton <bjfultn@gmail.com>
2023-08-01 12:41:51 -0700

    tests working manually now

--- tests/regression/test_masters_recipe.py
+++ tests/regression/test_masters_recipe.py
@@ -21,7 +21,7 @@ master_wls_config = 'configs/wls_auto.cfg'
 def test_master_stacks():
     recipe_test(master_stacks_recipe, master_stacks_config, date_dir=masters_test_date)
     os.system(f'mkdir /data/masters/{masters_test_date}; mv -v /testdata/kpf_{masters_test_date}_master_*.fits /data/masters/{masters_test_date}/')
-
+    os.system(f'rm -vf /data/2D/{masters_test_date}/*.fits')
 
 def test_master_l1l2():
     recipe_test(master_l1l2_recipe, master_l1l2_config,
@@ -34,8 +34,8 @@ def test_master_wls():
 
 
 def main():
-    test_master_stacks()
-    test_master_l1l2()
+    # test_master_stacks()
+    # test_master_l1l2()
     test_master_wls()
 
 if __name__ == '__main__':
