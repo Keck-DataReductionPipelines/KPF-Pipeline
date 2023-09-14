2e7224d867a8d7fb17ff63a5ca7611b22785ee65
Andrew Howard <awhoward@gmail.com>
2023-09-14 16:06:42 -0700

    added L1b

--- modules/Utils/analyze_wls.py
+++ modules/Utils/analyze_wls.py
@@ -12,18 +12,20 @@ class AnalyzeWLS:
 
     Arguments:
         L1 - an L1 object
+        L1b (optiona) - a second L1 object to compare to L1
 
     Attributes:
         None so far
     """
 
-    def __init__(self, L1, logger=None):
+    def __init__(self, L1, L1b=None, logger=None):
         if self.logger:
             self.logger = logger
             self.logger.debug('Initializing AnalyzeWLS object.')
         else:
             self.logger = None
         self.L1 = L1
+        self.L1b = L1b
         primary_header = HeaderParse(L1, 'PRIMARY')
         self.header = primary_header.header
         self.name = primary_header.get_name()
