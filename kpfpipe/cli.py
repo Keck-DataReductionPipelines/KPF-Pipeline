9fc57022c49c9a33e2805f17bf8b0e4151dc1c03
BJ Fulton <bjfultn@gmail.com>
2023-10-05 10:21:04 -0700

    undo changes to config

--- kpfpipe/cli.py
+++ kpfpipe/cli.py
@@ -12,6 +12,7 @@ from datetime import datetime
 import time
 import threading
 import logging
+import tempfile
 from multiprocessing import Process, cpu_count
 
 from watchdog.observers import Observer
@@ -84,7 +85,7 @@ def worker(worker_num, pipeline_config, framework_logcfg_file, framework_config_
     # tells this instance to wait for something to happen, forever
     # qm_only=False, ingest_data_only=False,
     framework.pipeline.start(pipeline_config)
-    framework.start(wait_for_event=True, continuous=True)
+    framework.start(wait_for_event=False, continuous=False)
 
 
 class FileAlarm(PatternMatchingEventHandler):
@@ -172,7 +173,13 @@ def main():
     if args.reprocess:
         print(f"Setting queue manager to shutdown after reprocessing.")
         frame_config.set('DEFAULT', 'event_timeout', '5')
-        frame_config.set('DEFAULT', 'no_event_event', 'None')
+        frame_config.set('DEFAULT', 'no_event_event', 'exit')
+        frame_config.set('DEFAULT', 'no_event_wait_time', '60')
+
+    with tempfile.NamedTemporaryFile(mode='w', delete=False, suffix='cfg') as tp:
+        frame_config.write(tp)
+    
+    frame_config = tp.name
 
     # Using the multiprocessing library, create the specified number of instances
     if args.watch and args.ncpus > 1:
@@ -182,6 +189,7 @@ def main():
             p.start()
     else:
         frame_config = ConfigClass(framework_config)
+
 
     # Try to initialize the framework
     try:
