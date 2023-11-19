b6dad9adc7e69745440a33ab6eb5c9652476e8b4
Andrew Howard <awhoward@gmail.com>
2023-11-18 21:13:54 -0800

    started on DiagnosticsFramework

--- modules/quality_control/src/diagnostics_framework.py
+++ modules/quality_control/src/diagnostics_framework.py
@@ -1,6 +1,6 @@
 import ast
 import configparser as cp
-import modules.quality_control.src.quality_control as qc
+import modules.quality_control.src.diagnostics as diatnostics
 
 # Pipeline dependencies
 from kpfpipe.logger import *
@@ -10,29 +10,25 @@ from keckdrpframework.models.arguments import Arguments
 # Global read-only variables
 DEFAULT_CFG_PATH = 'modules/quality_control/configs/default.cfg'
 
-class QualityControlFramework(KPF0_Primitive):
+class DiagnosticsFramework(KPF0_Primitive):
 
     """
     Description:
-        Performs quality control on a FITS file.  Includes logic for automatically determining the data level.
+        Adds diagnostics information to FITS headers of KPF files.
 
     Arguments:
-        data_type (str): Type of data (e.g., KPF).
+        kpf_object (obj):
         data_level_str (str): L0, 2D, L1, L2 are possible choices.
-        fits_filename (str): Input FITS filename.
-        fits_object (str):
-        actual_dir (str): Prefix of actual directory outside container that maps to /data (e.g., /data/kpf)
+        diagnostics_name (str): name of diagnostics to add to headers
     """
 
     def __init__(self, action, context):
 
         KPF0_Primitive.__init__(self, action, context)
 
-        self.data_type = self.action.args[0]
-        self.data_level_str = self.action.args[1]
-        self.fits_filename = self.action.args[2]
-        self.fits_object = self.action.args[3]
-        self.qc_list_flag = self.action.args[4]
+        self.data_level_str = self.action.args[0]
+        self.kpf_object = self.action.args[1]
+        self.diagnostics_name = self.action.args[2]
 
         try:
             self.module_config_path = context.config_path['quality_control']
@@ -53,8 +49,8 @@ class QualityControlFramework(KPF0_Primitive):
         self.logger.info('Started {}'.format(self.__class__.__name__))
         self.logger.debug('module_config_path = {}'.format(self.module_config_path))
 
-        self.logger.info('self.data_type = {}'.format(self.data_type))
-        self.logger.info('self.fits_filename = {}'.format(self.fits_filename))
+        self.logger.info('self.data_level_str = {}'.format(self.data_type_str))
+        self.logger.info('self.diagnostics_name = {}'.format(self.diagnostics_name))
 
         module_config_obj = cp.ConfigParser()
         res = module_config_obj.read(self.module_config_path)
@@ -78,47 +74,24 @@ class QualityControlFramework(KPF0_Primitive):
             0 = Normal
         """
 
-        quality_control_exit_code = 0
+        diagnostics_exit_code = 0
 
-        # Perform quality control.
-
-        self.logger.info('Performing quality control on {}'.format(self.fits_filename))
-
+        # Measure Diagnostics.
         if 'L0' in self.data_level_str:
-            qc_obj = qc.QCL0(self.fits_object)
-            name = 'jarque_bera_test_red_amp1'
-            value = 3.14159256
-            qc_obj.add_qc_keyword_to_header(name,value)
+            pass
+            
         elif '2D' in self.data_level_str:
-            qc_obj = qc.QC2D(self.fits_object)
-            name = 'jarque_bera_test_red_amp1'
-            value = 3.14159256
-            qc_obj.add_qc_keyword_to_header(name,value)
+            if self.diagnostics_name == 'write_read_noise':
+                self.logger.info('Measuring diagnostics: {}'.format(self.diagnostics_name))
+                self.kpf_object = diagnostics.write_read_noise(self.kpf_object, logger=None)
+            
         elif 'L1' in self.data_level_str:
-
-            self.logger.info('self.data_level_str = {}'.format(self.data_level_str))
-
-            qc_obj = qc.QCL1(self.fits_object)
-
-            name = 'monotonic_wavelength_solution_check'
-            try:
-                qc_obj.add_qc_keyword_to_header_for_monotonic_wls(name)
-            except:
-                pass
-
+            pass
+            
         elif 'L2' in self.data_level_str:
-            qc_obj = qc.QCL2(self.fits_object)
-            name = 'jarque_bera_test_red_amp1'
-            value = 3.14159256
-            qc_obj.add_qc_keyword_to_header(name,value)
-
-        # Optionally list QC metrics.
-
-        if self.qc_list_flag == 1:
-            qc_obj.qcdefinitions.list_qc_metrics()
+            pass
 
         # Finish.
-
         self.logger.info('Finished {}'.format(self.__class__.__name__))
 
-        return Arguments([quality_control_exit_code, self.fits_object])
+        return Arguments([diagnostics_exit_code, self.kpf_object])
