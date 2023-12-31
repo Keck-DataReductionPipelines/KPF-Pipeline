16528a14588e9aa502903157047ae519dd45d0a4
Andrew Howard <awhoward@gmail.com>
2023-12-31 10:15:35 -0800

    updated help statement

--- scripts/add_to_tsdb.py
+++ scripts/add_to_tsdb.py
@@ -4,6 +4,22 @@ import sys
 from  modules.quicklook.src.analyze_time_series import AnalyzeTimeSeries
 
 def main():
+    """
+    Script Name: ingest_kpf_tsdb.py
+   
+    Description:
+      This script is used to ingest KPF observations into a KPF Time Series Database.
+
+    Options:
+      --help            Display this message
+   
+    Usage:
+      python kpf_processing_progress.py YYYYMMDD [YYYYMMDD] [--print_files] [--print_files_2D] [--print_files_L1] [--print_files_L2] [--touch_missing] [--check_version]
+   
+    Example:
+      python kpf_processing_progress.sh 20231114 20231231 --print_files
+    """
+
     if len(sys.argv) > 3:
         print("Usage: python add_to_tsdb.py start_date end_date db_path")
         sys.exit(1)
