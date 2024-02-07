3373f6f491e965f575c387e1da5f1d93edacc405
Andrew Howard <awhoward@gmail.com>
2024-02-07 00:34:45 -0800

    proper bash

--- scripts/launch_QLP.sh
+++ scripts/launch_QLP.sh
@@ -5,10 +5,11 @@
 data_levels=("L0" "2D" "L1" "L2" "masters")
 
 for lvl in "${data_levels[@]}"; do
-    if lvl=='L1':
-        ncpus=30
-    else:
+    if [ "$lvl" == "L1" ]; then
+        ncpus=20
+    else
         ncpus=10
     cmd="kpf --ncpus=${ncpus} --watch ${KPFPIPE_DATA}/${lvl}/ -r recipes/quicklook_watch.recipe -c configs/quicklook_watch.cfg"
-    $cmd &
+    fi
+    eval $cmd &
 done
