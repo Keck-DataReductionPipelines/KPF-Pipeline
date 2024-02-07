ea1f09652f774142e500f38037b96273e8852371
Andrew Howard <awhoward@gmail.com>
2024-02-07 00:17:59 -0800

    increaes Ncpu for L1, for now

--- scripts/launch_QLP.sh
+++ scripts/launch_QLP.sh
@@ -6,7 +6,7 @@ data_levels=("L0" "2D" "L1" "L2" "masters")
 
 for lvl in "${data_levels[@]}"; do
     if lvl=='L1':
-        ncpus=20
+        ncpus=30
     else:
         ncpus=10
     cmd="kpf --ncpus=${ncpus} --watch ${KPFPIPE_DATA}/${lvl}/ -r recipes/quicklook_watch.recipe -c configs/quicklook_watch.cfg"
