820882130ea2660d967214f7f2f1341a704e6bde
Russ Laher <laher@ipac.caltech.edu>
2024-02-20 08:08:44 -0800

    Initical check-in.

---
+++ c/src/imheaders/imheaders.h
@@ -0,0 +1,17 @@
+/* $Id: imheaders.h,v 1.1 2013/01/24 20:53:23 laher Exp $ */
+
+
+/* Constants. */
+
+#define CODENAME "imheaders"
+#define VERSION  1.0
+
+#define TERMINATE_SUCCESS  0
+#define TERMINATE_WARNING  32
+#define TERMINATE_FAILURE  64
+
+
+/* Prototypes. */
+
+int printUsage( char codeName[], char version[] );
+
