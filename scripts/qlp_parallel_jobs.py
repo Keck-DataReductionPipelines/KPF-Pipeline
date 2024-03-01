7a50df0e0e88b968429f7055f234b09cc48a9de2
Andrew Howard <awhoward@gmail.com>
2024-03-01 10:00:45 -0800

    added docstring

--- scripts/qlp_parallel_jobs.py
+++ scripts/qlp_parallel_jobs.py
@@ -13,16 +13,30 @@ def main(start_date, end_date, l0, d2, l1, l2, ncpu):
     Script Name: qlp_parallel_jobs.py
    
     Description:
-      
+      This script uses the 'parallel' utility to execute the recipe called 
+      'recipes/quicklook_match.recipe' to generate standard Quicklook data 
+      products.  The script selects all KPF files based on their
+      type (L0/2D/L1/L2) from the standard data directory using a date range
+      specified by the parameters start_date and end_date.  L0 files are 
+      included if the --l0 flag is set or none of the --l0, --2d, --l1, --l2
+      flags are set (in which case all data types are included).  The --2d, 
+      --l1, and --l2 flags have similar functions.  
 
     Options:
+      --help        Display this message
+      --start_date  Start date as YYYYMMDD, YYYYMMDD.SSSSS, or YYYYMMDD.SSSSS.SS
+      --end_date    End date as YYYYMMDD, YYYYMMDD.SSSSS, or YYYYMMDD.SSSSS.SS
+      --ncpu        Number of cores used for paralle processing; default=10
+      --l0          Select all L0 files in date range
+      --2d          Select all 2D files in date range
+      --l1          Select all L1 files in date range
+      --l2          Select all L2 files in date range
    
-
     Usage:
-      python kpf_processing_progress.py YYYYMMDD.SSSSS YYYYMMDD.SSSSS --all --L0 --2D --L1 --L2
+      python kpf_processing_progress.py YYYYMMDD.SSSSS YYYYMMDD.SSSSS --ncpu NCPU --l0 --2d --l1 --l2
     
     Example:
-      ./scripts/qlp_parallel_jobs.py 20230101.12345.67 20230101.17
+      ./scripts/qlp_parallel_jobs.py 20230101.12345.67 20230101.17 --ncpu 50 --l0 --2d
     """
 
     if start_date.count('.') == 2:
@@ -63,7 +77,7 @@ def main(start_date, end_date, l0, d2, l1, l2, ncpu):
     sorted_indices = np.argsort([file.split('/')[-1] for file in filtered_files])
     sorted_paths = filtered_files[sorted_indices]
     sorted_files = sorted_paths.tolist()
-    print(f"Number of files for parallel Quicklook processing: {len(filtered_files)}")
+    print(f"Number of files queued for parallel Quicklook processing: {len(filtered_files)}")
 
     # Create a temporary file and write the sorted file paths to it
     with tempfile.NamedTemporaryFile(mode='w+', delete=False) as tmpfile:
@@ -71,8 +85,6 @@ def main(start_date, end_date, l0, d2, l1, l2, ncpu):
         for file_path in sorted_files:
             tmpfile.write(file_path + '\n')
     command = f"""bash -c "parallel -j {ncpu} -k --bar bash -c 'echo \\"Starting Quicklook instance {{}}\\"; config=\$(mktemp) && sed \\"s|INSERT_FITS_PATH|{{}}|\\" configs/quicklook_parallel.cfg > \\"\\$config\\" && kpf -c \\"\\$config\\" -r recipes/quicklook_match.recipe && rm \\"\\$config\\"' :::: {tmpfile_name}" """
-    # note: this version of the command prints on the verbose output with every line from the logger
-    #command = f"""bash -c "parallel -j {ncpu} -k --bar --ungroup bash -c 'echo \\"Starting Quicklook instance {{}}\\"; config=\$(mktemp) && sed \\"s|INSERT_FITS_PATH|{{}}|\\" configs/quicklook_parallel.cfg > \\"\\$config\\" && kpf -c \\"\\$config\\" -r recipes/quicklook_match.recipe && rm \\"\\$config\\"' :::: {tmpfile_name}" """
     
     try:
         subprocess.run(command, shell=True, check=True)
@@ -81,14 +93,15 @@ def main(start_date, end_date, l0, d2, l1, l2, ncpu):
 
 
 if __name__ == "__main__":
+    sys.argv = [arg.lower() if arg.startswith('--') else arg for arg in sys.argv] # make command line flags case insensitive
     parser = argparse.ArgumentParser(description='Launch Quicklook pipeline for selected observations.')
     parser.add_argument('start_date', type=str, help='Start date as YYYYMMDD, YYYYMMDD.SSSSS, or YYYYMMDD.SSSSS.SS')
     parser.add_argument('end_date',  type=str, help='End date as YYYYMMDD, YYYYMMDD.SSSSS, or YYYYMMDD.SSSSS.SS')
     parser.add_argument('--ncpu', type=str, default=10, help='Number of cores for parallel processing')
-    parser.add_argument('--l0',   action='store_true', help='Select all L0 files')
-    parser.add_argument('--2d',   action='store_true', dest='d2', help='Select all 2D files')
-    parser.add_argument('--l1',   action='store_true', help='Select all L1 files')
-    parser.add_argument('--l2',   action='store_true', help='Select all L2 files')
+    parser.add_argument('--l0', action='store_true', help='Select all L0 files in date range')
+    parser.add_argument('--2d', action='store_true', dest='d2', help='Select all 2D files in date range')
+    parser.add_argument('--l1', action='store_true', help='Select all L1 files in date range')
+    parser.add_argument('--l2', action='store_true', help='Select all L2 files in date range')
 
     args = parser.parse_args()
     main(args.start_date, args.end_date, args.l0, args.d2, args.l1, args.l2, args.ncpu)
@@ -96,4 +109,4 @@ if __name__ == "__main__":
     # to-do: check if the script is being run in Docker
     # to-do: start_date > end_date and stop, or flip them
     # to-do: add a mode where it will just print out the file names
-    # to-do check if there are no matching files and stop if so
+    # to-do: check if there are no matching files and stop if so
