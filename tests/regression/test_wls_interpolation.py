2d4a46eecdc2086aee682327b116a708472775cb
Andrew Howard <awhoward@gmail.com>
2024-04-16 11:02:11 -0700

    put files in /data/reference_fits/

--- tests/regression/test_wls_interpolation.py
+++ tests/regression/test_wls_interpolation.py
@@ -1,20 +1,17 @@
 """
 Test of interpolation between two wavelength solutions.
 """
-import os
 from kpfpipe.tools.recipe_test_unit import recipe_test
 
-this_recipe = """from modules.Utils.string_proc import date_from_kpffile
-from modules.wavelength_cal.src.wavelength_cal import WaveInterpolate
+this_recipe = """from modules.wavelength_cal.src.wavelength_cal import WaveInterpolate
 
 data_type = 'KPF'
-wls1_file = os.getenv('KPFPIPE_DATA') + '/masters/20240101/kpf_20240101_master_WLS_autocal-lfc-all-eve_L1.fits'
-wls2_file = os.getenv('KPFPIPE_DATA') + '/masters/20240101/kpf_20240101_master_WLS_autocal-lfc-all-morn_L1.fits'
+wls1_file = '/data/reference_fits/kpf_20240101_master_WLS_autocal-lfc-all-eve_L1.fits'
+wls2_file = '/data/reference_fits/kpf_20240101_master_WLS_autocal-lfc-all-morn_L1.fits'
 wls1_l1 = kpf1_from_fits(wls1_file, data_type=data_type)
 wls2_l1 = kpf1_from_fits(wls2_file, data_type=data_type)
 obsid_l1 = 'KP.20240101.24368.88'
-datecode_l1 = date_from_kpffile(obsid_l1)
-l1_file = os.getenv('KPFPIPE_DATA') + '/L1/' + datecode_l1 + '/' + obsid_l1 + '_L1.fits'
+l1_file = '/data/reference_fits/' + obsid_l1 + '_L1.fits'
 l1 = kpf1_from_fits(l1_file, data_type=data_type)
 l1_out = WaveInterpolate(wls1_l1, wls2_l1, l1)
 """
