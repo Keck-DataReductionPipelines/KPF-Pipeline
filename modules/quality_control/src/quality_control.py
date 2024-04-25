d2ac7e5ac99e419a82ce4889766b5ce75d041d82
aaronhouseholder <70820831+aaronhouseholder@users.noreply.github.com>
2024-04-25 10:48:41 -0400

    Add new qc test

--- modules/quality_control/src/quality_control.py
+++ modules/quality_control/src/quality_control.py
@@ -189,6 +189,16 @@ class QCDefinitions:
         self.fits_comments[name7] = 'QC: EM not negative flux'
         self.db_columns[name7] = None
 
+        name8 = 'lfc_flux_check'
+        self.names.append(name8)
+        self.kpf_data_levels[name8] = ['L0']
+        self.descriptions[name8] = 'Check if an LFC frame that goes into a masters has enough flux'
+        self.data_types[name8] = 'int'
+        self.spectrum_types[name7] = ['all', ]
+        self.fits_keywords[name8] = 'LFC2DFOK'
+        self.fits_comments[name8] = 'QC: LFC Flux not below threshold'
+        self.db_columns[name8] = None
+        
         # Integrity checks
         if len(self.names) != len(self.kpf_data_levels):
             raise ValueError("Length of kpf_data_levels list does not equal number of entries in descriptions dictionary.")
