52eb0f37f0c027eae3c0ed21dfab937d07082055
Andrew Howard <awhoward@gmail.com>
2024-05-29 14:55:43 -0700

    Revert

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,7 +43,6 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx_rtd_theme',
     'sphinxcontrib.napoleon',
     'nbsphinx',
-    'nbsphinx_link',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
