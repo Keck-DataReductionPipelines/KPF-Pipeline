616aa3c1854d3e1287b5c09a5a32cbad8b5babc9
Andrew Howard <awhoward@gmail.com>
2024-05-29 14:23:30 -0700

    try again with the Jupyter Notebook linking

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.githubpages',
     'sphinx_rtd_theme',
     'sphinxcontrib.napoleon',
-    'nbsphinx'
+    'nbsphinx',
+    'nbsphinx-link'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
