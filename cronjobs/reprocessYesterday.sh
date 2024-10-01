566e0e0c70d8595756dd2f89b3b845dfab7b3391
BJ Fulton <bjfultn@gmail.com>
2024-10-01 14:40:31 -0700

    update reprocess yesterday script

--- cronjobs/reprocessYesterday.sh
+++ cronjobs/reprocessYesterday.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export DATE = `/usr/bin/date -d "yesterday" +%Y%m%d`
-/scr/doppler/production/KPF-Pipeline/scripts/kpf_slowtouch.sh -e -d /data/L0/${DATE}/ >> /scr/doppler/kpf.cron 2>&1
+export DATE=`/usr/bin/date -d "yesterday" +%Y%m%d`
+/scr/doppler/production/KPF-Pipeline/scripts/kpf_slowtouch.sh -e -d /data/kpf/L0/${DATE}/
