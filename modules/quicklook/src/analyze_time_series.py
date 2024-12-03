28e297c80d9e58145edfc63330f2ec381a90e8da
Andrew Howard <awhoward@gmail.com>
2024-12-02 17:43:08 -0800

    bugfix for L2 ingestion

--- modules/quicklook/src/analyze_time_series.py
+++ modules/quicklook/src/analyze_time_series.py
@@ -309,14 +309,14 @@ class AnalyzeTimeSeries:
 
             # If any associated file has been updated, proceed
             if self.is_any_file_updated(L0_file_path):
-                L0_header_data = self.extract_kwd(L0_file_path,       self.L0_header_keyword_types, extension='PRIMARY')   
-                D2_header_data = self.extract_kwd(D2_file_path,       self.D2_header_keyword_types, extension='PRIMARY')   
-                L1_header_data = self.extract_kwd(L1_file_path,       self.L1_header_keyword_types, extension='PRIMARY')   
-                L2_header_data = self.extract_kwd(L2_file_path,       self.L2_header_keyword_types, extension='PRIMARY')   
-                L2_header_data = self.extract_kwd(L2_file_path,       self.L2_RV_header_keyword_types, extension='RV')   
-                L0_telemetry   = self.extract_telemetry(L0_file_path, self.L0_telemetry_types) 
+                L0_header_data  = self.extract_kwd(L0_file_path,       self.L0_header_keyword_types, extension='PRIMARY')   
+                D2_header_data  = self.extract_kwd(D2_file_path,       self.D2_header_keyword_types, extension='PRIMARY')   
+                L1_header_data  = self.extract_kwd(L1_file_path,       self.L1_header_keyword_types, extension='PRIMARY')   
+                L2_header_data1 = self.extract_kwd(L2_file_path,       self.L2_header_keyword_types, extension='PRIMARY')   
+                L2_header_data2 = self.extract_kwd(L2_file_path,       self.L2_RV_header_keyword_types, extension='RV')   
+                L0_telemetry    = self.extract_telemetry(L0_file_path, self.L0_telemetry_types) 
 
-                header_data = {**L0_header_data, **D2_header_data, **L1_header_data, **L2_header_data, **L0_telemetry}
+                header_data = {**L0_header_data, **D2_header_data, **L1_header_data, **L2_header_data1, **L2_header_data2, **L0_telemetry}
                 header_data['ObsID'] = base_filename
                 header_data['datecode'] = get_datecode(base_filename)
                 header_data['L0_filename'] = os.path.basename(L0_file_path)
@@ -840,7 +840,8 @@ class AnalyzeTimeSeries:
             end_date = (start_date + pd.offsets.MonthEnd(0) + timedelta(days=1) - timedelta(seconds=0.1))
             df = df[(df['DATE-BEG'] >= start_date) & (df['DATE-BEG'] <= end_date)]
             df['DAY'] = df['DATE-BEG'].dt.day    
-            full_range = pd.date_range(start=f'{start_date.year}-{start_date.month}-{start_date.day}', end=f'{end_date.year}-{end_date.month}-{end_date.day}', freq='D')
+            #full_range = pd.date_range(start=f'{start_date.year}-{start_date.month}-{start_date.day}', end=f'{end_date.year}-{end_date.month}-{end_date.day}', freq='D')
+            full_range = range(1, end_date.day + 1) 
             entry_counts = df['DAY'].value_counts().sort_index()
             entry_counts = entry_counts.reindex(full_range, fill_value=0)
             major_locator = DayLocator()
