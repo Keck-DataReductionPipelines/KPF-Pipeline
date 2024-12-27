ca07f1fe661d70f3a0b9c81d847ca7e2667623d4
Andrew Howard <awhoward@gmail.com>
2024-12-27 00:07:42 -0800

    import all

--- static/tsdb_plot_configs/__init__.py
+++ static/tsdb_plot_configs/__init__.py
@@ -1,1 +1,27 @@
+import os
+import importlib
 
+__all__ = []
+
+# Get the absolute path to this directory
+this_dir = os.path.dirname(__file__)
+
+# Enumerate everything in the current directory
+for name in os.listdir(this_dir):
+    subdir_path = os.path.join(this_dir, name)
+    
+    # Check if it's a directory AND has an __init__.py (i.e., a subpackage)
+    init_file = os.path.join(subdir_path, "__init__.py")
+    if os.path.isdir(subdir_path) and os.path.isfile(init_file):
+        # Dynamically import the subpackage
+        module_name = f"static_files.tsdb_plot_configs.{name}"
+        
+        # Import the subpackage using importlib
+        subpkg = importlib.import_module(module_name)
+        
+        # Make it available as an attribute of this package
+        # e.g. tsdb_plot_configs.ccds => from ... import ccds
+        globals()[name] = subpkg
+        
+        # Also, add it to __all__ so it’s included in "from ... import *"
+        __all__.append(name)
