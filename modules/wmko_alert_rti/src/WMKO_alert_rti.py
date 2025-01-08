a7a1355c6b4d39bcb715723de9eb614c1b5aba4c
Max Brodheim <max.brodheim@gmail.com>
2025-01-08 11:18:50 -1000

    clean up http code

--- modules/wmko_alert_rti/src/WMKO_alert_rti.py
+++ modules/wmko_alert_rti/src/WMKO_alert_rti.py
@@ -1,12 +1,3 @@
-"""
-    The file contains primitive KPFModeExample
-
-    Attributes:
-       KPFModeExample: primitive for simmple recipe test
-
-
-"""
-
 import time
 import configparser
 import os
@@ -23,11 +14,16 @@ DEFAULT_CFG_PATH = 'modules/wmko_alert_rti/configs/default.cfg'
 
 class SendRTIHttp(KPF_Primitive):
     """
-    This module sends the HTTP GET request needed by KOA RTI to archive new data products
-
     Description:
-         - `action (keckdrpframework.models.action.Action)`: `action.args` contains positional arguments and
-                  keyword arguments passed by the `SendRTIHttp` event issued in the recipe:
+        Sends the HTTP GET request needed by KOA RTI to archive new
+        data products. This module is intended to be used only at WMKO.
+        The call to this module will also fail if no RTI credentials are
+        provided in the configuration file.
+
+    Arguments:
+        input_file (str): The path to the file to be ingested. This filename
+        is used to extract the KOAID.
+        output_dir (str): The directory where the file is stored.
     """
 
 def __init__(self,
@@ -46,56 +42,90 @@ def __init__(self,
     except:
         self.config_path = DEFAULT_CFG_PATH
     self.config.read(self.config_path)
-    
-    
+
     if not self.logger:
         self.logger = self.context.logger
     self.logger.info('Loading config form: {}'.format(self.config_path))
 
+    # Load arguments into atributtes
+    self.input_file = self.action.args[0]
+    self.output_dir = self.action.args[1]
+
 
 def _pre_condition(self) -> bool:
+    """Precondition for the SendRTIHttp primitive. If the RTI URL, user, and
+    password are not provided in the config file, then the precondition fails.
+
+    Returns
+    -------
+    bool
+        True if the RTI URL, user, and password are provided in the config file.
+    """
+    if self.config['RTI']['rti_url'] is None:
+        self.logger.error("No RTI URL specified in config file.")
+        self.logger.error("Please add RTI URL to config file.")
+        return False
+    if self.config['RTI']['rti_user'] is None:
+        self.logger.error("No RTI user specified in config file.")
+        self.logger.error("Please add RTI user and password to config file.")
+        return False
+    if self.config['RTI']['rti_pass'] is None:
+        self.logger.error("No RTI password specified in config file.")
+        self.logger.error("Please add RTI user and password to config file.")
+        return False
     return True
 
 def _post_condition(self) -> bool:
     return True
 
 def _perform(self):
-            
-    data_directory = os.path.join(self.config.instrument.cwd,
-                                self.config.instrument.output_directory)
     
-    self.logger.info(f"Alerting RTI that {self.action.args.name} is ready for ingestion")
+    self.logger.info(f"Alerting RTI that {self.input_file} is ready for ingestion")
 
-    url = self.config.rti.rti_url
+    url = self.config['RTI']['rti_url']
+    koaid = os.path.basename(self.input_file).split('.fits')[0]
     data = {
-        'instrument': 'KCWI',
-        'koaid': self.action.args.ccddata.header['KOAID'],
-        'ingesttype': self.config.rti.rti_ingesttype,
-        'datadir': str(data_directory),
-        'start': str(self.action.args.ingest_time),
-        'reingest': self.config.rti.rti_reingest,
-        'testonly': self.config.rti.rti_testonly,
-        'dev': self.config.rti.rti_dev
+        'instrument': 'KPF',
+        'koaid': koaid,
+        'ingesttype': self.config['RTI']['rti_ingesttype'],
+        'datadir': str(self.output_dir),
+        'start': None,
+        'reingest': self.config['RTI']['rti_reingest'],
+        'testonly': self.config['RTI']['rti_testonly'],
+        'dev': self.config['RTI']['rti_dev']
     }
     
     attempts = 0
-    limit = self.config.rti.rti_attempts
+    limit = int(self.config['rti']['rti_attempts'])
     while attempts < limit:
         res = self.get_url(url, data)
         if res is None:
-            t = self.config.rti.rti_retry_time
+            t = self.config['rti']['rti_retry_time']
             attempts += 1
             self.logger.error(f"Waiting {t} seconds to attempt again... ({attempts}/{limit})")
             time.sleep(t)
         else:
             self.logger.info(f"Post returned status code {res.status_code}")
-            return self.action.args
+            return Arguments([self.input_file, self.output_dir])
     
     self.logger.error(f"Post attempted {limit} times and got no response.")
-    self.logger.error("Aborting.")
-    return self.action.args
+    return Arguments([self.input_file, self.output_dir])
 
 def get_url(self, url, data):
+    """Sends a get request to the specified URL with the specified data.
+
+    Parameters
+    ----------
+    url : str
+        URL to send the request to.
+    data : dict
+        dictionary of key/value pairs to send as parameters in the request.
+
+    Returns
+    -------
+    requests.Response
+        Response object from the request, or None if an error occurred.
+    """
     try:
         res = requests.get(url, params = data, auth=(
                                                     self.user,
