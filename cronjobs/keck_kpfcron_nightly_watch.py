456df0cdae58c48a4c35ba7f5c856aea98af88e1
= <=>
2025-01-10 15:07:06 -1000

    fixed issue with slow touch dir

--- cronjobs/keck_kpfcron_nightly_watch.py
+++ cronjobs/keck_kpfcron_nightly_watch.py
@@ -61,7 +61,7 @@ class KPFPipeNightly(KPFPipeCronBase):
             make init >> {self.stdout_log} 2>&1;
    
             # touch any files currently in the directory so the pipe recognized them as new
-            python /code/KPF-Pipeline/cronjobs/keck_slow_touch.py --date {self.procdate} --log /data/logs/ &
+            python /code/KPF-Pipeline/cronjobs/keck_slow_touch.py --date {self.procdate} --fits /data_workspace/L0 --log /data/logs/ &
 
             # start the infinite process to watch for new data
             kpf --watch /data/L0/{self.procdate}/ --ncpus={self.ncpu} -r {self.recipe} -c {self.config} >> {self.stdout_log} 2>&1;
