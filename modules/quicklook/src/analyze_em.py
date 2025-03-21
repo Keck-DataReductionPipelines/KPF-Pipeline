18acefa1b38c1b9f4b7c874cacba7bd390b248d8
Andrew Howard <awhoward@gmail.com>
2025-03-21 15:30:09 -0700

    bugfix for Astropy tables

--- modules/quicklook/src/analyze_em.py
+++ modules/quicklook/src/analyze_em.py
@@ -125,12 +125,14 @@ class AnalyzeEM:
         self.ind_551_658 = np.where((self.wav_SCI >= self.wav1) & (self.wav_SCI < self.wav2))
         self.ind_658_764 = np.where((self.wav_SCI >= self.wav2) & (self.wav_SCI < self.wav3))
         self.ind_764p    = np.where((self.wav_SCI >= self.wav3))
-        self.counts_SCI         = self.df_SCI_EM.sum(axis=1)                         # flux (ADU) vs. time (per sample)
+
+        cols_to_exclude = ['Date-Beg', 'Date-Beg-Corr','Date-End', 'Date-End-Corr'] # for sums below
+        self.counts_SCI         = self.df_SCI_EM.drop(columns=cols_to_exclude).sum(axis=1) # flux (ADU) vs. time (per sample)
         self.counts_SCI_551m    = self.df_SCI_EM[self.wav_SCI_str[np.where((self.wav_SCI <  self.wav1))]].sum(axis=1)
         self.counts_SCI_551_658 = self.df_SCI_EM[self.wav_SCI_str[np.where((self.wav_SCI >= self.wav1) & (self.wav_SCI < self.wav2))]].sum(axis=1)
         self.counts_SCI_658_764 = self.df_SCI_EM[self.wav_SCI_str[np.where((self.wav_SCI >= self.wav2) & (self.wav_SCI < self.wav3))]].sum(axis=1)
         self.counts_SCI_764p    = self.df_SCI_EM[self.wav_SCI_str[np.where((self.wav_SCI >= self.wav3))]].sum(axis=1)
-        self.counts_SKY         = self.df_SKY_EM.sum(axis=1)                         # flux (ADU) vs. time (per sample)
+        self.counts_SKY         = self.df_SKY_EM.drop(columns=cols_to_exclude).sum(axis=1) # flux (ADU) vs. time (per sample)
         self.counts_SKY_551m    = self.df_SKY_EM[self.wav_SKY_str[np.where((self.wav_SKY <  self.wav1))]].sum(axis=1)
         self.counts_SKY_551_658 = self.df_SKY_EM[self.wav_SKY_str[np.where((self.wav_SKY >= self.wav1) & (self.wav_SKY < self.wav2))]].sum(axis=1)
         self.counts_SKY_658_764 = self.df_SKY_EM[self.wav_SKY_str[np.where((self.wav_SKY >= self.wav2) & (self.wav_SKY < self.wav3))]].sum(axis=1)
