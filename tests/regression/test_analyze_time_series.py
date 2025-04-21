469d562fee37862cd3e3fa851cadf26a76c849c1
Andrew Howard <awhoward@gmail.com>
2025-04-21 09:25:04 -0700

    update multitable db again

--- tests/regression/test_analyze_time_series.py
+++ tests/regression/test_analyze_time_series.py
@@ -64,7 +64,7 @@ def test_analyze_time_series():
     # Test miscellaneous methods
     columns = ['ObsID','GDRXRMS','FIUMODE']
     myTS.db.display_dataframe_from_db(columns)
-    df = myTS.dataframe_from_db(columns=columns)
+    df = myTS.db.dataframe_from_db(columns=columns)
     myTS.db.ObsIDlist_from_db('autocal-bias')
     myTS.db.drop_table()
     
