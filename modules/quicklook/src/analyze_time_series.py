0d31399fc473336ebdab0c951fb348d0f33f3a6f
Andrew Howard <awhoward@gmail.com>
2025-04-30 17:48:28 -0700

    fixed only_object

--- modules/quicklook/src/analyze_time_series.py
+++ modules/quicklook/src/analyze_time_series.py
@@ -1493,7 +1493,7 @@ class AnalyzeTimeSeries:
                 axs[p].xaxis.set_major_locator(ticker.MaxNLocator(7, prune=None))
             if p == npanels-1: 
                 axs[p].set_xlabel(xtitle, fontsize=14)
-                axs[0].set_title(thistitle, fontsize=16)
+                axs[0].set_title(thistitle, fontsize=18)
             if 'ylabel' in thispanel['paneldict']:
                 axs[p].set_ylabel(thispanel['paneldict']['ylabel'], fontsize=14)
             axs[p].grid(color='lightgray')        
@@ -2152,7 +2152,7 @@ class AnalyzeTimeSeries:
             plt.ylabel(dict_ylabel, fontsize=14)
         else:
             plt.ylabel("Number of Observations", fontsize=14)
-        plt.title(plot_title, fontsize=16)
+        plt.title(plot_title, fontsize=18)
     
         ax = plt.gca()
         ax.xaxis.set_major_locator(major_locator)
