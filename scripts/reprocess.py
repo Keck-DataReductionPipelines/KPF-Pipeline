10a8f516c801751941c7a852d69e19dd49725d1e
Andrew Howard <awhoward@gmail.com>
2025-05-12 20:54:48 -0700

    add nice option

--- scripts/reprocess.py
+++ scripts/reprocess.py
@@ -19,10 +19,11 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Reprocess datecoded directories.')
     parser.add_argument('startdate', type=str, help='Start date in YYYYMMDD format')
     parser.add_argument('enddate', type=str, help='End date in YYYYMMDD format')
-    parser.add_argument('--ncpus', type=int, default=max(1, multiprocessing.cpu_count() // 2),
+    parser.add_argument('--ncpu', type=int, default=max(1, multiprocessing.cpu_count() // 2),
                         help='Number of CPUs to use')
     parser.add_argument('--logfile', type=str, default='reprocess.log', help='Log file path')
     parser.add_argument('--dry-run', action='store_true', help='Print commands without executing them')
+    parser.add_argument('--nonice', action='store_true', help='Do not apply nice (priority) modifications')
     return parser.parse_args()
 
 
@@ -42,12 +43,14 @@ def main():
 
     if not log_exists:
         with open(args.logfile, 'w') as f:
-            f.write(f"{'Datecode':<10}  {'Start Time':<19}  {'End Time':<19}  {'Version':<10}\n")
+            f.write(f"{'Datecode':<10}  {'Start Time':<19}  {'End Time':<19}  {'Compute Time':<15}  {'Version':<10}\n")
 
     start_date = datetime.datetime.strptime(args.startdate, '%Y%m%d')
     end_date = datetime.datetime.strptime(args.enddate, '%Y%m%d')
 
-    dates = list(daterange(start_date, end_date))
+    dates = list(daterange(start_date, end_date))[::-1]  # reversed order
+
+    nice_prefix = [] if args.nonice else ['nice', '-n', '10']
 
     for single_date in tqdm(dates, desc="Reprocessing Dates"):
         datecode = single_date.strftime('%Y%m%d')
@@ -65,29 +68,29 @@ def main():
         ]
 
         cmd_kpf = [
-            'kpf', '--ncpus', str(args.ncpus), '--watch',
-            src_dir, '--reprocess', '-c', 'configs/kpf_drp.cfg', '-r', 'recipes/kpf_drp.recipe'
+            'kpf', '--ncpu', str(args.ncpu), '--reprocess',
+            src_dir, '-c', 'configs/kpf_drp.config', '-r', 'recipes/kpf_drp.recipe'
         ]
 
         if args.dry_run:
             for cmd_rm in cmds_rm:
-                print(' '.join(cmd_rm))
-            print(' '.join(cmd_kpf))
+                print(' '.join(nice_prefix + cmd_rm))
+            print(' '.join(nice_prefix + cmd_kpf))
         else:
             for cmd_rm in cmds_rm:
-                print(' '.join(cmd_rm))
-                subprocess.run(cmd_rm, check=False)
-
-            print(' '.join(cmd_kpf))
+                subprocess.run(nice_prefix + cmd_rm, check=False)
 
             start_time = datetime.datetime.now()
-            result = subprocess.run(cmd_kpf)
+            result = subprocess.run(nice_prefix + cmd_kpf)
             end_time = datetime.datetime.now()
 
+            compute_time = end_time - start_time
+            compute_time_str = str(compute_time).split('.')[0]  # remove microseconds
+
             if result.returncode == 0:
-                logging.info(f"{datecode:<10}  {start_time.strftime('%Y-%m-%d %H:%M:%S')}  {end_time.strftime('%Y-%m-%d %H:%M:%S')}  {__version__:<10}")
+                logging.info(f"{datecode:<10}  {start_time.strftime('%Y-%m-%d %H:%M:%S')}  {end_time.strftime('%Y-%m-%d %H:%M:%S')}  {compute_time_str:<15}  {__version__:<10}")
             else:
-                logging.info(f"{datecode:<10}  {start_time.strftime('%Y-%m-%d %H:%M:%S')}  {end_time.strftime('%Y-%m-%d %H:%M:%S')}  FAILED    {__version__:<10}")
+                logging.info(f"{datecode:<10}  {start_time.strftime('%Y-%m-%d %H:%M:%S')}  {end_time.strftime('%Y-%m-%d %H:%M:%S')}  {compute_time_str:<15}  FAILED    {__version__:<10}")
                 print(f"Error processing date {datecode}", file=sys.stderr)
 
 
