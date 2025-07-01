ea4221745aaecbfc683720c697200c01ad545ed7
Greg Gilbert <gjgilbert@astro.ucla.edu>
2025-06-30 17:25:10 -0700

    recipe tests for blaze and stray light

--- tests/regression/test_stray_light_recipe.py
+++ tests/regression/test_stray_light_recipe.py
@@ -1,12 +1,12 @@
 from kpfpipe.config.pipeline_config import ConfigClass
 from kpfpipe.tools.recipe_test_unit import recipe_test
 
-recipe = open('tests/recipes/test_blaze.recipe', 'r').read()
-config = 'tests/configs/test_blaze.cfg'
+recipe = open('tests/recipes/test_stray_light.recipe', 'r').read()
+config = 'tests/configs/test_stray_light.cfg'
 date_dir = '20241022'
 
-def test_blaze_recipe():
+def test_stray_light_recipe():
     recipe_test(recipe, config, date_dir=date_dir)
 
 if __name__ == '__main__':
-    test_blaze_recipe()
+    test_stray_light_recipe()
