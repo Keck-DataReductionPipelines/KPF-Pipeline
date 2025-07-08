55417a0569bf31535c0c92b39c92be089b855aef
Andrew Howard <awhoward@gmail.com>
2025-07-08 11:29:18 -0700

    Update reprocessYesterday.sh

--- cronjobs/reprocessYesterday.sh
+++ cronjobs/reprocessYesterday.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export DATE=`/usr/bin/date -s 15 -d "yesterday" +%Y%m%d`
+export DATE=`/usr/bin/date -s 20 -d "yesterday" +%Y%m%d`
 /scr/doppler/production/KPF-Pipeline/scripts/kpf_slowtouch.sh -d /data/kpf/L0/${DATE}/
 
-export DATE=`/usr/bin/date -s 15 -d "2 days ago" +%Y%m%d`
+export DATE=`/usr/bin/date -s 20 -d "2 days ago" +%Y%m%d`
 /scr/doppler/production/KPF-Pipeline/scripts/kpf_slowtouch.sh -d /data/kpf/L0/${DATE}/
