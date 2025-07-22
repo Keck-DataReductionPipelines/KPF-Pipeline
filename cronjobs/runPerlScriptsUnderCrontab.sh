c51b56e11737ea5be96d038c85a15bf6657ac564
Russ Laher <laher@ipac.caltech.edu>
2025-07-22 13:15:41 -0700

    Modified to run cleanup script inside container.

--- cronjobs/runPerlScriptsUnderCrontab.sh
+++ cronjobs/runPerlScriptsUnderCrontab.sh
@@ -9,5 +9,5 @@ echo "Processing date: $procdate"
 $KPFCRONJOB_CODE/cronjobs/updateSoftwareAndReferenceFits.pl >& $KPFCRONJOB_LOGS/jobs/updateSoftwareAndReferenceFits_$procdate.out
 echo Return value from updateSoftwareAndReferenceFits.pl = $?
 
-$KPFCRONJOB_CODE/cronjobs/cleanOldFilesFromDisk.pl >& $KPFCRONJOB_LOGS/jobs/cleanOldFilesFromDisk_$procdate.out
-echo Return value from cleanOldFilesFromDisk.pl = $?
+$KPFCRONJOB_CODE/cronjobs/runCleanOldFilesFromDiskInContainer.pl $procdate >& $KPFCRONJOB_LOGS/jobs/runCleanOldFilesFromDiskInContainer_$procdate.out
+echo Return value from runCleanOldFilesFromDiskInContainer.pl = $?
