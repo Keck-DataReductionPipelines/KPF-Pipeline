a5eb004e280b72b5f8431b65ef674acfa36866da
Greg Gilbert <gjgilbert@astro.ucla.edu>
2025-07-23 10:01:51 -0700

    troubleshooting recipes and tests

--- modules/stray_light/src/stray_light.py
+++ modules/stray_light/src/stray_light.py
@@ -36,11 +36,10 @@ class StrayLight(KPF0_Primitive):
                                        self.config_path
                                       )
             
-            out_2D = straylight.target_2D
             for chip in ['GREEN', 'RED']:
-                out_2D[f'{chip}_CCD'] = straylight.remove_stray_light(chip)
+                straylight.target_2D[f'{chip}_CCD'] = straylight.remove_stray_light(chip)
             exit_code = 1
-            return Arguments([exit_code, out_2D])
+            return Arguments([exit_code, straylight.target_2D])
         except Exception as e:
             self.logger.error(f"StrayLight algorithm failed: {e}\n{traceback.format_exc()}")
             return Arguments([exit_code, None])
