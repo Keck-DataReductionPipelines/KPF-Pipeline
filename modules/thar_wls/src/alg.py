720a87dbe5191fe83771fd0f054458022491a1c6
Greg Gilbert <gjgilbert@astro.ucla.edu>
2026-02-04 15:48:17 -0800

    added functions for fitting WLS legendre polynomial over FFI

--- modules/thar_wls/src/alg.py
+++ modules/thar_wls/src/alg.py
@@ -53,6 +53,7 @@ class WLSAlg:
         # init routines
         self._load_stack()
         self._set_linefunc(str(cfg_params.get_config_value('linefunc')))
+        
 
 
     def _load_stack(self):
@@ -195,7 +196,7 @@ class WLSAlg:
         return line_x, line_w
 
 
-    def fit_line_positions_ffi(self, 
+    def fit_line_positions(self, 
                                obs_id, 
                                chip, 
                                fiber, 
@@ -243,9 +244,131 @@ class WLSAlg:
 
             line_m[o] = (o+1)*np.ones_like(line_x[o], dtype=int)
              
-
         line_x = np.hstack(line_x)
         line_w = np.hstack(line_w)
         line_m = np.hstack(line_m)
 
+        if do_plot:
+            plt.figure(figsize=(5,6))
+            plt.plot(line_x, line_w, 'k.')
+            plt.xlabel("pixel column", fontsize=16)
+            plt.ylabel("wavelength (A)", fontsize=16)
+            plt.title(f"{obs_id} | {chip} {fiber}")
+            plt.show()
+
         return line_x, line_w, line_m
+
+
+    def calculate_wls_coeffs(self, 
+                             obs_id, 
+                             chip, 
+                             method, 
+                             linelist = None,
+                             linefunc = None, 
+                             window = 5, 
+                             qc_sigma = 2.5,
+                             polyorder_x = None,
+                             polyorder_m = None,
+                             polyorder_s = None,
+                             verbose = True,
+                             do_plot = False,
+                             ):
+        """
+        Docstring 
+        """
+        # sanitize inputs
+        if linelist is None:
+            linelist = self.linelist
+        if linefunc is None:
+            linefunc = self.linefunc
+
+        if polyorder_x is None:
+            polyorder_x = self.polyorder_x
+        if polyorder_m is None:
+            polyorder_m = self.polyorder_m
+        if polyorder_s is None:
+            polyorder_s = self.polyorder_s
+
+        flux_ext, _, _ = self._get_orderlet_ext_from_fiber_name(chip, 'CAL')
+        norder, ncol = np.shape(self.rough_wls[flux_ext])
+
+        if method == 'SCI':
+            fibers = ['SCI1', 'SCI2', 'SCI3']
+        elif method == 'CAL':
+            fibers = ['CAL']
+        else:
+            raise ValueError("method must be 'SCI' or 'CAL'")
+        
+        # fit line positions order-by-order
+        line = {}
+        line['x'] = [None]*len(fibers)     # x = pixel
+        line['w'] = [None]*len(fibers)     # w = wavelength
+        line['m'] = [None]*len(fibers)     # m = order
+        line['f'] = [None]*len(fibers)     # f = fiber
+
+        for i, fiber in enumerate(fibers):
+            if verbose:
+                print(f"fitting line positions for {fiber} fiber")
+            
+            result = self.fit_line_positions_ffi(obs_id, 
+                                                 chip, 
+                                                 fiber, 
+                                                 linelist = linelist,
+                                                 linefunc = linefunc, 
+                                                 window = 5, 
+                                                 qc_sigma = 2.5,
+                                                 verbose = True,
+                                                 do_plot = False,
+                                                 )
+
+            line['x'][i], line['w'][i], line['m'][i] = result
+
+            if method == 'SCI':
+                line['f'][i] = int(fiber[-1])*np.ones_like(result[0])
+            elif method == 'CAL':
+                line['f'][i] = np.zeros_like(result[0])
+
+        for k in line.keys():
+            line[k] = np.hstack(line[k])
+
+        # rescale position variables to [-1,1] for Legendre fitting
+        _x = 2*line['x']/ncol - 1
+        _m = 2*(line['m'] - line['m'].min())/(line['m'].max() - line['m'].min()) - 1
+
+        if method == 'SCI':
+            _f = line['f'] - 2
+
+        # fit Legendre polynomials
+        if method == 'SCI':
+            V = legendre.legvander3d(_x, _m, _f, deg=[polyorder_x, polyorder_m, polyorder_s])
+
+            coeffs, *_ = np.linalg.lstsq(V, line['w'], rcond=None)
+            coeffs = coeffs.reshape(polyorder_x+1, polyorder_m+1, polyorder_s+1)
+
+        elif method == 'CAL':
+            V = legendre.legvander2d(_x, _m, deg=[polyorder_x, polyorder_m])
+
+            coeffs, *_ = np.linalg.lstsq(V, line['w'], rcond=None)
+            coeffs = coeffs.reshape(polyorder_x+1, polyorder_m+1)
+
+        return coeffs
+
+
+    @staticmethod
+    def evaluate_wls_coeffs(coeffs, ncol, norder, nfiber):
+        """
+        Docstring
+        """
+        _x = np.linspace(-1, 1, ncol)
+        _y = np.linspace(-1, 1, norder)
+        _z = np.linspace(-1, 1, nfiber)
+
+        if coeffs.ndim == 2:
+            X, Y = np.meshgrid(_x, _y)
+            W = legendgre.legval2d(X, Y, coeffs)
+        
+        elif coeffs.ndim == 3:
+            X, Y, Z = np.meshgrid(_x, _y, _z)
+            W = legendre.legval3d(X, Y, Z, coeffs)
+
+        return W
