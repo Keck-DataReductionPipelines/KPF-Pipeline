538eb6f47b090490a93ad8a8a702ccd907353289
Greg Gilbert <gjgilbert@astro.ucla.edu>
2026-02-04 10:56:18 -0800

    add staticmethod utils

--- modules/thar_wls/src/alg.py
+++ modules/thar_wls/src/alg.py
@@ -38,3 +38,63 @@ class WLSAlg:
             self.l1_stack[i] = KPF1.from_fits(filepath, data_type='KPF')
 
         return self.l1_stack
+
+
+    @staticmethod
+    def _get_orderlet_ext_from_fiber_name(chip, fiber):
+        flux_dict = {'SKY': f'{chip}_SKY_FLUX',
+                    'SCI1': f'{chip}_SCI_FLUX1',
+                    'SCI2': f'{chip}_SCI_FLUX2',
+                    'SCI3': f'{chip}_SCI_FLUX3',
+                    'CAL': f'{chip}_CAL_FLUX'
+                    }
+
+        var_dict = {'SKY': f'{chip}_SKY_VAR',
+                    'SCI1': f'{chip}_SCI_VAR1',
+                    'SCI2': f'{chip}_SCI_VAR2',
+                    'SCI3': f'{chip}_SCI_VAR3',
+                    'CAL': f'{chip}_CAL_VAR'
+                }
+
+        wave_dict = {'SKY': f'{chip}_SKY_WAVE',
+                    'SCI1': f'{chip}_SCI_WAVE1',
+                    'SCI2': f'{chip}_SCI_WAVE2',
+                    'SCI3': f'{chip}_SCI_WAVE3',
+                    'CAL': f'{chip}_CAL_WAVE'
+                }
+        
+        return flux_dict[fiber], var_dict[fiber], wave_dict[fiber]
+
+
+    @staticmethod
+    def _get_norder(chip):
+        norder_dict = {'GREEN':35, 'RED':32}
+
+        return norder_dict[chip]
+
+
+    @staticmethod
+    def clipped_mean(x, p):
+        xmin, xmax = np.nanpercentile(x, [p/2,100-p/2])
+        return np.nanmean(x[(x > xmin) & (x < xmax)])
+
+    
+    @staticmethod
+    def gaussian(theta, x):
+        mu, sigma, a, b = theta
+
+        return b + a * np.exp(-(x-mu)**2/(2*sigma**2))
+
+    
+    @staticmethod
+    def optimize_lsq(func, theta0, x, y):
+        """
+        optimize theta for a given function using non-linear least-squares
+        """
+        def _residuals(theta, x, y):
+            return func(theta, x) - y
+        
+        result = least_squares(_residuals, theta0, args=(x,y))
+        theta, rms = result.x, np.std(result.fun)
+        
+        return theta, rms
