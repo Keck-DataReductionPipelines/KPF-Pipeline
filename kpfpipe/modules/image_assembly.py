f3487ff19f1208f3cdd3a52208b71438bb6f601a
Greg Gilbert <gjgilbert@astro.ucla.edu>
2026-02-17 19:39:35 -0800

    cleaned up a few image assembly methods

--- kpfpipe/modules/image_assembly.py
+++ kpfpipe/modules/image_assembly.py
@@ -15,62 +15,64 @@ from kpfpipe.data_models.level1 import KPF1
 class ImageAssembly:
     def __init__(self, l0_obj, config=None):
         self.l0_obj = copy.deepcopy(l0_obj)
-
-        self.count_amplifiers('GREEN')
-        self.count_amplifiers('RED')
+        
+        CHIPS = ['GREEN', 'RED']
+        for chip in CHIPS:
+            self.count_amplifiers(chip)
+            self.orient_channels(chip)
 
         # temporarily hard-code config params during development
-        self.prescan_region = [0,4]
-        self.overscan_method = 'rowmedian'
-        self.overscan_buffer = [5,5]
-        self.overscan_sigma = 2.1
-        self.overscan_order = 1
+        overscan_method = 'rowmedian'
+        overscan_sigma_clip = 2.1
 
-
+    
     def count_amplifiers(self, chip):
         """
         Determine if extensions are present for a given CCD and
         count the number of amplifier regions. Sets attributes to
         track chips, namp, and dims (i.e. channel dimensions)
         """
-        if not hasattr(self.chips):
-            self.chips = []
         if not hasattr(self.namp):
             self.namp = {}
         if not hasattr(self.dims):
             self.dims = {}
 
         chip = chip.upper()
-        
+
         self.namp[chip] = 0
         for i in range(4):
             if f'{chip}_AMP{i+1}' in self.l0_obj.extensions:
                 if np.size(self.l0_obj[f'{chip}_AMP{i+1}']) > 0:
                     self.namp[chip] += 1
 
-        if self.namp[chip] > 0:
-            self.chips.append(chip)
+        if self.namp[chip] == 2:
+            self.dims[chip] = (4080, 2040)
+        elif self.namp[chip] == 4:
+            self.dims[chip] = (2040, 2040)
+        else:
+            raise ValueError(f"Only 2-amp and 4-amp mode supported, detected {self.namp[chip]} on {chip} CCD")
+        
 
-            if self.namp[chip] == 2:
-                self.dims[chip] = (4080, 2040)
-            elif self.namp[chip] == 4:
-                self.dims[chip] = (2040, 2040)
-            else:
-                raise ValueError(f"Only 2-amp and 4-amp mode supported, detected {self.namp[chip]} on {chip} CCD")
+    def orient_channels(self, chip):
+        chip = chip.upper()
+
+        for i in range(self.namp[chip]):
+            self._orient_single_channel(chip, i+1)
 
 
     def _read_orientation_reference(self, chip):
         if not hasattr(self, 'orientation'):
             self.orientation = {}
 
-        filepath = f'static/ccd_orrientation_{chip.lower()}_{self.namp[chip.upper()]}amp.txt'
+        # TODO: fix filepath handling and make it robust
+        filepath = f'static/ccd_orientation_{chip.lower()}_{self.namp[chip.upper()]}amp.txt'
         with open(filepath, 'r') as f:
             self.orientation[chip.upper()] = pd.read_csv(f, delimiter=' ')
 
         return self.orientation[chip.upper()]
     
 
-    def _orient_channel(self, chip, amp_no):
+    def _orient_single_channel(self, chip, amp_no):
         """
         Extracts and flips single-amplifier image to standardize readout orientation.
             - serial overscan on right
@@ -85,7 +87,7 @@ class ImageAssembly:
             np.ndarray : Correctly-oriented raw image for single amplifier region
         """
         channel = f'{chip.upper()}_AMP{amp_no}'
-        image = np.array(self.l1_obj[channel])
+        image = np.array(self.l0_obj[channel])
 
         try:
             orientation = self.orientation[chip]
@@ -103,32 +105,32 @@ class ImageAssembly:
         elif channel_key == 4: # no change
             image_reoriented = image
 
-        return image_reoriented
+        self.l0_obj[channel] = image_reoriented
 
     
     def _get_overscan_pixels(self, chip, amp_no, prescan=[0,4], buffer=[5,5]):
         """
         Gets array of overscan pixel from full amplifier region
-        Assumes image orientaion has not been altered from raw L0 file. 
+        Assumes image orientaion has been standardized
+            - serial overscan on right
+            - parallel overscan on bottom
 
         Args:
             chip (str) : which CCD to use, 'GREEN' or 'RED'
-            amp (int) : amplifier number
+            amp_no (int) : amplifier number
 
         Returns:
             oscan_pix_srl (np.ndarray): Array of serial overscan pixels
             oscan_pix_prl (np.ndarray): Array of parallel overscan pixels
         """
-        # TODO: reconcile with config for prescan_region and overscan_buffer
-        # TODO: add checks for valid prescan and buffer
         chip = chip.upper()
-        image = self._orient_channel(chip, amp_no)
+        full_amplifier = self.l0_obj[f'{chip}_AMP{amp_no}']
         
         ncol_prescan = prescan[1] - prescan[0]
-        ncol_datasec, nrow_datasec = self.dims[chip]
+        ncol_imaging, nrow_imaging = self.dims[chip]
 
-        oscan_pix_srl = image[:,ncol_prescan+ncol_datasec:]
-        oscan_pix_prl = image[nrow_datasec:,:]
+        oscan_pix_srl = full_amplifier[:,ncol_prescan+ncol_imaging:]
+        oscan_pix_prl = full_amplifier[nrow_imaging:,:]
 
         oscan_pix_srl = oscan_pix_srl[:,buffer[0]:-buffer[1]-1]
         oscan_pix_prl = oscan_pix_prl[buffer[0]:-buffer[1]-1,:]
@@ -136,29 +138,29 @@ class ImageAssembly:
         return oscan_pix_srl, oscan_pix_prl
 
 
-    def _remove_overscan_pixels(self, chip, amp_no, prescan=[0,4], buffer=[5,5]):
+    def _get_imaging_pixels(self, chip, amp_no, prescan=[0,4]):
         """
-        Removes overscan pixels from full amplifier region
-        Assumes image orientaion has not been altered from raw L0 file. 
+        Gets array of imaging pixels from full amplifier region
+        Assumes image orientaion has been standardized
+            - serial overscan on right
+            - parallel overscan on bottom
 
         Args:
             chip (str) : which CCD to use, 'GREEN' or 'RED'
-            amp (int) : amplifier number
+            amp_no (int) : amplifier number
 
         Returns:
-            ndarray : image with only datasec pixels
+            ndarray : data with only active imaging area pixels
         """
         chip = chip.upper()
-        channel = f'{chip}_AMP{amp_no}'
-        image = self.orient_channel(chip, amp_no)
+        full_amplifier = self.l0_obj[f'{chip}_AMP{amp_no}']
         
         ncol_prescan = prescan[1] - prescan[0]
-        ncol_datasec, nrow_datasec = self.dims[chip]
+        ncol_imaging, nrow_imaging = self.dims[chip]
 
-        self.l0_obj[channel] = image[:nrow_datasec,ncol_prescan:ncol_prescan+ncol_datasec]
-        self.l0_obj[channel] = self._orient_channel(chip, amp_no)
+        image = full_amplifier[:nrow_imaging,ncol_prescan:ncol_prescan+ncol_imaging]
         
-        return self.target_l0[channel]
+        return image
 
     
     
