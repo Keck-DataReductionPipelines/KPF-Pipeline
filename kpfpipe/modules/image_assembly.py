9733c2b7caf702723ed872587012b4866e16a26f
Greg Gilbert <gjgilbert@astro.ucla.edu>
2026-02-17 20:08:10 -0800

    combined image assembly orient_channels into a single method

--- kpfpipe/modules/image_assembly.py
+++ kpfpipe/modules/image_assembly.py
@@ -1,11 +1,9 @@
 """
 KPF Image Assembly Module.
 
-Orients and assembles raw data from amplifers
-into a full frame image. Processes data from L0
-to L1.
+Orients and assembles raw data from amplifers into 
+a full frame image. Processes data from L0 to L1.
 """
-import copy
 import numpy as np
 import pandas as pd
 
@@ -14,7 +12,7 @@ from kpfpipe.data_models.level1 import KPF1
 
 class ImageAssembly:
     def __init__(self, l0_obj, config=None):
-        self.l0_obj = copy.deepcopy(l0_obj)
+        self.l0_obj = l0_obj
         
         CHIPS = ['GREEN', 'RED']
         for chip in CHIPS:
@@ -22,6 +20,7 @@ class ImageAssembly:
             self.orient_channels(chip)
 
         # temporarily hard-code config params during development
+        # switch to config once that interface has been standardized
         overscan_method = 'rowmedian'
         overscan_sigma_clip = 2.1
 
@@ -31,6 +30,12 @@ class ImageAssembly:
         Determine if extensions are present for a given CCD and
         count the number of amplifier regions. Sets attributes to
         track chips, namp, and dims (i.e. channel dimensions)
+        
+        Args:
+            chip (str) : which CCD to use, 'GREEN' or 'RED'
+        
+        Returns:
+            None
         """
         if not hasattr(self.namp):
             self.namp = {}
@@ -54,58 +59,50 @@ class ImageAssembly:
         
 
     def orient_channels(self, chip):
-        chip = chip.upper()
-
-        for i in range(self.namp[chip]):
-            self._orient_single_channel(chip, i+1)
-
-
-    def _read_orientation_reference(self, chip):
-        if not hasattr(self, 'orientation'):
-            self.orientation = {}
-
-        # TODO: fix filepath handling and make it robust
-        filepath = f'static/ccd_orientation_{chip.lower()}_{self.namp[chip.upper()]}amp.txt'
-        with open(filepath, 'r') as f:
-            self.orientation[chip.upper()] = pd.read_csv(f, delimiter=' ')
-
-        return self.orientation[chip.upper()]
-    
-
-    def _orient_single_channel(self, chip, amp_no):
         """
-        Extracts and flips single-amplifier image to standardize readout orientation.
+        Extracts and flips amplifier channels to standardize readout orientation.
             - serial overscan on right
             - parallel overscan on bottom
         All transformations are flips, so a second call to this function will undo the transformation.
 
         Args:
             chip (str) : which CCD to use, 'GREEN' or 'RED'
-            amp (int) : amplifier number
         
         Returns:
-            np.ndarray : Correctly-oriented raw image for single amplifier region
+            None
         """
-        channel = f'{chip.upper()}_AMP{amp_no}'
-        image = np.array(self.l0_obj[channel])
+        chip = chip.upper()
+        orientation = self._read_orientation_reference(chip)
 
-        try:
-            orientation = self.orientation[chip]
-        except Exception as e:
-            orientation = self._read_orientation_reference(chip)
+        for i in range(self.namp[chip]):
+            channel_ext = f'{chip.upper()}_AMP{i+1}'
+            channel_key = int(orientation.loc[orientation.CHANNEL_EXT == channel_ext, 'CHANNEL_KEY'])
+            image = np.array(self.l0_obj[channel_ext])
 
-        channel_key = int(orientation.loc[orientation.CHANNEL_EXT == channel, 'CHANNEL_KEY'])
+            if channel_key == 1: # flip lr
+                image_reoriented = np.flip(image,axis=1)
+            elif channel_key == 2: # turn upside down and flip lr
+                image_reoriented = np.flip(np.flip(image,axis=0),axis=1)
+            elif channel_key == 3: # turn upside down
+                image_reoriented = np.flip(image,axis=0)
+            elif channel_key == 4: # no change
+                image_reoriented = image
 
-        if channel_key == 1: # flip lr
-            image_reoriented = np.flip(image,axis=1)
-        elif channel_key == 2: # turn upside down and flip lr
-            image_reoriented = np.flip(np.flip(image,axis=0),axis=1)
-        elif channel_key == 3: # turn upside down
-            image_reoriented = np.flip(image,axis=0)
-        elif channel_key == 4: # no change
-            image_reoriented = image
+            self.l0_obj[channel_ext] = image_reoriented
 
-        self.l0_obj[channel] = image_reoriented
+
+    def _read_orientation_reference(self, chip):
+        chip = chip.upper()
+        
+        if not hasattr(self, 'orientation'):
+            self.orientation = {}
+
+        # TODO: fix filepath handling and make it robust
+        filepath = f'static/ccd_orientation_{chip.lower()}_{self.namp[chip]}amp.txt'
+        with open(filepath, 'r') as f:
+            self.orientation[chip] = pd.read_csv(f, delimiter=' ')
+
+        return self.orientation[chip]
 
     
     def _get_overscan_pixels(self, chip, amp_no, prescan=[0,4], buffer=[5,5]):
@@ -127,7 +124,7 @@ class ImageAssembly:
         full_amplifier = self.l0_obj[f'{chip}_AMP{amp_no}']
         
         ncol_prescan = prescan[1] - prescan[0]
-        ncol_imaging, nrow_imaging = self.dims[chip]
+        nrow_imaging, ncol_imaging = self.dims[chip]
 
         oscan_pix_srl = full_amplifier[:,ncol_prescan+ncol_imaging:]
         oscan_pix_prl = full_amplifier[nrow_imaging:,:]
@@ -156,11 +153,11 @@ class ImageAssembly:
         full_amplifier = self.l0_obj[f'{chip}_AMP{amp_no}']
         
         ncol_prescan = prescan[1] - prescan[0]
-        ncol_imaging, nrow_imaging = self.dims[chip]
+        nrow_imaging, ncol_imaging = self.dims[chip]
 
-        image = full_amplifier[:nrow_imaging,ncol_prescan:ncol_prescan+ncol_imaging]
+        image_pix = full_amplifier[:nrow_imaging,ncol_prescan:ncol_prescan+ncol_imaging]
         
-        return image
+        return image_pix
 
     
     
