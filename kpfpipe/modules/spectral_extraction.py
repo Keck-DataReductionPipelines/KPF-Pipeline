b9d767f0d6a056d25ebbb2a6cef3f51c7e87f3f9
Greg Gilbert <gjgilbert@astro.ucla.edu>
2026-02-19 14:58:22 -0800

    manually constructed order trace reference from 20240405

--- kpfpipe/modules/spectral_extraction.py
+++ kpfpipe/modules/spectral_extraction.py
@@ -6,8 +6,26 @@ Processes data from L1 to SL2.
 """
 
 from kpfpipe import REPO_ROOT
-DEFAULTS = {''}
+DEFAULTS = {'extraction_method':'box'}
 
 class SpectralExtraction:
-    def __init__(self, ffi_obj):
-        pass
+    def __init__(self, l1_obj, config={}):
+        self.l1_obj = l1_obj
+        self.CHIPS = ['GREEN', 'RED']
+
+        # TODO: check if this config parsing works
+        for k in DEFAULTS.keys():
+            self.__setattr__(k, config.get(k,DEFAULTS[k]))
+
+
+    def _read_order_trace_reference(self, chip):
+        chip = chip.upper()
+        
+        if not hasattr(self, 'order_trace'):
+            self.order_trace = {}
+
+        filepath = f'{REPO_ROOT}/reference/order_trace_{chip.lower()}.txt'
+        with open(filepath, 'r') as f:
+            self.order_trace[chip] = pd.read_csv(f, index_col=0)
+
+        return self.order_trace[chip]
