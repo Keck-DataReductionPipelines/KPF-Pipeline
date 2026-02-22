222585d4d4684cd49540c80a5e46fec8be259998
Greg Gilbert <gjgilbert@astro.ucla.edu>
2026-02-22 10:23:54 -0800

    built method BaseMastersModule.compute_direct_statistics()

--- kpfpipe/modules/masters/base.py
+++ kpfpipe/modules/masters/base.py
@@ -3,12 +3,14 @@ Base class for KPF Masters modules.
 """
 from astropy.stats import mad_std
 import numpy as np
+import warnings
 
 from kpfpipe import DEFAULTS
+from kpfpipe.constants import NROW, NCOL
 from kpfpipe.data_models.level0 import KPF0
 from kpfpipe.modules.image_assembly import ImageAssembly
-from kpfpipe.utils import get_datecode, fetch_filepath
-from kpfpipe.constants import NROW, NCOL
+from kpfpipe.utils.kpf_parse import get_datecode, fetch_filepath
+from kpfpipe.utils.stats import flag_outliers
 
 DEFAULTS.update({
     'nframe_stream': 5,
@@ -18,7 +20,7 @@ DEFAULTS.update({
 
 class BaseMastersModule:
     def __init__(self, l0_file_list):
-        self.obs_ids = obs_ids
+        self.l0_file_list = l0_file_list
 
         for k in DEFAULTS.keys():
             self.__setattr__(k, config.get(k,DEFAULTS[k]))
@@ -32,7 +34,8 @@ class BaseMastersModule:
 
     @staticmethod
     def assemble_frame(l0_obj):
-        return ImageAssembly(l0_obj).perform()
+        l1_obj = ImageAssembly(l0_obj).perform()
+        return l1_obj
 
 
     def stack_frames(self, l0_file_list=None, nframe_stream=None, sigma=None):
@@ -59,13 +62,28 @@ class BaseMastersModule:
         return mean, var
 
 
-    def compute_direct_statistics(self, l0_file_list=None, nframe_max=None, sigma=None):
+    def compute_direct_statistics(self, l0_file_list=None, nframe_stack=None, nframe_cache=None, sigma=None):
+        """
+        nframe_stack : maximum number of frames to stack for computing statistics
+        nframe_cache : maximum number of L1 objects to cache
+
+        Notes
+        -----
+        This function will automatically cache a subset of L1 objects for downstream use.
+        The maximum number of cached items allowed is set by nframe_cached, but the actual number
+        of cached items will be the smallest of len(l0_file_list), nframe_stack, and nframe_cache.
+        """
         if l0_file_list is None:
             l0_file_list = self.l0_file_list
-        if nframe_max is None:
-            nframe_max = self.nframe_stream
+        if nframe_stack is None:
+            nframe_stack = self.nframe_stream
+        if nframe_cache is None:
+            nframe_cache = self.nframe_stream
+        if sigma is None:
+            sigma = self.stack_sigma
         
-        nframe = np.min([nframe_max,len(l0_file_list)])
+        nframe = np.min([nframe_stack,len(l0_file_list)])
+        ncache = np.min([nframe, nframe_cache])
 
         if not hasattr(self, '_l1_obj_cache'):
             self._l1_obj_cache = {}
@@ -73,50 +91,70 @@ class BaseMastersModule:
         data_cube = {}
 
         for chip in self.chips:
-            chip = chip.upper()
-            data_cube[chip] = np.empty((nframe,NROW,NCOL),dtype=np.float32)
+            data_cube[f'{chip}_CCD'] = np.zeros((nframe,NROW,NCOL),dtype=np.float32)
+            data_cube[f'{chip}_VAR'] = np.zeros((nframe,NROW,NCOL),dtype=np.float32)
 
-            i = 0
-            failure = 0
-            while (i < nframe) and (i + failure < len(l0_file_list)):
-                for fn in l0_file_list:
-                    if np.isin(fn, self._l1_obj_cache[fn]):
-                        data_cube[i]
+        i = 0
+        failure = 0
 
+        for fn in l0_file_list:
+            if i >= nframe:
+                break
 
+            if fn in self._l1_obj_cache.keys():
+                for chip in self.chips:
+                    data_cube[f'{chip}_CCD'] = self._l1_obj_cache[fn].data[f'{chip}_CCD']
+                    data_cube[f'{chip}_VAR'] = self._l1_obj_cache[fn].data[f'{chip}_VAR']
+                i += 1
+
+            else:
                 try:
-                    
-                    data_cube[i] = self.load_frame(fn).assemble_frame()
+                    l0_obj = self.load_frame(fn)
+                    l1_obj = self.assemble_frame(l0_obj)
 
+                    for chip in self.chips:
+                        data_cube[f'{chip}_CCD'][i] = l1_obj.data[f'{chip}_CCD']
+                        data_cube[f'{chip}_VAR'][i] = l1_obj.data[f'{chip}_VAR']
+    
+                    if len(self._l1_obj_cache.keys()) < ncache:
+                        self._l1_obj_cache[fn] = l1_obj
 
-        for i, obs_id in enumerate(obs_ids):
-            # TODO: scale by exposure time
-            try:
-                l0_obj = self.load_frame(obs_id)
-                frame = self.assemble_frame(l0_obj)
-                data_cube[i] = frame
-            except Exception as e:
-                logger.warning(f"Skipping {obs_id} in compute_direct_mean_and_variance: {e}")
-                data_cube[i,...] = np.nan
-                failure += 1
-                continue
+                    i += 1
 
-            if failure / nframe > 0.2:
-                raise ValueError(f"more than 20% of frames in stack failed to load")
+                except FileNotFoundError as e:
+                    warnings.warn(f"Skipping {fn} in compute_direct_statistics: {e}")
+                    failure += 1
+                    continue
 
-        if not sigma_clip:
-            mean = np.nanmean(data_cube, axis=0)
-            var = np.nanvar(data_cube, axis=0)
-            return mean, var
+        if failure / nframe > 0.2:
+            raise ValueError(f"more than 20% of frames in stack failed to load")
 
-        med = np.nanmedian(data_cube, axis=0)
-        mad = mad_std(data_cube, axis=0, ignore_nan=True)
-        out = np.abs(data_cube - med) / mad > sigma_clip
+        stats = {}
 
-        clipped_mean = np.nansum(np.where(out, np.nan, data_cube), axis=0) / np.sum(~out, axis=0)
-        clipped_var = np.nansum(np.where(out, np.nan, (data_cube - clipped_mean)**2), axis=0) / np.sum(~out, axis=0)
+        for chip in chips:
+            stats[f'{chip}_CCD'] = {}
+            stats[f'{chip}_VAR'] = {}
 
-        return clipped_mean, clipped_var
+            # TODO: streamline quality control
+            if np.any(np.isnan(data_cube[f'{chip}_CCD'])):
+                raise ValueError(f"NaN values detected in {chip}_CCD data cube")
+            if np.any(np.isnan(data_cube[f'{chip}_VAR'])):
+                raise ValueError(f"NaN values detected in {chip}_VAR data cube")
+
+            out = flag_outliers(data_cube[f'{chip}_CCD'], axis=0) | flag_outliers(data_cube[f'{chip}_VAR'], axis=0)
+
+            for suffix in ['CCD', 'VAR']:
+                ext_name = f'{chip}_{suffix}'
+
+                stats[ext_name]['nframe'] = np.sum(~out, axis=0)
+
+                S = np.sum(np.where(out, 0, data_cube[ext_name]), axis=0)
+                stats[ext_name]['mean'] = S / stats[ext_name]['nframe']
+
+                S2 = np.sum(np.where(out, 0, (data_cube[ext_name] - stats[ext_name]['mean'])**2), axis=0)
+                stats[ext_name]['rms'] = np.sqrt(S2 / stats[ext_name]['nframe'])
+
+        return stats
 
 
     def compute_streaming_statistics(self, sigma_clip=5.0):
