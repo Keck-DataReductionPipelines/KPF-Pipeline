34cd7a163d21b37190837d3cfe736b81f9ca8f7e
Greg Gilbert <gjgilbert@astro.ucla.edu>
2026-02-22 07:42:49 -0800

    bugfix for default parameters handling in ImageAssembly

--- tests/profile_image_assembly.py
+++ tests/profile_image_assembly.py
@@ -7,11 +7,10 @@ from kpfpipe.utils.kpf import get_datecode
 
 from line_profiler import LineProfiler
 
+OBS_ID = 'KP.20240405.49597.71'    # 2-amp mode
+#OBS_ID = 'KP.20250419.84046.71'    # 4-amp mode
 
 def run():
-    OBS_ID = 'KP.20240405.49597.71'    # 2-amp mode
-    #OBS_ID = 'KP.20250419.84046.71'    # 4-amp mode
-
     datecode = get_datecode(OBS_ID)
     filepath = os.path.join('/data/kpf/L0/', datecode, f'{OBS_ID}.fits')
 
@@ -23,7 +22,7 @@ def run():
         image_assembly.orient_channels(chip)
         image_assembly.apply_gain_conversion(chip)
         image_assembly.measure_read_noise(chip)
-        image_assembly.subtract_overscan(chip, 'rowmedian')
+        image_assembly.subtract_overscan(chip)
         image_assembly.orient_channels(chip)
         
         ccd_ffi, var_ffi = image_assembly.stitch_ffi(chip)
